6c8630791a3ba35db8e5940d62d95f32d09513c0
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-05-19 20:34:20 +0530

    Added solution - LeetHub

---
+++ Search in a Rotated Array - GFG/search-in-a-rotated-array.java
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+
+public class GFG
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while (t-- > 0)
+        {
+            int n = sc.nextInt();
+            int A[] = new int[n];
+            
+            for (int i = 0;i < n;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            int key = sc.nextInt();
+            
+            System.out.println(new Solution().search(A, 0, n - 1, key));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    int search(int A[], int l, int h, int key)
+    {
+        while(l<=h){
+            int mid = (l+h)/2;
+            if(A[mid]==key)
+                return mid;
+            if(A[l]<A[mid]){
+                if(key>=A[l] && key<A[mid]){
+                    h = mid-1;
+                }else{
+                    l = mid+1;
+                }
+            }else{
+                if(key>A[mid] && key<=A[h]){
+                    l = mid+1;
+                }else{
+                    h = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
