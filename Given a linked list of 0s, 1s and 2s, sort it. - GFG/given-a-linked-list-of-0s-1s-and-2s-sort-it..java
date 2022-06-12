32a8af782d2213a01a9df388425ab9a24051e7ce
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-06-12 22:12:22 +0530

    Added solution - LeetHub

--- Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..java
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..java
@@ -79,33 +79,65 @@ class Solution
     //Function to sort a linked list of 0s, 1s and 2s.
     static Node segregate(Node head)
     {
-        Node res = head;
-        Node ans = head;
-        int cnt0=0,cnt1=0,cnt2=0;
-        while(head!=null){
-            if(head.data==0){
-                cnt0++;
-            }else if(head.data==1){
-                cnt1++;
-            }else{
-                cnt2++;
+        // Node res = head;
+        // Node ans = head;
+        // int cnt0=0,cnt1=0,cnt2=0;
+        // while(head!=null){
+        //     if(head.data==0){
+        //         cnt0++;
+        //     }else if(head.data==1){
+        //         cnt1++;
+        //     }else{
+        //         cnt2++;
+        //     }
+        //     head = head.next;
+        // }
+        // while(cnt0--!=0){
+        //     res.data = 0;
+        //     res = res.next;
+        // }
+        // while(cnt1--!=0){
+        //     res.data = 1;
+        //     res = res.next;
+        // }
+        // while(cnt2--!=0){
+        //     res.data = 2;
+        //     res = res.next;
+        // }
+        
+        // return ans;
+        
+        Node zeroD = new Node(0);
+        Node oneD = new Node(0);
+        Node twoD = new Node(0);
+        
+        Node zero = zeroD,one = oneD, two = twoD;
+        
+        Node curr = head;
+        
+        while(curr!=null){
+            if(curr.data==0){
+                zero.next = curr;
+                zero = zero.next;
+                curr = curr.next;
+            }else if(curr.data==1){
+                one.next = curr;
+                one = one.next;
+                curr = curr.next;
+            }else if(curr.data==2){
+                two.next = curr;
+                two = two.next;
+                curr = curr.next;
             }
-            head = head.next;
-        }
-        while(cnt0--!=0){
-            res.data = 0;
-            res = res.next;
-        }
-        while(cnt1--!=0){
-            res.data = 1;
-            res = res.next;
-        }
-        while(cnt2--!=0){
-            res.data = 2;
-            res = res.next;
         }
         
-        return ans;
+        zero.next = (oneD.next!=null)? oneD.next : twoD.next;
+        one.next = twoD.next;
+        two.next = null;
+        
+        head = zeroD.next;
+        
+        return head;
     }
 }
 
