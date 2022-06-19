06ac1f9880a228c4054eb45f9f152dffe1a23cf2
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-06-19 10:43:07 +0530

    Time: 3 ms (61.13%), Space: 84.7 MB (11.12%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,30 +1,30 @@
 class Solution {
     public int maxProfit(int[] arr) {
-//         int profit = 0;
-//         int minSofar = arr[0];
-        
-//         for(int i = 0 ; i <arr.length ; i++){
-//             minSofar = Math.min(minSofar,arr[i]);
-            
-//             profit = Math.max(profit,arr[i]-minSofar);
-//         }
-        
-//         return profit;
-        
-        int l = 0 ;
-        int r = 1;
         int profit = 0;
-        int maxP = 0;
-        while(r<arr.length){
-            if(arr[l]<arr[r]){
-                profit = arr[r] - arr[l];
-                maxP = Math.max(maxP,profit);
-            }else{
-                l = r;
-            }
-            r++;
+        int minSofar = arr[0];
+        
+        for(int i = 0 ; i <arr.length ; i++){
+            minSofar = Math.min(minSofar,arr[i]);
+            
+            profit = Math.max(profit,arr[i]-minSofar);
         }
         
-        return maxP;
+        return profit;
+        
+//         int l = 0 ;
+//         int r = 1;
+//         int profit = 0;
+//         int maxP = 0;
+//         while(r<arr.length){
+//             if(arr[l]<arr[r]){
+//                 profit = arr[r] - arr[l];
+//                 maxP = Math.max(maxP,profit);
+//             }else{
+//                 l = r;
+//             }
+//             r++;
+//         }
+        
+//         return maxP;
     }
 }
