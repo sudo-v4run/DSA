307862539a466ec2fb3f750fbc8528e4e0103262
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-07-02 22:06:48 +0530

    Added solution - LeetHub

---
+++ String Ignorance - GFG/string-ignorance.java
@@ -0,0 +1,30 @@
+/*package whatever //do not write package name here */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		
+	    Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+	    sc.nextLine();
+	    
+	    while(t--!=0){
+	        HashMap<Character,Integer> hm = new HashMap<>();
+	        String S = sc.nextLine();
+	        String s = S.toLowerCase();
+	        for(int i = 0 ; i < s.length();i++){
+	            char ch = s.charAt(i);
+	            hm.put(ch,hm.getOrDefault(ch,0)+1);
+	            
+	            if((hm.get(ch))%2!=0){
+	                System.out.print(S.charAt(i));
+	            }
+	            
+	        }
+	        System.out.println();
+	    }
+	}
+}
