2dbab32a65951398b1dddf46113d44bb929f17d4
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-07-03 20:48:39 +0530

    Added solution - LeetHub

--- Number of 1 Bits - GFG/number-of-1-bits.java
+++ Number of 1 Bits - GFG/number-of-1-bits.java
@@ -22,14 +22,21 @@ class GFG {
 // User function Template for Java
 class Solution {
     static int setBits(int n) {
-        int cnt = 0;
-        while(n!=0){
-            if((n&1)==1){
-                cnt++;
-            }
-            n=n>>1;
-        }
         
-        return cnt;
+        int cnt = 0;
+        
+        while(n!=0){
+            cnt++;
+            n = n & (n-1);
+        }
+        // int cnt = 0;
+        // while(n!=0){
+        //     if((n&1)==1){
+        //         cnt++;
+        //     }
+        //     n=n>>1;
+        // }
+        
+         return cnt;
     }
 }
