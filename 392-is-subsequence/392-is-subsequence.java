cc7b6ab745cd9174f35e2147fab2eac0c07a8273
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-07-11 05:22:45 +0530

    Time: 1 ms (93.02%), Space: 42.2 MB (32.76%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,27 +1,29 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-//         int i = 0 , j = 0;
-//         while(i<s.length() && j<t.length()){
-//             if(s.charAt(i)==t.charAt(j)){
-//                 i++;
-//             }
-//             j++;
-//         }
-        
-//         return i==s.length();
-        
-        int m = s.length();
-        int n = t.length();
-        if(m==0)
-            return true;
-        if(n==0)
-            return false;
-        if(s.charAt(m-1)==t.charAt(n-1)){
-            m = m-1;
-            n = n-1;
-            return isSubsequence(s.substring(0,m),t.substring(0,n));
+        int i = 0 , j = 0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+            }
+            j++;
         }
-        n = n-1;
-        return isSubsequence(s,t.substring(0,n));
+        
+        return i==s.length();
+        
+        
+        
+        // int m = s.length();
+        // int n = t.length();
+        // if(m==0)
+        //     return true;
+        // if(n==0)
+        //     return false;
+        // if(s.charAt(m-1)==t.charAt(n-1)){
+        //     m = m-1;
+        //     n = n-1;
+        //     return isSubsequence(s.substring(0,m),t.substring(0,n));
+        // }
+        // n = n-1;
+        // return isSubsequence(s,t.substring(0,n));
     }
 }
