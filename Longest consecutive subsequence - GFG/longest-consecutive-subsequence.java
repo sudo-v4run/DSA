095fb0c0e91046d8c558342297429189c51285b1
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-07-26 22:58:44 +0530

    Added solution - LeetHub

---
+++ Longest consecutive subsequence - GFG/longest-consecutive-subsequence.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			
+			for(int i=0; i<n; i++)
+				a[i] = sc.nextInt();
+		    out.println(new Solution().findLongestConseqSubseq(a, n));
+		    t--;
+		}
+		out.flush();
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{   
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+	static int findLongestConseqSubseq(int arr[], int N)
+	{
+	    int ans = 1;
+	    HashSet<Integer> hs = new HashSet<>();
+	    for(int e : arr){
+	        hs.add(e);
+	    }
+	    
+	    for(int e : hs){
+	        if(!hs.contains(e-1)){
+	            int len = 1;
+	            while(hs.contains(++e)){
+	                len++;
+	            }
+	            ans = Math.max(ans,len);
+	        }
+	    }
+	    
+	    return ans;
+	}
+}
