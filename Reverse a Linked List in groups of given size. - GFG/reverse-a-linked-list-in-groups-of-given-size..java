ee3bf216cd87f1f7bf2e53010ce73bd508b6778f
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-08-01 22:17:11 +0530

    Added solution - LeetHub

---
+++ Reverse a Linked List in groups of given size. - GFG/reverse-a-linked-list-in-groups-of-given-size..java
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class ReverseInSize
+{
+    static Node head;
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                // addToTheLast(new Node(a));
+                tail.next = new Node(a);
+                tail =tail.next;
+            }
+            
+            int k = sc.nextInt();
+            Solution ob = new Solution();
+            Node res = ob.reverse(head, k);
+            printList(res);
+            System.out.println();
+        }
+    }
+    
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+}
+
+
+// } Driver Code Ends
+
+
+/*node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution
+{
+    public static Node reverse(Node head, int k)
+    {
+        Node prevFirst = null, newHead = null;
+        Node cur = head;
+        while(cur!=null){
+            int count = 0;
+            Node prev = null;
+            Node first = cur;
+            while(cur!=null && count<k){
+                Node temp = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = temp;
+                count++;
+            }
+            
+            if(newHead==null){
+                newHead = prev;
+            }else{
+                prevFirst.next = prev;
+            }
+            prevFirst = first;
+        }
+        
+        return newHead;
+    }
+}
+
