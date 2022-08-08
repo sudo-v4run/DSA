1f3ac5846958dcb6b6e8baff4678473c9f770d3d
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-08-08 16:59:09 +0530

    Added solution - LeetHub

--- Clone a linked list with next and random pointer - GFG/clone-a-linked-list-with-next-and-random-pointer.java
+++ Clone a linked list with next and random pointer - GFG/clone-a-linked-list-with-next-and-random-pointer.java
@@ -189,10 +189,10 @@ class Clone {
         Node ori = head;
         cur = head.next;
         Node ans = cur;
-        while(head!=null&&cur!=null){
-            head.next = head.next.next;
+        while(ori!=null&&cur!=null){
+            ori.next = ori.next.next;
             cur.next = (cur.next!=null)?cur.next.next:null;
-            head=head.next;
+            ori=ori.next;
             cur = cur.next;
         }
         
