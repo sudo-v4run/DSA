29d8cb590dec31d7277be49c9cc003ccdda86564
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-08-17 09:47:40 +0530

    Added solution - LeetHub

--- IPL 2021 - Match Day 2 - GFG/ipl-2021-match-day-2.java
+++ IPL 2021 - Match Day 2 - GFG/ipl-2021-match-day-2.java
@@ -42,56 +42,49 @@ class Main {
 
 class Solution {
     static ArrayList<Integer> max_of_subarrays(int arr[], int n, int k) {
-        // ArrayList<Integer> res = new ArrayList<>();
-        // ArrayDeque<Integer> q = new ArrayDeque<>();
-        // int i = 0 ;
-        // for(; i < k ; i++){
-        //     while(!q.isEmpty() && arr[q.peekLast()]<=arr[i]){
-        //         q.removeLast();
-        //     }
-        //     q.addLast(i);
-        //     res.add(arr[q.peekFirst()]);
-        // }
-        // for(; i<n ; i++){
-        //     if(!q.isEmpty() && q.peek()==i-k){
-        //         q.removeFirst();
-        //     }
-        //     while(!q.isEmpty() && arr[q.peekLast()]<=arr[i]){
-        //         q.removeLast();
-        //     }
-        //     q.addLast(i);
-        //     res.add(arr[q.peekFirst()]);
-        // }
         
-        // return res;
-        ArrayList<Integer> res2 = new ArrayList<>();
-        int res[] = new int[arr.length-k+1];
-        ArrayDeque<Integer> q = new ArrayDeque<>();
-        int j = 0 ;
+       ArrayList<Integer> res = new ArrayList<Integer>(0);
+
+        // Declaring and Initializing an ArrayDeque
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+
+        // Creating a StringBuilder variable sb
+        StringBuilder sb = new StringBuilder();
+
         int i = 0;
-        for(;i<k;i++){
-            while(!q.isEmpty() && arr[q.peekLast()]<=arr[i]){
-                q.removeLast();
-            }
-            q.addLast(i);
-            res[j] = arr[q.peek()];
+
+        // adding only the maximum element index in the range
+        // 0 to k-1 and polling the remaining elements index
+        for (i = 0; i < k; i++) {
+            while (dq.isEmpty() == false && arr[i] >= arr[dq.peekLast()])
+                dq.pollLast();
+
+            dq.add(i);
         }
-        for(;i<arr.length;i++){
-          
-            if(!q.isEmpty() && q.peek()==i-k){
-                q.removeFirst();
-            }
-            while(!q.isEmpty() && arr[q.peekLast()]<=arr[i]){
-                q.removeLast();
-            }
-            q.addLast(i);
-            res[++j] = arr[q.peek()];
+
+        // adding only the maximum element index in the range
+        // interval k and polling the remaining elements index
+        for (; i < n; i++) {
+            // adding the maximum in interval k
+            // to the ArrayList
+            res.add(arr[dq.peek()]);
+
+            while (dq.isEmpty() == false && (dq.peekFirst() <= i - k))
+                dq.pollFirst();
+
+            while (dq.isEmpty() == false && (arr[i] >= arr[dq.peekLast()]))
+                dq.pollLast();
+
+            dq.add(i);
         }
-        
-        for(int l = 0 ; l < res.length ; l++){
-            res2.add(res[l]);
-        }
-        
-        return res2;
+
+        // adding the last element of the
+        // ArrayDeque to the ArrayList
+        res.add(arr[dq.peek()]);
+        dq.pollFirst();
+
+        // returning the ArrayList of maximum elements
+        // in subArrays of size k
+        return res;
     }
 }
