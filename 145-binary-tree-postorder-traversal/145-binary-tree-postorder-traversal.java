cff430d613d2d30198c9e5e90f1483f04271a237
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-08-23 11:46:01 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (16.26%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.java
@@ -16,37 +16,45 @@
 class Solution {
     List<Integer> res = new ArrayList<Integer>();
     public List<Integer> postorderTraversal(TreeNode root) {
-//         if(root==null)
-//             return res;
-//         postorderTraversal(root.left);
-//         postorderTraversal(root.right);
-//         res.add(root.val);
         
-//         return res;
-        Stack<TreeNode> s1 = new Stack<>();
-        Stack<TreeNode> s2 = new Stack<>();
-        if(root==null){
+        //Recursive method
+        
+        if(root==null)
             return res;
-        }
-        
-        s1.push(root);
-        
-        while(!s1.isEmpty()){
-            TreeNode pop = s1.pop();
-            s2.push(pop);
-            if(pop.left!=null){
-                s1.push(pop.left);
-            }
-            if(pop.right!=null){
-                s1.push(pop.right);
-            }
-        }
-        
-        while(!s2.isEmpty()){
-            res.add(s2.pop().val);
-        }
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        res.add(root.val);
         
         return res;
+        
+        
+        
+        //Without recursion using 2 stacks
+        
+//         Stack<TreeNode> s1 = new Stack<>();
+//         Stack<TreeNode> s2 = new Stack<>();
+//         if(root==null){
+//             return res;
+//         }
+        
+//         s1.push(root);
+        
+//         while(!s1.isEmpty()){
+//             TreeNode pop = s1.pop();
+//             s2.push(pop);
+//             if(pop.left!=null){
+//                 s1.push(pop.left);
+//             }
+//             if(pop.right!=null){
+//                 s1.push(pop.right);
+//             }
+//         }
+        
+//         while(!s2.isEmpty()){
+//             res.add(s2.pop().val);
+//         }
+        
+//         return res;
         
     }
 }
