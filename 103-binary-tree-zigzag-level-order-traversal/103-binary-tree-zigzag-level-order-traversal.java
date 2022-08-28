a8481b35a6fd884719fc422bfcf669789a8ddde0
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-08-28 09:04:23 +0530

    Time: 1 ms (96.34%), Space: 43.1 MB (27.03%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -38,73 +38,46 @@ class Solution {
 //         zigzag(root.left,ans,level+1);
 //         zigzag(root.right,ans,level+1);
 //     }
-   public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-if (root == null) return res;
-Queue<TreeNode> queue = new LinkedList<>();
-queue.add(root);
-boolean zigzag = false;
-while (!queue.isEmpty()) {
-    List<Integer> level = new ArrayList<>();
-    int cnt = queue.size();
-    for (int i = 0; i < cnt; i++) {
-        TreeNode node = queue.poll();
-        if (zigzag) {
-            level.add(0, node.val);
-        }
-        else {
-            level.add(node.val);
-        }
-        if (node.left != null) {
-            queue.add(node.left);
-        }
-        if (node.right != null) {
-            queue.add(node.right);
-        }
-    }
-    res.add(level);
-    zigzag = !zigzag;
-}
-return res;
-    }
     
-//     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-//        ITERATIVE.....
-//         Stack<TreeNode> s1 = new Stack<>();
-// 	    Stack<TreeNode> s2 = new Stack<>();
-// 	    List<List<Integer>> ans = new ArrayList<>();
-//         List<Integer> res;
-// 	    if(root==null){
-//             return ans;
-//         }
-// 	    s1.push(root);
+    
+    
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+     //  ITERATIVE.....
+        Stack<TreeNode> s1 = new Stack<>();
+	    Stack<TreeNode> s2 = new Stack<>();
+	    List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> res;
+	    if(root==null){
+            return ans;
+        }
+	    s1.push(root);
 	    
-// 	    while(!s1.isEmpty()||!s2.isEmpty()){
-//             res = new ArrayList<>();
-// 	        while(!s1.isEmpty()){
-// 	            TreeNode pop = s1.pop();
-// 	            res.add(pop.val);
-// 	            if(pop.left!=null)
-// 	                s2.push(pop.left);
-// 	            if(pop.right!=null)
-// 	                s2.push(pop.right);   
-// 	        }
-//             if(!res.isEmpty())
-//                 ans.add(res);
+	    while(!s1.isEmpty()||!s2.isEmpty()){
+            res = new ArrayList<>();
+	        while(!s1.isEmpty()){
+	            TreeNode pop = s1.pop();
+	            res.add(pop.val);
+	            if(pop.left!=null)
+	                s2.push(pop.left);
+	            if(pop.right!=null)
+	                s2.push(pop.right);   
+	        }
+            if(!res.isEmpty())
+                ans.add(res);
             
-//             res = new ArrayList<>();
-// 	        while(!s2.isEmpty()){
-// 	            TreeNode pop = s2.pop();
-// 	            res.add(pop.val);
-// 	            if(pop.right!=null)
-// 	                s1.push(pop.right);
-// 	            if(pop.left!=null)
-// 	                s1.push(pop.left);
-// 	        }
-//             if(!res.isEmpty())
-//                 ans.add(res);
-// 	    }
+            res = new ArrayList<>();
+	        while(!s2.isEmpty()){
+	            TreeNode pop = s2.pop();
+	            res.add(pop.val);
+	            if(pop.right!=null)
+	                s1.push(pop.right);
+	            if(pop.left!=null)
+	                s1.push(pop.left);
+	        }
+            if(!res.isEmpty())
+                ans.add(res);
+	    }
 	    
-// 	    return ans;
-//    }
+	    return ans;
+   }
 }
