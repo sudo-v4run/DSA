ee3bae500a46541d074c8aecd8536e0e87aeeebb
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-09-02 18:04:58 +0530

    Time: 0 ms (100.00%), Space: 43 MB (84.50%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,12 +14,39 @@
  * }
  */
 class Solution {
-    TreeNode pre = null;
+    // TreeNode pre = null;
+    // public boolean isValidBST(TreeNode root) {
+    //     if (root == null) return true;
+    //     if (!isValidBST(root.left)) return false;
+    //     if (pre != null && pre.val >= root.val) return false;
+    //     pre = root;
+    //     return isValidBST(root.right);
+    // }
+    
+    static  TreeNode prev;
+     
+    static boolean isBSTUtil(TreeNode root)
+    {
+        // traverse the tree in inorder fashion and
+        // keep track of prev node
+        if (root != null)
+        {
+            if (!isBSTUtil(root.left))
+                return false;
+     
+            // Allows only distinct valued nodes
+            if (prev != null && root.val <= prev.val)
+                return false;
+     
+            prev = root;
+     
+            return isBSTUtil(root.right);
+        }
+        return true;
+    }
+     
     public boolean isValidBST(TreeNode root) {
-        if (root == null) return true;
-        if (!isValidBST(root.left)) return false;
-        if (pre != null && pre.val >= root.val) return false;
-        pre = root;
-        return isValidBST(root.right);
+        prev=null;
+        return isBSTUtil(root);
     }
 }
