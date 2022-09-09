34ba70beacd6f92c902da225c7b81bfef2fe7a4c
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-09-10 00:34:31 +0530

    Added solution - LeetHub

--- Jump Game - GFG/jump-game.java
+++ Jump Game - GFG/jump-game.java
@@ -28,15 +28,27 @@ class GFG {
 //User function Template for Java
 
 class Solution {
-    static int canReach(int[] arr, int N) {
-        int goal = arr.length-1;
+    static int canReach(int[] arr, int n) {
+        // int goal = n-1;
         
-        for(int i = goal ; i>=0 ; i--){
-            if(i+arr[i]>=goal){
-                goal = i;
-            }
+        // for(int i = n-1 ; i>=0 ; i--){
+        //     if(i+arr[i]>=goal){
+        //         goal = i;
+        //     }
+        // }
+        
+        // return (goal==0)?1:0;
+        
+        
+        int reach = 0;
+        
+        for(int i = 0 ; i < n ; i++){
+            if(reach<i)
+                return 0;
+            
+            reach = Math.max(reach,i+arr[i]);
         }
         
-        return (goal==0)?1:0;
+        return 1;
     }
 };
