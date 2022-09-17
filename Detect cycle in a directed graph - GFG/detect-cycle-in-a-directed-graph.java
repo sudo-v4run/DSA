ab5331dd00d83a34a891890debcc8fb97eaa1acc
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-09-17 06:52:03 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -34,78 +34,78 @@ class DriverClass {
 class Solution {
     
     public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
-        int cnt = 0 ; 
         
         //............BFS-Kahns Algo............
         
-        // int indeg[] = new int [V];
-        // Queue<Integer> q = new LinkedList<>();
+        int cnt = 0 ; 
+        int indeg[] = new int [V];
+        Queue<Integer> q = new LinkedList<>();
         
-        // findIG(adj,indeg);
+        findIG(adj,indeg);
         
-        // for(int i = 0 ; i < V ; i++){
-        //     if(indeg[i]==0){
-        //         q.offer(i);
-        //         cnt++;
-        //     }
-        // }
+        for(int i = 0 ; i < V ; i++){
+            if(indeg[i]==0){
+                q.offer(i);
+                cnt++;
+            }
+        }
         
-        // while(!q.isEmpty()){
-        //     int pop = q.poll();
+        while(!q.isEmpty()){
+            int pop = q.poll();
             
-        //     for(int nei : adj.get(pop)){
-        //         indeg[nei]--;
-        //         if(indeg[nei]==0){
-        //             q.offer(nei);
-        //             cnt++;
-        //         }
-        //     }
-        // }
+            for(int nei : adj.get(pop)){
+                indeg[nei]--;
+                if(indeg[nei]==0){
+                    q.offer(nei);
+                    cnt++;
+                }
+            }
+        }
         
-        // if(cnt<V){
-        //     return true;
-        // }
+        if(cnt<V){
+            return true;
+        }
         
-        // return false;
+        return false;
      
         
         //.............DFS...........................
         
-        Stack<Integer> st = new Stack<>();
-        int vis[] = new int [V];
-        int pathVis[] = new int[V];
+        // Stack<Integer> st = new Stack<>();
+        // int vis[] = new int [V];
+        // int pathVis[] = new int[V];
         
-        for(int i =  0 ; i < V ; i++){
-            if(vis[i]!=1){
-                if(dfs(i,vis,adj,cnt,pathVis))
-                    return true;
-            }
-        }
+        // for(int i =  0 ; i < V ; i++){
+        //     if(vis[i]!=1){
+        //         if(dfs(i,vis,adj,cnt,pathVis))
+        //             return true;
+        //     }
+        // }
         
-        return false;
+        // return false;
       
         
         
     }
     
-    static boolean dfs(int s , int vis[],ArrayList<ArrayList<Integer>> adj,int cnt,int pathVis[] ){
-        vis[s] = 1;
-        pathVis[s] = 1;
-        for(int nei : adj.get(s) ){
-            if(vis[nei]!=1){
-                if(dfs(nei,vis,adj,cnt,pathVis)){
-                    return true;
-                }
-            }else{
-                if(pathVis[nei]==1){
-                    return true;
-                }
-            }
-        }
-        pathVis[s] = 0;
-        return false;
+    // static boolean dfs(int s , int vis[],ArrayList<ArrayList<Integer>> adj,int cnt,int pathVis[] ){
+    //     vis[s] = 1;
+    //     pathVis[s] = 1;
+    //     for(int nei : adj.get(s) ){
+    //         if(vis[nei]!=1){
+    //             if(dfs(nei,vis,adj,cnt,pathVis)){
+    //                 return true;
+    //             }
+    //         }else{
+    //             if(pathVis[nei]==1){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     pathVis[s] = 0;
+    //     return false;
         
-    }
+    // }
     
     static void findIG(ArrayList<ArrayList<Integer>> adj , int indeg[]){
         for(int i = 0 ; i < adj.size() ; i++){
