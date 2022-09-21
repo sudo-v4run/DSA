2132debb449876a6838840df9e5e0e90843427ff
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-09-21 23:51:23 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.java
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.java
@@ -45,7 +45,6 @@ class DriverClass
 }
 // } Driver Code Ends
 
-
 class Solution
 {
     static class Pair{
@@ -60,46 +59,104 @@ class Solution
     {
         // kruskal ...
         
+        TreeMap<Integer,ArrayList<Pair> > tm = new TreeMap<>();
+        int par[] = new int[V];
+        for(int i = 0 ; i < V ; i++){
+            par[i] = i;
+        }
+        
+        for(int i = 0 ; i < V ; i++){
+            ArrayList<ArrayList<Integer>> nei = adj.get(i);
+            for(ArrayList<Integer> a : nei){
+                ArrayList<Pair> al = tm.get(a.get(1));
+                if (al == null) {
+                    al = new ArrayList<Pair>();
+                    tm.put(a.get(1), al);
+                }
+                al.add(new Pair(i,a.get(0)));
+            }
+        }
+        
+        int ans = 0;
+      
+        
+        for (Map.Entry<Integer,ArrayList<Pair>>entry : tm.entrySet()){
+            ArrayList<Pair> list = entry.getValue();
+            for(Pair pr : list){
+                Pair e = pr;
+                if(find(par,e.v)!=find(par,e.w)){
+                    ans+=entry.getKey();
+                    unionSet(par,e.v,e.w);
+                }
+            }
+            
+        }
+        
+        return ans;
+        
+        
+        
         
         // Prims ...
         
-        int key[] = new int[V];
-        boolean mst[] = new boolean[V];
-        //int parent[] = new int[V];
-        int ans = 0;
+        // int key[] = new int[V];
+        // boolean mst[] = new boolean[V];
+        // //int parent[] = new int[V];
+        // int ans = 0;
         
-        for(int i = 0 ; i < V ;i++){
-            key[i] = 10000000;
-            mst[i] = false;
-            //parent[i] = -1;
-        }
-        key[0] = 0;
+        // for(int i = 0 ; i < V ;i++){
+        //     key[i] = 10000000;
+        //     mst[i] = false;
+        //     //parent[i] = -1;
+        // }
+        // key[0] = 0;
         
-        PriorityQueue<Pair> q = new PriorityQueue<>((a,b)->{return a.w-b.w;});
+        // PriorityQueue<Pair> q = new PriorityQueue<>((a,b)->{return a.w-b.w;});
         
-        q.offer(new Pair(0,key[0]));
+        // q.offer(new Pair(0,key[0]));
         
-        while(!q.isEmpty()){
-            Pair cur = q.poll();
+        // while(!q.isEmpty()){
+        //     Pair cur = q.poll();
             
-            mst[cur.v] = true;
+        //     mst[cur.v] = true;
             
-            ArrayList<ArrayList<Integer>> nei = adj.get(cur.v);
+        //     ArrayList<ArrayList<Integer>> nei = adj.get(cur.v);
             
-            for(ArrayList<Integer> a : nei){
-                int vr = a.get(0);
-                int wt = a.get(1);
+        //     for(ArrayList<Integer> a : nei){
+        //         int vr = a.get(0);
+        //         int wt = a.get(1);
                 
-                if(mst[vr]==false && key[vr]>wt){
-                    key[vr]=wt;
-                   // parent[vr]=cur.v;
-                    q.offer(new Pair(vr,key[vr]));
-                }
-            }
-        }
-        for(int i=0; i < V ; i++){
-            ans+=key[i];
-        }
-        return ans;
+        //         if(mst[vr]==false && key[vr]>wt){
+        //             key[vr]=wt;
+        //           // parent[vr]=cur.v;
+        //             q.offer(new Pair(vr,key[vr]));
+        //         }
+        //     }
+        // }
+        // for(int i=0; i < V ; i++){
+        //     ans+=key[i];
+        // }
+        // return ans;
     }
+    
+    
+    static int find(int parent[],int x)
+    {
+          if(parent[x]!=x){
+              parent[x] = find(parent,parent[x]);
+          }
+          
+          return parent[x];
+	}
+	static void unionSet(int parent[],int x,int z)
+    {
+         int parX = find(parent,x);
+         int parZ = find(parent,z);
+         
+         if(parX==parZ){
+             return;
+         }
+         
+         parent[parX] = parZ;
+	}
 }
