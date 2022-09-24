0fc22a23033d56d347f0211f9f0d551b9757e7c7
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-09-25 00:03:07 +0530

    Added solution - LeetHub

--- Sum of Series - GFG/sum-of-series.java
+++ Sum of Series - GFG/sum-of-series.java
@@ -22,11 +22,7 @@ class GFG {
 
 // } Driver Code Ends
 
-
-// User function Template for Java
-
 class Solution {
-    // function to return sum of  1, 2, ... n
     long seriesSum(int n) {
         return (long)n*(n+1)/2;
     }
