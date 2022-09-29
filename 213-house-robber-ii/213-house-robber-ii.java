37c88e61e3a229d85918d774f62bef65f5153c2a
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-09-29 23:33:56 +0530

    Time: 0 ms (100.00%), Space: 42 MB (13.97%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -5,25 +5,25 @@ class Solution {
         if(n==1)
             return nums[0];
         
-        int temp1[] = new int[n-1];
-        int temp2[] = new int[n-1];
-        int k=0 , l=0;
-        for(int i = 0 ; i < n ; i++){
-            if(i!=0){
-                temp1[k++] = nums[i]; 
-            }
-            if(i!=(n-1)){
-                temp2[l++] = nums[i];
-            }
-        }
+        // int temp1[] = new int[n-1];
+        // int temp2[] = new int[n-1];
+        // int k=0 , l=0;
+        // for(int i = 0 ; i < n ; i++){
+        //     if(i!=0){
+        //         temp1[k++] = nums[i]; 
+        //     }
+        //     if(i!=(n-1)){
+        //         temp2[l++] = nums[i];
+        //     }
+        // }
         
-        return Math.max(hr(temp1),hr(temp2));
+        return Math.max(hr(1,n,nums),hr(0,n-1,nums));
     }
-    public int hr(int arr[]){
-        int prev1 = arr[0];
+    public int hr(int low , int hi , int arr[]){
+        int prev1 = arr[low];
         int prev2 = 0;
         
-        for(int i = 1 ; i < arr.length ; i++){
+        for(int i = low+1 ; i < hi ; i++){
             int takeThis = arr[i];
             if(i>1)
                 takeThis += prev2;
