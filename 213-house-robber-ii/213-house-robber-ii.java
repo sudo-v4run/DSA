6fe694ecd6df96ade365f3d6dc6d3e4249e17387
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-09-29 23:34:08 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (31.42%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -5,18 +5,6 @@ class Solution {
         if(n==1)
             return nums[0];
         
-        // int temp1[] = new int[n-1];
-        // int temp2[] = new int[n-1];
-        // int k=0 , l=0;
-        // for(int i = 0 ; i < n ; i++){
-        //     if(i!=0){
-        //         temp1[k++] = nums[i]; 
-        //     }
-        //     if(i!=(n-1)){
-        //         temp2[l++] = nums[i];
-        //     }
-        // }
-        
         return Math.max(hr(1,n,nums),hr(0,n-1,nums));
     }
     public int hr(int low , int hi , int arr[]){
