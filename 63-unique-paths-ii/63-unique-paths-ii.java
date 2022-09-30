874ca82b484bc52dfc8d70b65281a280ea0bf289
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-09-30 15:44:22 +0530

    Time: 1 ms (71.04%), Space: 42.4 MB (40.60%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -7,13 +7,36 @@ class Solution {
         
         // Momoization (Top - Down) ...
         
+        // int dp[][] = new int[m][n];
+        // for(int[] row : dp){
+        //     Arrays.fill(row,-1);
+        // }
+        // return up2(m-1,n-1,arr,dp);
+        
+        
+        // Tabulation (Buttom - Up) ...
+        
         int dp[][] = new int[m][n];
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
+        
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(arr[i][j]==1)
+                    dp[i][j]=0;
+                else if(i==0 && j==0)
+                    dp[i][j] = 1;
+                else{
+                    int up = 0;
+                    int left = 0;
+                    if(i>0)
+                        up+=dp[i-1][j];
+                    if(j>0)
+                        left+=dp[i][j-1];
+                    dp[i][j] = up+left;
+                }
+            }
         }
-        return up2(m-1,n-1,arr,dp);
         
-        
+        return dp[m-1][n-1];
         
     }
     public static int up2(int i , int j , int arr[][],int dp[][]){
