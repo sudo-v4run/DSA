41dd11acb9d8e7c784f715fe02596b76b5f1d36b
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-09-30 15:37:48 +0530

    Time: 1 ms (71.04%), Space: 42.3 MB (49.55%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -0,0 +1,61 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] arr) {
+        
+        int m = arr.length;
+        int n = arr[0].length;
+        
+        
+        // Momoization (Top - Down) ...
+        
+        int dp[][] = new int[m][n];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
+        return up2(m-1,n-1,arr,dp);
+        
+        
+        
+    }
+    public static int up2(int i , int j , int arr[][],int dp[][]){
+        
+        if(i>=0 && j>=0 && arr[i][j]==1)
+            return 0;
+        if(i==0&&j==0){
+            return 1;
+        }
+        if(i<0||j<0)
+            return 0;
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        int up = up2(i-1,j,arr,dp);
+        int left = up2(i,j-1,arr,dp);
+        
+        return dp[i][j] = up+left;
+        
+        
+        
+        
+        
+        
+        
+        
+        // Only Recursion ...
+        
+//         if(i>=0 && j>=0 && arr[i][j]==1)
+//             return 0;
+//         if(i==0&&j==0){
+//             return 1;
+//         }
+//         if(i<0||j<0)
+//             return 0;
+        
+        
+        
+//         int up = up2(i-1,j,arr);
+//         int left = up2(i,j-1,arr);
+        
+//         return up+left;
+    }
+}
