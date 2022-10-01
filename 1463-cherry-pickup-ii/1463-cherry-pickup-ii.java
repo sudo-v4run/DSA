50e3c27649a28ffff772f620235d247276716cf0
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-01 11:35:03 +0530

    Time: 94 ms (14.82%), Space: 49.3 MB (25.52%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -4,13 +4,63 @@ class Solution {
         int n = arr[0].length;
         
         // Memoization ...
+        
+        // int dp[][][] = new int[m][n][n];
+        // for(int[][] row : dp){
+        //     for(int[] col : row){
+        //         Arrays.fill(col,-1);
+        //     }
+        // }
+        // return ch2(0,0,n-1,m,n,arr,dp);
+        
+        
+        
+        
+        
+        
+        // Tabulation ...
         int dp[][][] = new int[m][n][n];
-        for(int[][] row : dp){
-            for(int[] col : row){
-                Arrays.fill(col,-1);
+        
+        for(int j1 = 0 ; j1 < n ; j1++){
+            for(int j2=0 ; j2< n ; j2++){
+                if(j1==j2)
+                    dp[m-1][j1][j2] = arr[m-1][j1];
+                else
+                    dp[m-1][j1][j2] = arr[m-1][j1]+arr[m-1][j2];
             }
         }
-        return ch2(0,0,n-1,m,n,arr,dp);
+        
+        for(int i = m-2 ; i>=0 ; i--){
+            for(int j1 = 0 ; j1< n ; j1++){
+                for(int j2 = 0 ; j2 < n ; j2++){
+                    
+                    
+                    int maxi = -100000;
+                    for(int dj1 = -1 ; dj1 <= +1 ; dj1++){
+                        for(int dj2 = -1 ; dj2<=+1 ; dj2++){
+                            int value = 0;
+                            if(j1==j2){
+                                value = arr[i][j1];
+                            }else{
+                                value = arr[i][j1]+arr[i][j2];
+                            }
+                            if(j1+dj1>=0 && j1+dj1<=n-1 && j2+dj2>=0 && j2+dj2<=n-1)
+                                value += dp[i+1][j1+dj1][j2+dj2];
+                            else
+                                value = -1000000;
+                            maxi = Math.max(maxi,value);
+                        }
+                    }
+
+                    dp[i][j1][j2] = maxi;
+                    
+                }
+            }
+        }
+        
+        return dp[0][0][n-1];
+        
+        
     }
     public static int ch2(int i , int j1 , int j2 ,int m ,int n, int arr[][], int dp[][][]){
         
