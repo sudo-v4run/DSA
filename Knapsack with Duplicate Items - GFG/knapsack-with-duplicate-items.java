bb14eae39c55273a02bf67a49449d2a907a903c7
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-04 08:41:21 +0530

    Added solution - LeetHub

---
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String line1[] = in.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(line1[0]);
+            int W = Integer.parseInt(line1[1]);
+            String line2[] = in.readLine().trim().split("\\s+");
+            int val[] = new int[N];
+            for(int i = 0;i < N;i++)
+                val[i] = Integer.parseInt(line2[i]);
+            String line3[] = in.readLine().trim().split("\\s+");
+            int wt[] = new int[N];
+            for(int i = 0;i < N;i++)
+                wt[i] = Integer.parseInt(line3[i]);
+                
+            Solution ob = new Solution();
+            System.out.println(ob.knapSack(N, W, val, wt));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution{
+    static int knapSack(int n, int W, int val[], int wt[])
+    {
+        // Memoization .. (Top - Down) ...
+        
+        int dp[][] = new int[n][W+1];
+        for(int[] row :dp){
+            Arrays.fill(row,-1);
+        }
+        return uks(n-1,W,val,wt,dp);
+    }
+    static int uks(int item,int W,int val[],int wt[],int dp[][]){
+        
+        if(item==0){
+            if(wt[item]<=W){
+                return (W/wt[item])*val[item];
+            }
+            return 0;
+        }
+        
+        if(dp[item][W] != -1)
+            return dp[item][W];
+        
+        int pick = 0;
+        if(wt[item]<=W){
+            pick += val[item] + uks(item,W-wt[item],val,wt,dp);
+        }
+        int notPick = 0 + uks(item-1,W,val,wt,dp);
+        
+        return dp[item][W] =  Math.max(pick,notPick);
+        
+        
+        
+        
+        // Recursion ...
+        // if(item==0){
+        //     if(wt[item]<=W){
+        //         return (W/wt[item])*val[item];
+        //     }
+        //     return 0;
+        // }
+        // int pick = 0;
+        // if(wt[item]<=W){
+        //     pick += val[item] + uks(item,W-wt[item],val,wt);
+        // }
+        // int notPick = 0 + uks(item-1,W,val,wt);
+        
+        // return Math.max(pick,notPick);
+    }
+}
