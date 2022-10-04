953fb96383ad1719205bac2ef574ecf9def4f709
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-04 08:25:31 +0530

    Time: 622 ms (30.76%), Space: 41.7 MB (81.90%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,8 +1,32 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        int n = nums.length;
-        return countPartitions(n,target,nums);
+    public int findTargetSumWays(int[] arr, int target) {
+        int n = arr.length;
+        //return countPartitions(n,target,arr);
+        
+        return countTargetSum(n-1,target,arr);
+        
+        
+        
+        
+        
     }
+    public static int countTargetSum(int index,int target ,int arr[]){
+        if(index==0){
+            if(target==0 && arr[0]==0)
+                return 2;
+            if(arr[0]==target || arr[0] == -1*(target) ){
+                return 1;
+            }
+            return 0;
+        }
+        int plus = countTargetSum(index-1,target+arr[index],arr);
+        int minus = countTargetSum(index-1,target-arr[index],arr);
+        
+        return plus+minus;
+    }
+    
+    
+    
     public static int countPartitions(int n, int d, int[] arr) {
         int totSum = 0;
         for(int i : arr)
