8b5ee2396c203139ac20589a515c69f5f0e73fa3
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-05 15:12:38 +0530

    Time: 29 ms (35.63%), Space: 46.3 MB (63.00%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,15 +3,42 @@ class Solution {
         int n1 = s1.length();
         int n2 = s2.length();
         
-        // Memoization ...(Top - Down) ...
-        int dp[][] = new int[n1][n2];
         
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        // Tabulation ... (Buttom-Up) ...
+        
+        int dp[][] = new int[n1+1][n2+1];
+        
+        for(int j = 0 ; j <= n2 ; j++){
+            dp[0][j] = 0;
         }
         
-        return lcs(n1-1,n2-1,s1,s2,dp);
+        for(int i = 0 ; i <= n1 ; i++){
+            dp[i][0] = 0;
+        }
+        
+        for(int i = 1 ; i <= n1 ; i++){
+            for(int j = 1 ; j <= n2 ;j++){
+                 if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        
+        return dp[n1][n2];
+        
+        // Memoization ...(Top - Down) ...
+//         int dp[][] = new int[n1][n2];
+        
+//         for(int[] row:dp){
+//             Arrays.fill(row,-1);
+//         }
+        
+//         return lcs(n1-1,n2-1,s1,s2,dp);
     }
+    
+    
     public static int lcs(int n1,int n2,String s1,String s2,int dp[][]){
         
         if(n1<0||n2<0){
