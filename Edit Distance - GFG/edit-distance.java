bb02265c6b77a40e26fa8c45c5c9b21fb6bed333
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-08 12:24:12 +0530

    Added solution - LeetHub

---
+++ Edit Distance - GFG/edit-distance.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s1 = br.readLine();
+            String[] S = s1.split(" ");
+            String s = S[0];
+            String t = S[1];
+            Solution ob = new Solution();
+            int ans = ob.editDistance(s, t);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int editDistance(String s1, String s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        
+        // Memoization ..(Top - Down)...
+        int dp[][] = new int[n1][n2];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        
+        return nop(n1-1,n2-1,s1,s2,dp);
+    }
+    public static int nop(int index1, int index2 , String s1 , String s2,int dp[][]){
+        if(index1<0 && index2<0)
+            return 0;
+        if(index1<0){
+            return index2+1;
+        }
+        if(index2<0){
+            return index1+1;
+        }
+        
+        if(dp[index1][index2] != -1)
+            return dp[index1][index2];
+        
+        
+        if(s1.charAt(index1)==s2.charAt(index2)){
+            return dp[index1][index2] = 0 + nop(index1-1 , index2 -1 ,s1 , s2 ,dp);
+        }
+        return dp[index1][index2] = Math.min( 1+nop(index1,index2-1,s1,s2,dp), //insert
+                            Math.min(1+nop(index1-1,index2,s1,s2,dp) , //delete
+                            1+nop(index1-1,index2-1,s1,s2,dp))); //replace
+    }
+}
