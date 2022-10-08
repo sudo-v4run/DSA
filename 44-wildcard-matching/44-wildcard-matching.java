16a4b63d3ce88a86b1772d9837eeaa8959f7e790
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-08 13:39:57 +0530

    Time: 33 ms (72.50%), Space: 44.7 MB (76.74%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -3,14 +3,48 @@ class Solution {
         int n1 = s1.length();
         int n2 = s2.length();
         
+        // Tabulation (Button-Up) ...
+        int dp[][] = new int[n1+1][n2+1];
         
-        // Memoization (Top - Down) ..
-        int dp[][] = new int[n1][n2];
-        for(int[] row: dp){
-            Arrays.fill(row,-1);
+        dp[0][0] = 1;
+        
+        for(int index1 = 1 ; index1 <= n1 ; index1++){
+            int flag = 1;
+            for(int i = 1 ; i<= index1 ; i++){
+                if(s1.charAt(i-1)!='*'){
+                    flag = 0;
+                    break;
+                }
+            }
+            dp[index1][0] = flag;
         }
         
-        return wcm(n1-1,n2-1,s1,s2,dp)==1?true:false;
+        for(int index1 = 1 ; index1 <= n1 ; index1++){
+            for(int index2 = 1 ; index2 <= n2 ; index2++){
+                if(s1.charAt(index1-1)==s2.charAt(index2-1) || s1.charAt(index1-1)=='?'){
+                    dp[index1][index2] = dp[index1-1][index2-1];
+                }
+                else if(s1.charAt(index1-1)=='*'){
+                    if(dp[index1-1][index2]==1 || dp[index1][index2-1]==1 )
+                        dp[index1][index2] = 1;
+                    else
+                        dp[index1][index2] = 0;
+                }
+                else
+                    dp[index1][index2] = 0;
+            }
+        }
+        
+        return dp[n1][n2] == 1? true:false;
+        
+        
+        // Memoization (Top - Down) ..
+//         int dp[][] = new int[n1][n2];
+//         for(int[] row: dp){
+//             Arrays.fill(row,-1);
+//         }
+        
+//         return wcm(n1-1,n2-1,s1,s2,dp)==1?true:false;
     }
     public static int wcm(int index1 , int index2 , String s1 , String s2,int dp[][]){
         if(index1<0 && index2<0){
@@ -33,9 +67,7 @@ class Solution {
             return  dp[index1][index2] = wcm(index1-1,index2-1,s1,s2,dp);
         }
         if(s1.charAt(index1)=='*'){
-            if(wcm(index1-1,index2 , s1, s2,dp)==1)
-                return dp[index1][index2] = 1;
-            if(wcm(index1,index2-1,s1,s2,dp)==1)
+            if(wcm(index1-1,index2 , s1, s2,dp)==1 || wcm(index1,index2-1,s1,s2,dp)==1 )
                 return dp[index1][index2] = 1;
             return dp[index1][index2] = 0;
         }
