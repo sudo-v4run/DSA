1a255ce5981cb698e0336c940436ace5d646c491
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-08 13:48:22 +0530

    Time: 47 ms (48.61%), Space: 54.4 MB (16.17%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.java
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -3,10 +3,13 @@ class Solution {
         int n1 = s1.length();
         int n2 = s2.length();
         
-        // Tabulation (Button-Up) ...
-        int dp[][] = new int[n1+1][n2+1];
         
-        dp[0][0] = 1;
+        // Tabulation (Button-Up) ...
+        
+        int cur[] = new int[n2+1];
+        int prev[] = new int[n2+1];
+        
+        prev[0] = 1;
         
         for(int index1 = 1 ; index1 <= n1 ; index1++){
             int flag = 1;
@@ -16,26 +19,62 @@ class Solution {
                     break;
                 }
             }
-            dp[index1][0] = flag;
-        }
-        
-        for(int index1 = 1 ; index1 <= n1 ; index1++){
+            cur[0] = flag;
             for(int index2 = 1 ; index2 <= n2 ; index2++){
                 if(s1.charAt(index1-1)==s2.charAt(index2-1) || s1.charAt(index1-1)=='?'){
-                    dp[index1][index2] = dp[index1-1][index2-1];
+                    cur[index2] = prev[index2-1];
                 }
                 else if(s1.charAt(index1-1)=='*'){
-                    if(dp[index1-1][index2]==1 || dp[index1][index2-1]==1 )
-                        dp[index1][index2] = 1;
+                    if(prev[index2]==1 || cur[index2-1]==1 )
+                        cur[index2] = 1;
                     else
-                        dp[index1][index2] = 0;
+                        cur[index2] = 0;
                 }
                 else
-                    dp[index1][index2] = 0;
+                    cur[index2] = 0;
             }
+            prev = cur.clone();
         }
         
-        return dp[n1][n2] == 1? true:false;
+        return prev[n2] == 1? true:false;
+        
+        
+        
+        
+        // Tabulation (Button-Up) ...
+        
+//         int dp[][] = new int[n1+1][n2+1];
+        
+//         dp[0][0] = 1;
+        
+//         for(int index1 = 1 ; index1 <= n1 ; index1++){
+//             int flag = 1;
+//             for(int i = 1 ; i<= index1 ; i++){
+//                 if(s1.charAt(i-1)!='*'){
+//                     flag = 0;
+//                     break;
+//                 }
+//             }
+//             dp[index1][0] = flag;
+//         }
+        
+//         for(int index1 = 1 ; index1 <= n1 ; index1++){
+//             for(int index2 = 1 ; index2 <= n2 ; index2++){
+//                 if(s1.charAt(index1-1)==s2.charAt(index2-1) || s1.charAt(index1-1)=='?'){
+//                     dp[index1][index2] = dp[index1-1][index2-1];
+//                 }
+//                 else if(s1.charAt(index1-1)=='*'){
+//                     if(dp[index1-1][index2]==1 || dp[index1][index2-1]==1 )
+//                         dp[index1][index2] = 1;
+//                     else
+//                         dp[index1][index2] = 0;
+//                 }
+//                 else
+//                     dp[index1][index2] = 0;
+//             }
+//         }
+        
+//         return dp[n1][n2] == 1? true:false;
         
         
         // Memoization (Top - Down) ..
