94368983eee2e022869bf3d910bb560203e71d60
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-08 12:31:41 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.java
+++ Edit Distance - GFG/edit-distance.java
@@ -27,13 +27,43 @@ class Solution {
         int n1 = s1.length();
         int n2 = s2.length();
         
-        // Memoization ..(Top - Down)...
-        int dp[][] = new int[n1][n2];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        // Tabulation .. (Buttom - Up) ...
+        
+        int dp[][] = new int[n1+1][n2+1];
+        
+        dp[0][0] = 0;
+
+        for(int index1 = 1 ; index1 <= n1 ; index1++ ){
+            dp[index1][0] = index1;
+        }
+        for(int index2 = 1 ; index2 <= n2 ; index2++ ){
+            dp[0][index2] = index2;
         }
         
-        return nop(n1-1,n2-1,s1,s2,dp);
+        
+        for(int index1 = 1 ; index1 <=n1 ; index1++){
+            for(int index2 = 1 ; index2 <= n2 ; index2++){
+                if(s1.charAt(index1-1)==s2.charAt(index2-1)){
+                    dp[index1][index2] = 0 + dp[index1-1 ][ index2 -1 ];
+                }
+                else
+                    dp[index1][index2] = Math.min( 1+dp[index1][index2-1], //insert
+                                            Math.min(1+dp[index1-1][index2] , //delete
+                                            1+dp[index1-1][index2-1])); //replace
+            }
+        }
+        
+        return dp[n1][n2];
+        
+        
+        // Memoization ..(Top - Down)...
+        
+        // int dp[][] = new int[n1][n2];
+        // for(int[] row:dp){
+        //     Arrays.fill(row,-1);
+        // }
+        
+        // return nop(n1-1,n2-1,s1,s2,dp);
     }
     public static int nop(int index1, int index2 , String s1 , String s2,int dp[][]){
         if(index1<0 && index2<0)
