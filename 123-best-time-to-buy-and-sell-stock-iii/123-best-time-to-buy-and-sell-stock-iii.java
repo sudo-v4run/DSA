a096ff2be17d60643fbf5ff14a2041178734d7fe
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-11 21:10:36 +0530

    Time: 123 ms (32.77%), Space: 124.9 MB (22.34%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,18 +1,44 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int cap = 2;
+        // int cap = 2;
         
-        // Memoization..(Top-Down)...
         
-        int dp[][][] = new int[n][2][3];
-        for(int[][] ar1 : dp){
-            for(int[] ar2 : ar1){
-                Arrays.fill(ar2,-1);
+        // Tabulation..(Buttom-Up)...
+        int limit = 2;
+        
+        int dp[][][] = new int[n+1][2][3];
+        
+        for(int index = n-1; index>=0 ; index--){
+            for(int canBuy = 0 ; canBuy<=1 ; canBuy++){
+                for(int cap = 1 ; cap<=2 ; cap++){
+                    if(canBuy==1)
+                        dp[index][canBuy][cap] = Math.max(dp[index+1][0][cap]-prices[index],
+                                                dp[index+1][1][cap]-0 );
+                    else
+                        dp[index][canBuy][cap] = Math.max(dp[index+1][1][cap-1]+prices[index],
+                                                dp[index+1][0][cap]+0 );
+                }
             }
         }
         
-        return bss3(0,1,cap,prices,dp);
+        
+        return dp[0][1][2];
+        
+        
+        
+        
+        
+        // Memoization..(Top-Down)...
+        
+//         int dp[][][] = new int[n][2][3];
+//         for(int[][] ar1 : dp){
+//             for(int[] ar2 : ar1){
+//                 Arrays.fill(ar2,-1);
+//             }
+//         }
+        
+//         return bss3(0,1,cap,prices,dp);
     }
     public static int bss3(int index,int canBuy, int cap , int prices[],int dp[][][]){
         
