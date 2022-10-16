b4bef1652f88ca7e749c2dddd4697aeef7890d09
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-16 09:47:43 +0530

    Time: 40 ms (98.05%), Space: 42.3 MB (82.97%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.java
+++ 312-burst-balloons/312-burst-balloons.java
@@ -8,14 +8,38 @@ class Solution {
             arr[i] = nums[i-1];
         }
         
+        //Tabulation....
+        
+        int dp[][] = new int[n+2][n+2];
+        
+        for(int i = n ; i >= 1 ; i--){
+            for(int j = 1 ; j <= n ; j++){
+                
+                if(i>j)
+                    continue;               
+                
+                int maxi = Integer.MIN_VALUE;
+        
+                for(int index = i ; index<=j ; index++){
+                    int coins = arr[index]*arr[i-1]*arr[j+1]+
+                                    dp[i][index-1]+dp[index+1][j];
+                    maxi = Math.max(maxi,coins);
+                }
+
+                dp[i][j] = maxi;
+            }
+        }
+        
+        return dp[1][n];
+        
         
         //Memoization...
         
-        int dp[][] = new int[n+2][n+2];
-        for(int[] row:dp)
-            Arrays.fill(row,-1);
+//         int dp[][] = new int[n+2][n+2];
+//         for(int[] row:dp)
+//             Arrays.fill(row,-1);
         
-        return fbb(1,n,arr,dp);
+//         return fbb(1,n,arr,dp);
     }
     public static int fbb(int i , int j , int arr[],int dp[][]){
         
