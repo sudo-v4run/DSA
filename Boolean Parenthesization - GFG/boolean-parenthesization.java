74e884111422467ca027fae0fc04ab4c7bc009b6
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-17 17:59:52 +0530

    Added solution - LeetHub

--- Boolean Parenthesization - GFG/boolean-parenthesization.java
+++ Boolean Parenthesization - GFG/boolean-parenthesization.java
@@ -25,16 +25,74 @@ class Solution{
     
     static int countWays(int n, String s){
         
-        
-        //Memoization...
+        // Tabulation ...
         
         int dp[][][] = new int[n][n][2];
-        for(int arr1[][]:dp){
-            for(int arr2[] : arr1){
-                Arrays.fill(arr2,-1);
+        
+        for(int i = n-1 ; i >= 0 ; i--){
+            for(int j = 0 ; j < n ; j++){
+                for(int isTrue = 0 ; isTrue <= 1 ; isTrue++){
+                    
+                    if(i==j){
+                        if(s.charAt(i)=='T'){
+                            dp[i][i][1] = 1;
+                        }
+                        if(s.charAt(i)=='F'){
+                            dp[i][i][0] = 1;
+                        }
+                    }else{
+                    
+                        int ways = 0;
+            
+                        for(int index = i+1 ; index <= j-1 ; index+=2){
+                            
+                            int lT = dp[i][index-1][1];
+                            int lF = dp[i][index-1][0];
+                            int rT = dp[index+1][j][1];
+                            int rF = dp[index+1][j][0];
+                            
+                            if(s.charAt(index)=='&'){
+                                if(isTrue==1){
+                                    ways+=(lT*rT);
+                                }else{
+                                    ways+=(lT*rF+lF*rT+lF*rF);
+                                }
+                            }else if(s.charAt(index)=='|'){
+                                if(isTrue==1){
+                                    ways+=(lT*rF+lF*rT+lT*rT);
+                                }else{
+                                    ways+=(lF*rF);
+                                }
+                            }else{
+                                if(isTrue==1){
+                                    ways+=(lT*rF+lF*rT);
+                                }else{
+                                    ways+=(lT*rT+lF*rF);
+                                }
+                            }
+                        }
+                        
+                        dp[i][j][isTrue] = ways%mod;
+                    }
+                }
             }
         }
-        return ebett(0,n-1,1,s,dp);
+        
+        
+        return dp[0][n-1][1];
+        
+        
+        
+        
+        //Memoization ...
+        
+        // int dp[][][] = new int[n][n][2];
+        // for(int arr1[][]:dp){
+        //     for(int arr2[] : arr1){
+        //         Arrays.fill(arr2,-1);
+        //     }
+        // }
+        // return ebett(0,n-1,1,s,dp);
     }
     public static int ebett(int i , int j , int isTrue , String s,int dp[][][]){
         if(i>j)
