68a493a2513d33ba19c15db67c133de9f42ef904
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-18 16:30:55 +0530

    Time: 6 ms (94.22%), Space: 41.7 MB (89.78%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -2,12 +2,30 @@ class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int n = arr.length;
         
+        // Tabulation ..
+        int dp[] = new int[n+1];
+        
+        for(int i = n-1 ; i>=0 ; i--){
+            int maxi = Integer.MIN_VALUE;
+            int maxEle = Integer.MIN_VALUE;
+            for(int j = i ; j < Math.min(i+k,arr.length) ; j++){
+                maxEle = Math.max(maxEle,arr[j]);
+                int curSum = (j-i+1)*maxEle + dp[j+1];
+                maxi = Math.max(maxi,curSum);
+            }
+
+            dp[i] = maxi;
+        }
+        
+        
+        return dp[0];
+        
         // Memoization...
         
-        int dp[] = new int[n];
-        Arrays.fill(dp,-1);
+//         int dp[] = new int[n];
+//         Arrays.fill(dp,-1);
         
-        return pawms(0,k,arr,dp);
+//         return pawms(0,k,arr,dp);
     }
     public static int pawms(int i , int k,int arr[],int dp[]){
         
