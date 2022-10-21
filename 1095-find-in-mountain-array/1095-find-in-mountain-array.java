feb4d3c54f3cfd7f5e4a26cdb96cd4e80688a58d
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-21 21:47:31 +0530

    Time: 1 ms (39.30%), Space: 43.5 MB (64.07%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,60 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int peak = peakEle(mountainArr);
+        int ans = orderAgnosticBs(mountainArr,target,0,peak);
+        if(ans != -1)
+            return ans;
+        return orderAgnosticBs(mountainArr,target,peak+1,mountainArr.length()-1);
+    }
+    public static int peakEle(MountainArray mountainArr){
+        int s = 0;
+        int e = mountainArr.length()-1;
+        
+        while(s<e){
+            int m = s+(e-s)/2;
+            
+            if(mountainArr.get(m)<mountainArr.get(m+1)){
+                s = m+1;
+            }else{
+                e = m;
+            }
+        }
+        
+        return s;
+    }
+    public static int orderAgnosticBs(MountainArray mountainArr,int x,int s,int e){
+        boolean isAsc = mountainArr.get(s) < mountainArr.get(e);
+        
+        while(s<=e){
+            int m = s+(e-s)/2;
+            
+            if(x==mountainArr.get(m)){
+                return m;
+            }
+            if(isAsc){
+                if(x<mountainArr.get(m)){
+                    e = m-1;
+                }else{
+                    s = m+1;
+                }
+            }else{
+                if(x<mountainArr.get(m)){
+                    s = m+1;
+                }else{
+                    e = m-1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
