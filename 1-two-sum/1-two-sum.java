f3524d1254448172e6e9b9c1a78c390f090710d4
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-23 08:53:27 +0530

    Time: 7 ms (68.11%), Space: 45.2 MB (48.50%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -13,8 +13,7 @@ class Solution {
                 ans[1] = i;
                 break;
             }
-            else
-                hm.put(nums[i],i);
+            hm.put(nums[i],i);
         }
         return ans;
         
