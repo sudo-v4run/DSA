b12bd57dd0099d3a1c02d8768be2ffd6d5278a9b
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-23 16:32:04 +0530

    Time: 7 ms (77.10%), Space: 44.9 MB (74.67%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,5 +1,42 @@
 class Solution {
-//     public int romanToInt(String s) {
+    public int romanToInt(String s) {
+        int n = s.length();
+        
+        int arr[] = new int[n];
+        
+        for(int i = 0 ; i < n ; i++){
+            char ch = s.charAt(i);
+            
+            if(ch=='I'){
+                arr[i] = 1;
+            }else if(ch=='V'){
+                arr[i] = 5;
+            }else if(ch=='X'){
+                arr[i] = 10;
+            }else if(ch=='L'){
+                arr[i] = 50;
+            }else if(ch=='C'){
+                arr[i] = 100;
+            }else if(ch=='D'){
+                arr[i] = 500;
+            }else{
+                arr[i] = 1000;
+            }
+        }
+        
+        int ans = 0;
+        for(int i = 0 ; i < n-1 ; i++){
+            if(arr[i]<arr[i+1]){
+                ans-=arr[i];
+            }else{
+                ans+=arr[i];
+            }
+        }
+        
+        ans += arr[n-1];
+        
+        return ans;
+        
         
 //         int ans = 0;
         
@@ -30,45 +67,7 @@ class Solution {
 //         }
         
 //         return ans;
-//     }
-    
-    
-    public int romanToInt(String s) {
         
-    int nums[]=new int[s.length()];
-    for(int i=0;i<s.length();i++){
-        switch (s.charAt(i)){
-            case 'M':
-                nums[i]=1000;
-                break;
-            case 'D':
-                nums[i]=500;
-                break;
-            case 'C':
-                nums[i]=100;
-                break;
-            case 'L':
-                nums[i]=50;
-                break;
-            case 'X' :
-                nums[i]=10;
-                break;
-            case 'V':
-                nums[i]=5;
-                break;
-            case 'I':
-                nums[i]=1;
-                break;
-        }
-    }
-    int sum=0;
-    for(int i=0;i<nums.length-1;i++){
-        if(nums[i]<nums[i+1])
-            sum-=nums[i];
-        else
-            sum+=nums[i];
-    }
-    return sum+nums[nums.length-1];
-}
+    } 
 }
 
