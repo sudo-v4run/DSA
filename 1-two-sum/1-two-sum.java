076869c0a77f5b01c37068c8c91eae76131897d4
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-23 08:46:26 +0530

    Time: 3 ms (93.33%), Space: 46.7 MB (10.22%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,6 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        
         int ans[] = new int[2];
+        
+        
+        // O(N) ...
+        
         HashMap<Integer,Integer> hm = new HashMap<>();
         for(int i = 0 ; i < nums.length; i++){
             if(hm.containsKey(target-nums[i])){
@@ -12,5 +17,51 @@ class Solution {
             }
         }
         return ans;
+        
+        
+        
+        
+        
+        /* O(N.logN) .... This is soultion is only fore booelan return type.
+                                    Coz indices' position changes when sorted.*/
+        // Using 2 pointers approach....
+        
+//         Arrays.sort(nums);
+//         int s = 0;
+//         int e = nums.length-1;
+        
+//         while(s<e){
+//             if(nums[s]+nums[e]==target){
+//                 return true;
+//             }
+//             if(nums[s]+nums[e]<target){
+//                 s++;
+//             }else{
+//                 e--;
+//             }
+//         }
+        
+//         return false;
+        
+        
+        
+        
+        
+        
+        
+         /* O(N.logN) .... This is soultion is only fore booelan return type.
+                                    Coz indices' position changes when sorted.*/
+        
+        // Using Binary Search...
+        
+//         Arrays.sort(nums);
+        
+        
+//         for(int i = 0 ; i < n ; i++){
+//             if(bs(nums,target-nums[i]))
+//                 return true;
+//         }
+//         return false;
+        
     }
 }
