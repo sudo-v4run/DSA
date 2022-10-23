6ba5c8386d329d466edc69f2e3d160042797572b
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-23 16:24:52 +0530

    Time: 10 ms (53.92%), Space: 44.8 MB (79.57%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int romanToInt(String s) {
+        
+        int ans = 0;
+        
+        s = s.replace("IV","IIII");
+        s = s.replace("IX","VIIII");
+        s = s.replace("XL","XXXX");
+        s = s.replace("XC","LXXXX");
+        s = s.replace("CD","CCCC");
+        s = s.replace("CM","DCCCC");
+        
+        for(int i = s.length()-1 ; i >=0 ; i--){
+            char ch = s.charAt(i);
+            if(ch=='I'){
+                ans+=1;
+            }else if(ch=='V'){
+                ans+=5;
+            }else if(ch=='X'){
+                ans+=10;
+            }else if(ch=='L'){
+                ans+=50;
+            }else if(ch=='C'){  //"MCMXCIV" 
+                ans+=100;
+            }else if(ch=='D'){
+                ans+=500;
+            }else{
+                ans+=1000;
+            }
+            
+            // if(i>0 && ch != s.charAt(i-1)){
+            //     char last = s.charAt(i-1);
+            //     if(last=='I'){
+            //         ans-=1;
+            //     }else if(last=='X'){
+            //         ans-=10;
+            //     }else if(last=='C'){
+            //         ans-=100;
+            //     }
+            // }
+        }
+        
+        return ans;
+    }
+}
