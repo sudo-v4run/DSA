83a2043cf522641c308eb7c05a19e70dbeab33dd
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-25 20:35:37 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (67.42%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,21 +1,44 @@
 import java.math.BigInteger;
 class Solution {
     public int[] plusOne(int[] digits) {
-        String ins = Arrays.toString(digits).replaceAll("\\[|\\]|,|\\s", "");
         
-        BigInteger ans = new BigInteger(ins);
-        BigInteger one = new BigInteger("1");
-        ans = ans.add(one);
+        int n = digits.length;
         
-        ins = String.valueOf(ans);
-        
-        char arr[] = ins.toCharArray();
-        int res[] = new int[arr.length];
-        
-        for(int i = 0 ; i < arr.length ; i++){
-            res[i] = Integer.parseInt(arr[i]+"");
+        for(int i = n-1 ; i >= 0 ;i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
         
-        return res;
+        int arr[] = new int[n+1];
+        
+        arr[0] = 1;
+        
+        return arr;
+        
+        
+        
+        
+        
+        
+        
+//         String ins = Arrays.toString(digits).replaceAll("\\[|\\]|,|\\s", "");
+        
+//         BigInteger ans = new BigInteger(ins);
+//         BigInteger one = new BigInteger("1");
+//         ans = ans.add(one);
+        
+//         ins = String.valueOf(ans);
+        
+//         char arr[] = ins.toCharArray();
+//         int res[] = new int[arr.length];
+        
+//         for(int i = 0 ; i < arr.length ; i++){
+//             res[i] = Integer.parseInt(arr[i]+"");
+//         }
+        
+//         return res;
     }
 }
