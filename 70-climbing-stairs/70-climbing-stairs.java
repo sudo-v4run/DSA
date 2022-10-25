357d7bb1610bc69eb3eb57c55360a10db489f088
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-25 20:47:18 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (37.66%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,11 +1,31 @@
 class Solution {
     public int climbStairs(int n) {
         
-        // Memoization ...
+        //Tabulation....
         
         int dp[] = new int[n];
-        Arrays.fill(dp,-1);
-        return clmbstrs(n-1,dp);
+        
+        dp[0] = 1;
+        if(n>1)
+            dp[1] = 2;
+        
+        for(int index = 2 ; index < n ; index++){
+            int oneStep = dp[index-1];
+            int  twoSteps = dp[index-2];
+
+            dp[index] = oneStep+twoSteps;
+        }
+        
+        return dp[n-1];
+        
+        
+        
+        
+        // Memoization ...
+        
+//         int dp[] = new int[n];
+//         Arrays.fill(dp,-1);
+//         return clmbstrs(n-1,dp);
     }
     public static int clmbstrs(int n,int dp[]){
         
