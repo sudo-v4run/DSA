42eb52eb500dc43f52925bbfef83871773f1b298
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-26 21:38:46 +0530

    Time: 249 ms (5.06%), Space: 41.5 MB (32.71%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,9 +1,35 @@
 class Solution {
-    public int mySqrt(int x) { 
-	if (x == 0) return 0;
-	for (int i = 1; i <= x / i; i++) 		
-		if (i <= x / i && (i + 1) > x / (i + 1))
-			return i;		
-	return -1;
-}
+    public int mySqrt(int x) {
+        
+//         int s = 0;
+//         int e = x/2;
+        
+//         int ans = -1;
+        
+//         while(s<=e){
+//             int m = s+(e-s)/2;
+            
+//             if(m*m <= x){
+//                 ans = m;
+//                 s = m+1;
+//             }else{
+//                 e = m-1;
+//             }
+//         }
+        
+//         return ans;
+        
+        
+        
+        
+        
+        if (x == 0) return 0;
+        int i = 1;
+        while(i*i <= x){
+            if (i <= x / i && (i + 1) > x / (i + 1))
+                return i;
+            i++;
+        }
+        return -1;
+    }
 }
