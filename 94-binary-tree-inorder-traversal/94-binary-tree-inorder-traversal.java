10c1cdcffb5063fdc7a8165f50136544b254c00b
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-27 04:56:00 +0530

    Time: 1 ms (46.56%), Space: 43 MB (7.42%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -23,32 +23,56 @@ class Solution {
 //         return res;
         
         
-        // Morris .....
+        // Iterative ... Using Stack ...
+        
+        Stack<TreeNode> s = new Stack<>();
         
         TreeNode cur = root;
         
-        while(cur!=null){
-            if(cur.left == null){
+        while(cur!=null || !s.isEmpty()){
+            if(cur!=null){
+                s.push(cur);
+                cur = cur.left;
+            }else{
+                if(s.isEmpty())
+                    break;
+                
+                cur = s.pop();
                 res.add(cur.val);
                 cur = cur.right;
-            }else{
-                TreeNode temp = cur.left;
-                while(temp.right!=null && temp.right != cur){
-                    temp = temp.right;
-                }
-                if(temp.right==cur){
-                    temp.right = null;
-                    res.add(cur.val);
-                    cur = cur.right;
-                }else{
-                    temp.right = cur;
-                    cur = cur.left;
-                }
             }
-            
         }
         
         return res;
+        
+        
+        
+        // Morris .....
+        
+//         TreeNode cur = root;
+//         while(cur!=null){
+//             if(cur.left == null){
+//                 res.add(cur.val);
+//                 cur = cur.right;
+//             }else{
+//                 TreeNode temp = cur.left;
+//                 while(temp.right!=null && temp.right != cur){
+//                     temp = temp.right;
+//                 }
+//                 if(temp.right==cur){
+//                     temp.right = null;
+//                     res.add(cur.val);
+//                     cur = cur.right;
+//                 }else{
+//                     temp.right = cur;
+//                     cur = cur.left;
+//                 }
+//             }
+            
+//         }
+        
+//         return res;
+        
         
         
     }
