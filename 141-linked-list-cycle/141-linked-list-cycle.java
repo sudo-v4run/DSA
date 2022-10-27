f7ed0309dac796e42eddde48c6f7af4b758f9632
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-28 05:15:06 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (85.80%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,39 +11,41 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-//         if(head==null)
-//             return false;
         
-//         ListNode slow = head;
-//         ListNode fast = head;
-        
-//         while(fast.next!=null && fast.next.next !=null){
-//             slow = slow.next;
-//             fast = fast.next.next;
-            
-//             if(slow==fast){
-//                 return true;
-//             }
-//         }
-        
-//         return false;
-        
-        
-        HashSet<ListNode> hs = new HashSet<>();
-    
         if(head==null)
             return false;
         
-        while(head!=null){
-            if(!hs.contains(head)){
-                hs.add(head);
-                head = head.next;
-            }else{
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast.next!=null && fast.next.next !=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow==fast){
                 return true;
             }
         }
         
         return false;
+        
+        
+        
+//         HashSet<ListNode> hs = new HashSet<>();
+    
+//         if(head==null)
+//             return false;
+        
+//         while(head!=null){
+//             if(!hs.contains(head)){
+//                 hs.add(head);
+//                 head = head.next;
+//             }else{
+//                 return true;
+//             }
+//         }
+        
+//         return false;
         
     }
 }
