c6bf93227c99de5c1625323b7d9bac0975d64060
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-27 17:13:21 +0530

    Time: 7 ms (49.01%), Space: 49.7 MB (82.08%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,20 +1,31 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashSet<Integer> hs = new HashSet<>(10);
         
-        for(int i = 0 ; i < nums.length ; i++){
-            if(!hs.contains(nums[i])){
-                hs.add(nums[i]);
-            }else{
-                hs.remove(nums[i]);
-            }
-        }
-        int ans = 0;
-        for(int e : hs){
-            ans = e;
-            break;
+        int xor = 0;
+        
+        for(int i : nums){
+            xor^=i;
         }
         
-        return ans;
+        return xor;
+        
+        
+        
+//         HashSet<Integer> hs = new HashSet<>(10);
+        
+//         for(int i = 0 ; i < nums.length ; i++){
+//             if(!hs.contains(nums[i])){
+//                 hs.add(nums[i]);
+//             }else{
+//                 hs.remove(nums[i]);
+//             }
+//         }
+//         int ans = 0;
+//         for(int e : hs){
+//             ans = e;
+//             break;
+//         }
+        
+//         return ans;
     }
 }
