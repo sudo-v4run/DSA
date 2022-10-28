f8f36e0a049d3eefa42031e090bae4dfddef9c83
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-28 05:37:53 +0530

    Time: 1 ms (99.82%), Space: 45.1 MB (95.01%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -11,30 +11,70 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        
         if(headA==null||headB==null)
             return null;
         
-        HashSet<ListNode> hs = new HashSet<>();
+        int headALen = length(headA);
+        int headBLen = length(headB);
         
-        while(headA != null || headB !=null){
-            if(headA != null){
-                if(!hs.contains(headA)){
-                    hs.add(headA);
-                    headA = headA.next;
-                }else{
-                    return headA;
-                }
-            }
-            if(headB != null){
-                if(!hs.contains(headB)){
-                    hs.add(headB);
-                    headB = headB.next;
-                }else{
-                    return headB;
-                }
+        while(!(headALen==headBLen)){
+            if(headALen>headBLen){
+                headA = headA.next;
+                headALen--;
+            }else{
+                headB = headB.next;
+                headBLen--;
             }
         }
+        
+        while(headA!=null){
+            if(headA==headB){
+                return headA;
+            }
+            headA = headA.next;
+            headB = headB.next;
+        }
+        
         return null;
+        
+        
+        
+        
+        
+        
+        
+//         if(headA==null||headB==null)
+//             return null;
+        
+//         HashSet<ListNode> hs = new HashSet<>();
+        
+//         while(headA != null || headB !=null){
+//             if(headA != null){
+//                 if(!hs.contains(headA)){
+//                     hs.add(headA);
+//                     headA = headA.next;
+//                 }else{
+//                     return headA;
+//                 }
+//             }
+//             if(headB != null){
+//                 if(!hs.contains(headB)){
+//                     hs.add(headB);
+//                     headB = headB.next;
+//                 }else{
+//                     return headB;
+//                 }
+//             }
+//         }
+//         return null;
+    }
+    public static int length(ListNode node){
+        int len = 0;
+        while(node!=null){
+            node = node.next;
+            len++;
+        }
+        
+        return len;
     }
 }
