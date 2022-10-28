98c434d6c1ff610ff1e5f638339839de02e0e20d
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-28 17:24:25 +0530

    Time: 2 ms (51.98%), Space: 42.4 MB (59.33%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -1,11 +1,30 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
+//         int res = 0;
+        
+//         for(int i = 0 ; i <= 31 ; i++){
+//             int bit = (n>>>i)&1;
+//             res = res|(bit<<(31-i));
+//         }
+        
+//         return res;
+        
+        
+        
+        
+        
         int res = 0;
         
         for(int i = 0 ; i <= 31 ; i++){
-            int bit = (n>>>i)&1;
-            res = res|(bit<<(31-i));
+            
+            res = res << 1;
+            
+            if((n&1)==1){
+                res++;
+            }
+            
+            n = n >> 1;
         }
         
         return res;
