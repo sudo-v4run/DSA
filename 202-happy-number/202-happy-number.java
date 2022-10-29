982e224f466a73a109163f0463060c55886930bc
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-29 23:25:55 +0530

    Time: 1 ms (98.24%), Space: 39.5 MB (91.46%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,19 +1,48 @@
 class Solution {
     public boolean isHappy(int n) {
-        if((n/10)<=0 && n!=1 && n!=7){
-            return false;
+        
+        HashSet<Integer> hs = new HashSet<>();
+        
+        while(!hs.contains(n)){
+            hs.add(n);
+            n = sumOfDigits(n);
+            
+            if(n==1){
+                return true;
+            }
         }
-        int newN = 0;
+        
+        return false;
+        
+        
+        
+        
+        // if((n/10)<=0 && n!=1 && n!=7){
+        //     return false;
+        // }
+        // int newN = 0;
+        // while(n!=0){
+        //     int l = n%10;
+        //     newN += l*l;
+        //     n = n/10;
+        // }
+        // System.out.println(newN);
+        // if(newN==1){
+        //     return true;
+        // }else{
+        //     return isHappy(newN);
+        // }
+    }
+    
+    public static int sumOfDigits(int n){
+        int res = 0;
+        
         while(n!=0){
             int l = n%10;
-            newN += l*l;
+            res = res+l*l;
             n = n/10;
         }
-        System.out.println(newN);
-        if(newN==1){
-            return true;
-        }else{
-            return isHappy(newN);
-        }
+        
+        return res;
     }
 }
