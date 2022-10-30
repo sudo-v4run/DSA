9ef4eb2dfdd6a297a8abd5712fe4f71f94d52f48
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-30 10:14:46 +0530

    Time: 15 ms (75.72%), Space: 68.8 MB (57.10%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,18 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         
-//         Arrays.sort(nums);
-        
-//         for(int i = 1 ; i < nums.length ; i++){
-//             if(nums[i] == nums[i-1]){
-//                 return true;
-//             }
-//         }
-        
-//         return false;
-        
-        
-        
         HashSet<Integer> hs = new HashSet<>();
         
         for(int i : nums){
@@ -23,5 +11,18 @@ class Solution {
         }
         
         return false;
+        
+        
+        
+//         Arrays.sort(nums);
+        
+//         for(int i = 1 ; i < nums.length ; i++){
+//             if(nums[i] == nums[i-1]){
+//                 return true;
+//             }
+//         }
+        
+//         return false;
+        
     }
 }
