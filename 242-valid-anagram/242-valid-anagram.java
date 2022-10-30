a60ab814c926c482517e4a1bbf8bf8d33b14e3ad
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-30 09:49:24 +0530

    Time: 4 ms (89.29%), Space: 44.1 MB (45.59%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        int arr[] = new int[26];
+        
+        for(char ch : s.toCharArray()){
+            int toAscii = ch-'a';
+            arr[toAscii]++;
+        }
+        
+        for(char ch : t.toCharArray()){
+            int toAscii = ch-'a';
+            arr[toAscii]--;
+        }
+        
+        for(int i : arr){
+            if(i!=0){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
