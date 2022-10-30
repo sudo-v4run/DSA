737c4632621c23734b290e16084e6cff812724dd
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-10-30 11:00:04 +0530

    Time: 3 ms (99.96%), Space: 53.5 MB (96.78%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -11,6 +11,46 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         
+        
+        
+        // TC - O(N) and SC = O(1) while preserving the structure of input list..
+        
+//         ListNode slow = head;
+//         ListNode fast = head;
+        
+//         while(fast!=null && fast.next!=null){
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+//         if(fast!=null){             // for odd no.of Nodes...
+//             slow = slow.next;
+//         }
+        
+//         ListNode prev = null;
+//         while(slow!=null){
+//             ListNode temp = slow.next;
+//             slow.next = prev;
+//             prev = slow;
+//             slow = temp;
+//         }
+        
+//         ListNode nextNode = null;
+//         while(prev!=null){
+//             ListNode temp = prev.next;
+//             if(prev.val!=head.val)
+//                 return false;
+//             prev.next = nextNode;
+//             nextNode = prev;
+//             prev = temp;
+//             head = head.next;
+//         }
+        
+//         return true;
+        
+        
+        
+        // TC - O(N) and SC = O(1) while NOT preserving the structure of input list..
+        
         ListNode slow = head;
         ListNode fast = head;
         
@@ -32,12 +72,9 @@ class Solution {
         
         ListNode nextNode = null;
         while(prev!=null){
-            ListNode temp = prev.next;
             if(prev.val!=head.val)
                 return false;
-            prev.next = nextNode;
-            nextNode = prev;
-            prev = temp;
+            prev = prev.next;
             head = head.next;
         }
         
