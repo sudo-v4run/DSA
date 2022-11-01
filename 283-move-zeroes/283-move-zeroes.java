61bce4bd9b7bb1701c2877e273b31830c30329e8
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-01 16:34:58 +0530

    Time: 4 ms (56.63%), Space: 54.9 MB (42.84%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -3,12 +3,14 @@ class Solution {
         int n = nums.length;
         
         int j = 0;
+        
         for(int i = 0 ; i < nums.length; i++){
             if(nums[i]!=0){
                 swap(nums,i,j);
                 j++;
             }
         }
+        
     }
     public static void swap(int arr[] ,int i , int j){
         int temp = arr[i];
