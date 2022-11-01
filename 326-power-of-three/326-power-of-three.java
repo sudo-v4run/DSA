08578628041d35add73ab3ea9478296d8a3cc32e
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-01 16:20:20 +0530

    Time: 15 ms (95.81%), Space: 42.1 MB (90.11%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        int maxP = (int)Math.pow(3, (int)(Math.log(Integer.MAX_VALUE)/Math.log(3)) );
         
-        // MaxP = 3^19 coz 3^20 is not int Int range...
+        // int maxP = (int)Math.pow(3, (int)(Math.log(Integer.MAX_VALUE)/Math.log(3)) );
         
-        return (n>0 && maxP%n==0);  //return ( n>0 &&  1162261467%n==0);
+        // MaxP = 3^19 coz 3^20 is not int Int range... = 1162261467
+        
+        return(n>0 && 1162261467%n==0);     // return( n>0 &&  MaxP%n==0);
     }
 }
