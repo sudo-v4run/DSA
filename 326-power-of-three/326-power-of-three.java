e8718d6e4ab599bd94bf027a5f2ce59d0a9de110
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-01 16:19:18 +0530

    Time: 26 ms (54.43%), Space: 47.5 MB (33.90%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -4,6 +4,6 @@ class Solution {
         
         // MaxP = 3^19 coz 3^20 is not int Int range...
         
-        return (n>0 && maxP%n==0);
+        return (n>0 && maxP%n==0);  //return ( n>0 &&  1162261467%n==0);
     }
 }
