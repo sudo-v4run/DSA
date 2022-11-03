36905b5a3c03230f8b2f1d4b5696984a2c0b0bc8
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-03 19:29:53 +0530

    Time: 47 ms (49.08%), Space: 51.3 MB (39.38%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,39 +1,42 @@
 class Solution {
     
+    static String res = "";
+    static int resLen = 0;
+    
     public String longestPalindrome(String s){
         int n = s.length();
         
-        String res = "";
-        int resLen = 0;
+        res = "";
+        resLen = 0;
+        
         
         for(int i = 0 ; i < n ; i++){
             int l = i;
             int r = i;
             
             // Odd...
-            while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
-                if(r-l+1>resLen){
-                    resLen = r-l+1;
-                    res = s.substring(l,r+1);
-                }
-                l--;
-                r++;
-            }
+            pali(s,l,r);
             
             // Even...
             l = i;
             r = i+1;
-            while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
-                if(r-l+1>resLen){
-                    resLen = r-l+1;
-                    res = s.substring(l,r+1);
-                }
-                l--;
-                r++;
-            }
+            pali(s,l,r);
         }
         
         return res;
+    }
+    
+    public static void pali(String s , int l , int r){
+        int n =s.length();
+        
+        while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
+            if(r-l+1>resLen){
+                resLen = r-l+1;
+                res = s.substring(l,r+1);
+            }
+            l--;
+            r++;
+        }
     }
     
     
