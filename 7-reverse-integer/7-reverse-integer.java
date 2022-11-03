38e42a5ac9ee7b4816fd46045f1c406e1c5bc7ef
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-03 20:09:42 +0530

    Time: 2 ms (84.84%), Space: 41.7 MB (23.98%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.java
+++ 7-reverse-integer/7-reverse-integer.java
@@ -1,33 +1,52 @@
 class Solution {
+    
     public int reverse(int x) {
-        try{
-            String toString = String.valueOf(x);
-            if(x<0){
-                toString = toString.substring(1);
-            }
-
-            toString = reverse(toString);
-
-            int toInt = Integer.parseInt(toString);
-
-            if(x<0){
-                return toInt*(-1);
-            }
-
-            return toInt;
-        }catch(Exception e){
-            return 0;
-        }
-   
+        int rev = 0;
+        int prev = 0;
         
-    }
-    public static String reverse(String s){
-        String rev = "";
-        
-        for(int i = s.length()-1; i>=0 ; i--){
-            rev+=s.charAt(i);
+        while(x!=0){
+            int last = x%10;
+            rev = rev*10 + last;
+            if((rev-last)/10 != prev){
+                return 0;
+            }
+            prev = rev;
+            x/=10;
         }
         
         return rev;
     }
+    
+//     public int reverse(int x) {
+//         try{
+//             String toString = String.valueOf(x);
+//             if(x<0){
+//                 toString = toString.substring(1);
+//             }
+
+//             toString = reverse(toString);
+
+//             int toInt = Integer.parseInt(toString);
+
+//             if(x<0){
+//                 return toInt*(-1);
+//             }
+
+//             return toInt;
+//         }catch(Exception e){
+//             return 0;
+//         }
+   
+        
+//     }
+//     public static String reverse(String s){
+//         String rev = "";
+        
+//         for(int i = s.length()-1; i>=0 ; i--){
+//             rev+=s.charAt(i);
+//         }
+        
+//         return rev;
+//     }
+    
 }
