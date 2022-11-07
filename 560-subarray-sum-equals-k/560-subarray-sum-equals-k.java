c8b90658fd5862fa5bbc4b6b9b4946cf7ae35e35
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-07 21:00:28 +0530

    Time: 20 ms (99.50%), Space: 47 MB (82.83%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,20 +1,38 @@
 class Solution {
-    public int subarraySum(int[] nums, int k) {
+    public int subarraySum(int[] arr, int sum) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int curSum = 0;
+        int n = arr.length;
         int cnt = 0;
-        
-        for(int i = 0 ; i < nums.length ; i++){
-            int sum = nums[i];
-            if(sum==k){
-                cnt++;
+        hm.put(0,1);
+        for(int i = 0 ; i < n ; i++){
+            curSum+=arr[i];
+            if(hm.containsKey(curSum-sum)){
+                cnt+=hm.get(curSum-sum);
             }
-            for(int j = i+1 ; j < nums.length ; j++){
-                sum+=nums[j];
-                if(sum==k){
-                    cnt++;
-                }
-            }
+            hm.put(curSum,hm.getOrDefault(curSum,0)+1);
         }
         
         return cnt;
+        
+        
+        
+        
+//         int cnt = 0;
+        
+//         for(int i = 0 ; i < nums.length ; i++){
+//             int sum = nums[i];
+//             if(sum==k){
+//                 cnt++;
+//             }
+//             for(int j = i+1 ; j < nums.length ; j++){
+//                 sum+=nums[j];
+//                 if(sum==k){
+//                     cnt++;
+//                 }
+//             }
+//         }
+        
+//         return cnt;
     }
 }
