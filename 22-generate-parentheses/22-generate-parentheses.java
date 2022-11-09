1d1604ed9f05d559178910f555b426bb88610f40
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-09 18:26:45 +0530

    Time: 4 ms (27.98%), Space: 43.7 MB (47.75%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        
+        f("",n,n,res);
+        
+        return res;
+    }
+    public static void f(String ans,int open, int close, List<String> res){
+        
+        if(open==0 && close==0){
+            res.add(ans);
+            return;
+        }
+        
+        if(close<open){
+            return;
+        }
+        
+        if(open>0){
+            f(ans+'(',open-1,close,res);
+        }
+        if(close>0){
+            f(ans+')',open,close-1,res);
+        }
+    }
+}
