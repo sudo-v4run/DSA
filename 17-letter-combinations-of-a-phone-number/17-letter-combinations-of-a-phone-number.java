b93c99a4c5c898ae3c0fb2e9d49202bddb0b9de4
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-09 16:20:29 +0530

    Time: 9 ms (35.35%), Space: 43.2 MB (19.01%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,50 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        
+        // HashMap<Character,String> hm = new HashMap<>();
+        
+        String[] arr = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        
+        List<String> res = new ArrayList<>();
+        
+        // hm.put('2',"abc");
+        // hm.put('3',"def");
+        // hm.put('4',"ghi");
+        // hm.put('5',"jkl");
+        // hm.put('6',"mno");
+        // hm.put('7',"pqrs");
+        // hm.put('8',"tuv");
+        // hm.put('9',"wxyz");
+        
+        int n = digits.length();
+        
+        if(digits.equals(""))
+            return res;
+        
+        f(0,digits,"",res,arr);
+        
+        return res;
+        
+    }
+    public static void f(int index,String s,String ans,
+                                    List<String> res,String[] arr){
+        
+        if(index>s.length()-1){
+            res.add(ans);
+            return;
+        }
+        
+        int c = s.charAt(index)-'0';
+        String chars = arr[c];
+        
+        for(char ch : chars.toCharArray()){
+            
+            f(index+1,s,ans+ch,res,arr);
+            
+            // ans+=ch;
+            // f(index+1,s,ans,res,arr);
+            // ans = ans.substring(0,ans.length()-1);
+        }
+        
+    }
+}
