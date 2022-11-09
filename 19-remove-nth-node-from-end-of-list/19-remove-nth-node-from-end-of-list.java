34a2fbadec1e98570ce82f209d97845c85f14d19
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-09 17:35:30 +0530

    Time: 1 ms (82.31%), Space: 42.7 MB (16.89%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.java
@@ -10,28 +10,56 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int sz = 0;
-        ListNode dumm = head;
-        ListNode ans = head;
         
-        while(dumm!=null){
-            dumm = dumm.next;
-            sz++;
+        ListNode start = new ListNode();
+        start.next = head;
+        
+        ListNode fast = start;
+        ListNode slow = start;
+        
+        for(int i = 1 ; i <= n ; i++){
+            fast = fast.next;
         }
         
-        int frmStrt = sz-n;
-        
-        if(frmStrt==0){
-            return head.next;
+        while(fast.next!=null){
+            slow = slow.next;
+            fast = fast.next;
         }
         
-        while(ans!=null && frmStrt > 1){
-            ans = ans.next;
-            frmStrt--;
-        }
+        slow.next = slow.next.next;
         
-        ans.next = ans.next.next!=null?ans.next.next:null;
+        return start.next;
         
-        return head;
+        
+        
+        
+        
+        
+//         TC : O(2N) ....
+        
+//         int sz = 0;
+//         ListNode dumm = head;
+//         ListNode ans = head;
+        
+//         while(dumm!=null){
+//             dumm = dumm.next;
+//             sz++;
+//         }
+        
+//         int frmStrt = sz-n;
+        
+//         if(frmStrt==0){
+//             return head.next;
+//         }
+        
+//         while(ans!=null && frmStrt > 1){
+//             ans = ans.next;
+//             frmStrt--;
+//         }
+        
+//         ans.next = ans.next.next!=null?ans.next.next:null;
+        
+//         return head;
+        
     }
 }
