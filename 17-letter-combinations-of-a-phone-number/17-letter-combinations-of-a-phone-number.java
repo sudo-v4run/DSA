1dd07259646237b92e7ed164ba5a3c0daf1e4ba9
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-09 16:04:00 +0530

    Time: 11 ms (14.96%), Space: 43.6 MB (5.46%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,39 +1,42 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        HashMap<Character,String> hm = new HashMap<>();
+        // HashMap<Character,String> hm = new HashMap<>();
+        
+        String[] arr = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        
         List<String> res = new ArrayList<>();
         
-        hm.put('2',"abc");
-        hm.put('3',"def");
-        hm.put('4',"ghi");
-        hm.put('5',"jkl");
-        hm.put('6',"mno");
-        hm.put('7',"pqrs");
-        hm.put('8',"tuv");
-        hm.put('9',"wxyz");
+        // hm.put('2',"abc");
+        // hm.put('3',"def");
+        // hm.put('4',"ghi");
+        // hm.put('5',"jkl");
+        // hm.put('6',"mno");
+        // hm.put('7',"pqrs");
+        // hm.put('8',"tuv");
+        // hm.put('9',"wxyz");
         
         int n = digits.length();
         
         if(digits.equals(""))
             return res;
         
-        String ans = "";
-        f(0,digits,ans,res,hm);
+        f(0,digits,"",res,arr);
         
         return res;
     }
-    public static void f(int index,String s,String ans, 
-                                    List<String> res,HashMap<Character,String> hm){
+    public static void f(int index,String s,String ans,
+                                    List<String> res,String[] arr){
         
         if(index>s.length()-1){
             res.add(ans);
             return;
         }
         char num = s.charAt(index);
+        String chars =  arr[Integer.parseInt(String.valueOf(num))];
         
-        for(char ch : hm.get(num).toCharArray()){
+        for(char ch : chars.toCharArray()){
             ans+=ch;
-            f(index+1,s,ans,res,hm);
+            f(index+1,s,ans,res,arr);
             ans = ans.substring(0,ans.length()-1);
         }
     }
