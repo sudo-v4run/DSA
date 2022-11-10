1bda7e170ec82ba5802a741cb05d9de5f583a9be
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-10 21:02:59 +0530

    Time: 1 ms (82.46%), Space: 43.1 MB (10.26%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,10 +1,45 @@
 class Solution {
     public int search(int[] arr, int target) {
-        int pivot = findPivot(arr);
-        int ans = bs(arr,target,0,pivot);
-        if(ans != -1)
-            return ans;
-        return bs(arr,target,pivot+1,arr.length-1);
+        
+        int n = arr.length;
+        int s = 0;
+        int e = n-1;
+        
+        while(s<=e){
+            int m = s+(e-s)/2;
+            
+            if(arr[m]==target){
+                return m;
+            }
+            
+            if(arr[s]<=arr[m]){
+                if(arr[s]<=target && target< arr[m]){
+                    e = m-1;
+                }else{
+                    s = m+1;    
+                }
+            }else{
+                if(arr[m]<target && target<=arr[e]){
+                    s = m+1;
+                }else{
+                    e = m-1;
+                }
+            }
+            
+        }
+        
+        return -1;
+        
+        
+        
+        
+        
+        // int pivot = findPivot(arr);
+        // int ans = bs(arr,target,0,pivot);
+        // if(ans != -1)
+        //     return ans;
+        // return bs(arr,target,pivot+1,arr.length-1);
+        
     }
     public static int findPivot(int arr[]){
         
