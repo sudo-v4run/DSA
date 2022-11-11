586e5a76097c9d4006cc9c4c97056311d4898799
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-11 14:26:24 +0530

    Time: 0 ms (100.00%), Space: 47.1 MB (64.57%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,34 +1,34 @@
 class Solution {
     public int[] searchRange(int[] arr, int target) {
-        int ans[] = {-1,-1};
+        int res[] = {-1,-1};
+        int first = bs(arr,target,true);
+        int last = bs(arr,target,false);
         
-        ans[0] = bs(arr,target,true);
-        if(ans[0] != -1){
-            ans[1] = bs(arr,target,false);
-        }
+        res[0] = first;
+        res[1] = last;
         
-        return ans;
+        return res;
     }
-    public static int bs(int arr[], int x, boolean find1stIndex){
+    public static int bs(int arr[],int target,boolean forFirst){
         int ans = -1;
         
         int s = 0;
         int e = arr.length-1;
         
         while(s<=e){
-            int mid = s+(e-s)/2;
+            int m = s+(e-s)/2;
             
-            if(x<arr[mid]){
-                e  = mid-1;
-            }else if(arr[mid]<x){
-                s = mid+1;
-            }else{
-                ans = mid;
-                if(find1stIndex){
-                    e = mid-1;
+            if(arr[m]==target){
+                ans = m;
+                if(forFirst){
+                    e = m-1;
                 }else{
-                    s = mid+1;
+                    s = m+1;
                 }
+            }else if(target<arr[m]){
+                e = m-1;
+            }else{
+                s = m+1;
             }
         }
         
