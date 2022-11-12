50c1de1ca00492017ce93ad3b50af4066aa18b27
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-13 00:20:17 +0530

    Time: 6 ms (99.38%), Space: 45.7 MB (89.04%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.java
@@ -0,0 +1,81 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        int n = strs.length;
+        HashMap<String,List<String>> hm = new HashMap<>();
+        
+        for(int i = 0 ; i < n ; i++){
+            
+            String cur = strs[i];
+            char cnt[] = new char[26];
+            for(char ch : cur.toCharArray()){
+                cnt[ch-'a']++;
+            }
+            String keyS = String.valueOf(cnt);
+            
+            if(!hm.containsKey(keyS)){
+                hm.put(keyS,new ArrayList<String>());
+            }
+            hm.get(keyS).add(cur);
+        }
+        
+        
+        return new ArrayList<>(hm.values());
+        
+    }
+    
+    
+    
+//     public List<List<String>> groupAnagrams(String[] strs) {
+        
+//         List<List<String>> res = new ArrayList<List<String>>();
+//         int n = strs.length;
+//         boolean vis[] = new boolean[n];
+        
+//         for(int i = 0 ; i < n ; i++){
+            
+//             if(vis[i] == true)
+//                 continue;
+            
+//             ArrayList<String> ans = new ArrayList<>();
+//             String cur = strs[i];
+//             ans.add(cur);
+            
+//             for(int j = i+1 ; j < n ; j++){
+                
+//                 String cmp = strs[j];
+//                 boolean isAnagram = checkAnagram(cur,cmp);
+                
+//                 if(isAnagram && vis[j] != true){
+//                     vis[i] = true;
+//                     vis[j] = true;
+//                     ans.add(cmp);
+//                 }
+                
+//             }
+            
+//             res.add(ans);
+//         }
+        
+//         return res;
+//     }
+//     public boolean checkAnagram(String s, String t) {
+        
+//         int arr[] = new int[26];
+        
+//         for(char ch : s.toCharArray()){
+//             arr[ch-'a']++;
+//         }
+        
+//         for(char ch : t.toCharArray()){
+//             arr[ch-'a']--;
+//         }
+        
+//         for(int i : arr){
+//             if(i!=0){
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+}
