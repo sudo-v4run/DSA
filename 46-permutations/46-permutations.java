4dba6b991ca7eb54d176c32ee31a66b125bac394
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-12 22:24:10 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (89.40%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,28 +1,65 @@
 class Solution {
+    
     public List<List<Integer>> permute(int[] nums) {
+        
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-        HashSet<Integer> hs = new HashSet<>();
-        ArrayList<Integer> ans = new ArrayList<>();
-        f(nums,hs,res,ans);
+        
+        f(nums,0,res);
+        
         return res;
     }
-    public static void f(int arr[] ,HashSet<Integer> hs,
-                                List<List<Integer>> res,ArrayList<Integer> ans ){
+    public static void f(int arr[] , int index, List<List<Integer>> res){
         
-        if(hs.size()==arr.length){
-            res.add(new ArrayList<Integer>(ans));
+        if(index==arr.length){
+            ArrayList<Integer> ans = new ArrayList<>();
+            for(int i : arr){
+                ans.add(i);
+            }
+            res.add(ans);
             return;
         }
         
-        for(int i = 0 ; i < arr.length ; i++){
-            if(!hs.contains(arr[i])){
-                hs.add(arr[i]);
-                ans.add(arr[i]);
-                f(arr,hs,res,ans);
-                ans.remove(ans.size()-1);
-                hs.remove(arr[i]);
-            }
+        for(int i = index; i < arr.length ;i++){
+            swap(arr,i,index);
+            f(arr,index+1,res);
+            swap(arr,i,index);
         }
         
     }
+    public static void swap(int arr[],int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    
+//     public List<List<Integer>> permute(int[] nums) {
+    
+//         List<List<Integer>> res = new ArrayList<List<Integer>>();
+//         HashSet<Integer> hs = new HashSet<>();
+//         ArrayList<Integer> ans = new ArrayList<>();
+    
+//         f(nums,hs,res,ans);
+    
+//         return res;
+//     }
+//     public static void f(int arr[] ,HashSet<Integer> hs,
+//                                 List<List<Integer>> res,ArrayList<Integer> ans ){
+        
+//         if(hs.size()==arr.length){
+//             res.add(new ArrayList<Integer>(ans));
+//             return;
+//         }
+        
+//         for(int i = 0 ; i < arr.length ; i++){
+//             if(!hs.contains(arr[i])){
+//                 hs.add(arr[i]);
+//                 ans.add(arr[i]);
+//                 f(arr,hs,res,ans);
+//                 ans.remove(ans.size()-1);
+//                 hs.remove(arr[i]);
+//             }
+//         }
+        
+//     }
 }
