5381abd214a2ea7b7f8f024f9125d80c47ec932b
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-12 23:06:43 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (54.43%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        for(int i = 0 ; i < m ; i++){
+            for(int j = i ; j < n ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        for(int row[] : matrix){
+            int s = 0;
+            int e = n-1;
+            while(s<=e){
+                swap(row,s,e);
+                s++;
+                e--;
+            }
+        }
+        
+    }
+    public static void swap(int arr[],int i , int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
