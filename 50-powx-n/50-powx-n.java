2d27461b7fc4d486d333da168e9a5b940e72910e
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-14 19:39:30 +0530

    Time: 1 ms (87.87%), Space: 43.1 MB (39.33%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,18 +1,21 @@
 class Solution {
     public double myPow(double x, int n) {
-        double ans = 1;
+        
         if(n==0)
             return 1;
+        
+        double ans = 1;
             
         ans = myPow(x,Math.abs(n/2));
         ans = ans*ans;
         
         if(n%2!=0)
             ans = ans*x;
-        if(n<0){
+        
+        if(n<0)
             return 1/ans;
-        }
         
         return ans;
+        
     }
 }
