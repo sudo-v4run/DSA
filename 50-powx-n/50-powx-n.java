0fbbf51e8d65518693f14aabec95b6bc376d9a35
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-14 19:36:06 +0530

    Time: 1 ms (87.87%), Space: 42.9 MB (49.29%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -3,23 +3,14 @@ class Solution {
         double ans = 1;
         if(n==0)
             return 1;
-        int N = 1;
-        
-        if(n == Integer.MIN_VALUE)
-            N = Integer.MAX_VALUE;
-        else
-            N = Math.abs(n);
-        
             
-        ans = myPow(x,N/2);
+        ans = myPow(x,Math.abs(n/2));
         ans = ans*ans;
-        if(N%2!=0)
+        
+        if(n%2!=0)
             ans = ans*x;
         if(n<0){
-            if(n==Integer.MIN_VALUE)
-                return 1/(ans*x);
-            else 
-                return 1/ans;
+            return 1/ans;
         }
         
         return ans;
