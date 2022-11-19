5022aeb4b10a0ba285280deba8894bc9f67ce563
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-19 15:05:43 +0530

    Time: 0 ms (100.00%), Space: 39 MB (91.38%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,13 +1,42 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        // return f(m-1,n-1);
+        
+        // Tabulation....
         
         int dp[][] = new int[m][n];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        
+        for(int i = 0 ; i < m ; i++){
+            dp[i][0] = 1;
+        }
+        for(int j = 0 ; j < n ; j++){
+            dp[0][j] = 1;
         }
         
-        return f(m-1,n-1,dp);
+        for(int i = 1; i < m ; i++){
+            for(int j = 1; j < n ; j++){
+                
+                int left = dp[i][j-1];
+                int right = dp[i-1][j];
+
+                dp[i][j] = left+right;
+            }
+        }
+        
+        return dp[m-1][n-1];
+        
+        
+        
+        // Memoization....
+        
+//         int dp[][] = new int[m][n];
+//         for(int[] row:dp){
+//             Arrays.fill(row,-1);
+//         }
+        
+//         return f(m-1,n-1,dp);
+        
+        
+        // return f(m-1,n-1);
     }
     public static int f(int m,int n,int dp[][]){
         
