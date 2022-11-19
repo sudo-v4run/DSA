3a6c50c54c4da93a120fbfb64999c30702e64e32
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-19 21:55:47 +0530

    Time: 119 ms (90.21%), Space: 40.4 MB (91.65%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,15 +1,16 @@
 class Solution {
+    
+//              O(1) SC....
+    
     public boolean exist(char[][] board, String word) {
         
         int m = board.length;
         int n = board[0].length;
         
-        int vis[][] = new int[m][n];
-        
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
                 
-                if(f(i,j,0,board,word,vis))
+                if(f(i,j,0,board,word))
                     return true;
                 
             }
@@ -18,7 +19,7 @@ class Solution {
         return false;
     }
     public static boolean f(int r,int c,int wordInd, 
-                                char[][] board,String word, int[][] vis){
+                                char[][] board,String word){
         
         if(wordInd>=word.length())
             return true;
@@ -26,24 +27,81 @@ class Solution {
         if(r<0 || r>= board.length || c<0 || c>= board[0].length)
             return false;
         
-        if(vis[r][c]==1)
+        if(board[r][c]=='#')
             return false;
         
         if(word.charAt(wordInd)!=board[r][c])
             return false;
         
-        vis[r][c] = 1;
+        char temp = board[r][c];
+        
+        board[r][c] = '#';
             
-        if(f(r-1,c,wordInd+1,board,word,vis) ||
-           f(r+1,c,wordInd+1,board,word,vis) ||
-           f(r,c-1,wordInd+1,board,word,vis) ||
-           f(r,c+1,wordInd+1,board,word,vis)){
+        if(f(r-1,c,wordInd+1,board,word) ||
+           f(r+1,c,wordInd+1,board,word) ||
+           f(r,c-1,wordInd+1,board,word) ||
+           f(r,c+1,wordInd+1,board,word)){
             
             return true;
         }
         
-        vis[r][c] = 0;
+        board[r][c] = temp;
         
         return false;
     }
+    
+    
+    
+    
+    
+//              O(m*m) SC .....
+    
+//     public boolean exist(char[][] board, String word) {
+        
+//         int m = board.length;
+//         int n = board[0].length;
+        
+//         int vis[][] = new int[m][n];
+        
+//         for(int i = 0 ; i < m ; i++){
+//             for(int j = 0 ; j < n ; j++){
+                
+//                 if(f(i,j,0,board,word,vis))
+//                     return true;
+                
+//             }
+//         }
+        
+//         return false;
+//     }
+//     public static boolean f(int r,int c,int wordInd, 
+//                                 char[][] board,String word, int[][] vis){
+        
+//         if(wordInd>=word.length())
+//             return true;
+        
+//         if(r<0 || r>= board.length || c<0 || c>= board[0].length)
+//             return false;
+        
+//         if(vis[r][c]==1)
+//             return false;
+        
+//         if(word.charAt(wordInd)!=board[r][c])
+//             return false;
+        
+//         vis[r][c] = 1;
+            
+//         if(f(r-1,c,wordInd+1,board,word,vis) ||
+//            f(r+1,c,wordInd+1,board,word,vis) ||
+//            f(r,c-1,wordInd+1,board,word,vis) ||
+//            f(r,c+1,wordInd+1,board,word,vis)){
+            
+//             return true;
+//         }
+        
+//         vis[r][c] = 0;
+        
+//         return false;
+//     }
+    
 }
