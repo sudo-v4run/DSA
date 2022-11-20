4c2f0cfd1ef6b8ed3ccd5db1ba7120dda048d0ac
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-20 12:33:16 +0530

    Time: 1 ms (98.25%), Space: 40.8 MB (87.91%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int numDecodings(String s) {
+        
+        int n = s.length();
+        
+        int dp[] = new int[n];
+        
+        Arrays.fill(dp,-1);
+        
+        return f(0,s,dp);
+    }
+    public static int f(int index,String s,int dp[]){
+        
+        if(index>=s.length())
+            return 1;
+        
+        if(s.charAt(index)=='0')
+            return 0;
+        
+        if(dp[index] != -1)
+            return dp[index];
+        
+        int take1digit = f(index+1,s,dp);
+        int take2digits = 0;
+        if(index+1 <= s.length()-1 && ( (s.charAt(index)=='1') ||  
+            (s.charAt(index)=='2' && 
+             (s.charAt(index+1)>='0' && s.charAt(index+1)<='6') ) ) ){
+            
+            take2digits = f(index+2,s,dp);
+        }
+        
+        return dp[index] = take1digit+take2digits;
+        
+        
+        
+        // Recursion....
+        
+//         if(index>=s.length())
+//             return 1;
+        
+//         if(s.charAt(index)=='0')
+//             return 0;
+        
+//         int take1digit = f(index+1,s);
+//         int take2digits = 0;
+//         if(index+1 <= s.length()-1 && ( (s.charAt(index)=='1') ||  
+//             (s.charAt(index)=='2' && 
+//              (s.charAt(index+1)>='0' && s.charAt(index+1)<='6') ) ) ){
+            
+            
+//             take2digits = f(index+2,s);
+//         }
+        
+//         return take1digit+take2digits;
+        
+    }
+}
