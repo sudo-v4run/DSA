1551f342d004cba7fa3aaffc0fff721564832cdb
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-21 16:50:16 +0530

    Time: 2 ms (52.01%), Space: 42.7 MB (60.94%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -16,60 +16,134 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         
+        
         // 2 Stack....
         
         List<List<Integer>> res = new ArrayList<List<Integer>>();        
         Stack<TreeNode> s1 = new Stack<>();
         Stack<TreeNode> s2 = new Stack<>();
+        ArrayList<Integer> ans;
         
         if(root==null)
             return res;
-        
-        boolean right = true;
         
         s1.push(root);
         
         while(!s1.isEmpty() || !s2.isEmpty()){
             
-            ArrayList<Integer> ans = new ArrayList<>();
+            ans = new ArrayList<>();
         
-            if(right){
-                while(!s1.isEmpty()){
+            while(!s1.isEmpty()){
                     
-                    TreeNode pop = s1.pop();
-                    ans.add(pop.val);
-                    
-                    if(pop.left!=null){
-                        s2.push(pop.left);
-                    }
-                    if(pop.right!=null){
-                        s2.push(pop.right);
-                    }
-                    
+                TreeNode pop = s1.pop();
+                ans.add(pop.val);
+
+                if(pop.left!=null){
+                    s2.push(pop.left);
                 }
-            }else{
-                while(!s2.isEmpty()){
-                    
-                    TreeNode pop = s2.pop();
-                    ans.add(pop.val);
-                    
-                    if(pop.right!=null){
-                        s1.push(pop.right);
-                    }
-                    if(pop.left!=null){
-                        s1.push(pop.left);
-                    }
-                    
+                if(pop.right!=null){
+                    s2.push(pop.right);
                 }
+
             }
             
-            res.add(ans);
+            if(!ans.isEmpty())
+                res.add(ans);
             
-            right = !right;
+            
+            ans = new ArrayList<>();
+            
+            while(!s2.isEmpty()){
+                    
+                TreeNode pop = s2.pop();
+                ans.add(pop.val);
+
+                if(pop.right!=null){
+                    s1.push(pop.right);
+                }
+                if(pop.left!=null){
+                    s1.push(pop.left);
+                }
+
+            }
+            
+            if(!ans.isEmpty())
+                res.add(ans);
             
         }
         
         return res;
+        
+        
+        
+        
+        
+        
+        
+        
+//         // 2 Stack....
+        
+//         List<List<Integer>> res = new ArrayList<List<Integer>>();        
+//         Stack<TreeNode> s1 = new Stack<>();
+//         Stack<TreeNode> s2 = new Stack<>();
+        
+//         if(root==null)
+//             return res;
+        
+//         boolean right = true;
+        
+//         s1.push(root);
+        
+//         while(!s1.isEmpty() || !s2.isEmpty()){
+            
+//             ArrayList<Integer> ans = new ArrayList<>();
+        
+//             if(right){
+//                 while(!s1.isEmpty()){
+                    
+//                     TreeNode pop = s1.pop();
+//                     ans.add(pop.val);
+                    
+//                     if(pop.left!=null){
+//                         s2.push(pop.left);
+//                     }
+//                     if(pop.right!=null){
+//                         s2.push(pop.right);
+//                     }
+                    
+//                 }
+//             }else{
+//                 while(!s2.isEmpty()){
+                    
+//                     TreeNode pop = s2.pop();
+//                     ans.add(pop.val);
+                    
+//                     if(pop.right!=null){
+//                         s1.push(pop.right);
+//                     }
+//                     if(pop.left!=null){
+//                         s1.push(pop.left);
+//                     }
+                    
+//                 }
+//             }
+            
+//             res.add(ans);
+            
+//             right = !right;
+            
+//         }
+        
+//         return res;
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
 //         List<List<Integer>> res = new ArrayList<List<Integer>>();
