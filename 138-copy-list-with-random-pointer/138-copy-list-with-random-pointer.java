16562c5c77610c9d1f14e6f0fedeb1eb80270c34
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-24 20:29:07 +0530

    Time: 0 ms (100.00%), Space: 45.1 MB (73.05%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -15,35 +15,38 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        if(head==null){
+        
+        if(head==null)
             return null;
-        }
+        
         Node cur = head;
-        while(cur!=null){
+        while(cur != null){
             Node temp = cur.next;
-            Node newNode = new Node(cur.val);
-            cur.next = newNode;
-            newNode.next = temp;
-            cur=temp;
+            cur.next = new Node(cur.val);
+            cur.next.next = temp;
+            cur = temp;
         }
+        
         cur = head;
         
         while(cur!=null){
-            if(cur.random!=null)
+            if(cur.random != null){
                 cur.next.random = cur.random.next;
+            }
             cur = cur.next.next;
         }
-        Node ori = head;
-        cur = ori.next;
-        Node ans = cur;
-        while(ori!=null&&cur!=null){
-            ori.next = ori.next.next;
-            if(cur.next!=null)
-                cur.next = cur.next.next;
-            ori=ori.next;
+        
+        cur = head;
+        Node copy = cur.next;
+        Node ans = copy;
+        
+        while(cur!=null){
+            cur.next = cur.next.next;
+            copy.next = copy.next!=null?copy.next.next:null;
             cur = cur.next;
+            copy = copy.next;
         }
         
-         return ans;
+        return ans;
     }
 }
