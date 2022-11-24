14ffa32f50e12db0ba44970999d78ea6c38fb627
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-11-24 18:45:30 +0530

    Time: 11 ms (34.76%), Space: 80.7 MB (45.72%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,36 +1,59 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         
-        int gasSum = 0;
-        
-        for(int i : gas){
-            gasSum += i;
-        }
-        
-        int costSum = 0;
-        
-        for(int i : cost){
-            costSum += i;
-        }
-        
-        if(gasSum<costSum){
-            return -1;
-        }
+        int cur = 0;
+        int deficit = 0;
         
         int n = gas.length;
         
         int start = 0;
-        int target = 0;
         
         for(int i = 0 ; i < n ; i++){
-            target += gas[i]-cost[i];
+            cur += gas[i]-cost[i];
             
-            if(target<0){
-                target = 0;
+            if(cur<0){
+                deficit += cur;
+                cur = 0;
                 start = i+1;
             }
         }
         
+        if(cur+deficit<0){  // or cur+deficit < 0
+            return -1;
+        }
+        
         return start;
+        
+//         int gasSum = 0;
+        
+//         for(int i : gas){
+//             gasSum += i;
+//         }
+        
+//         int costSum = 0;
+        
+//         for(int i : cost){
+//             costSum += i;
+//         }
+        
+//         if(gasSum<costSum){
+//             return -1;
+//         }
+        
+//         int n = gas.length;
+        
+//         int start = 0;
+//         int target = 0;
+        
+//         for(int i = 0 ; i < n ; i++){
+//             target += gas[i]-cost[i];
+            
+//             if(target<0){
+//                 target = 0;
+//                 start = i+1;
+//             }
+//         }
+        
+//         return start;
     }
 }
