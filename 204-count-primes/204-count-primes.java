53cfd4184c249b687da8e1fe8e8db8048d4fb94c
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-12-02 22:12:35 +0530

    Time: 213 ms (31.16%), Space: 68.5 MB (41.57%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -1,44 +1,44 @@
 class Solution {
     public int countPrimes(int n) {
         
-//         int cnt = 0;
-//         boolean notPrime[] = new boolean[n];
+        int cnt = 0;
+        boolean notPrime[] = new boolean[n];
         
-//         for(int i = 2 ; i < n ; i++){
+        for(int i = 2 ; i < n ; i++){
             
-//             if(notPrime[i])
-//                 continue;
+            if(notPrime[i])
+                continue;
             
-//             cnt++;
+            cnt++;
             
-//             for(int j = 2*i ; j < n ; j = j+i){
-//                 notPrime[j] = true;
+            for(int j = 2*i ; j < n ; j = j+i){
+                notPrime[j] = true;
+            }
+        }
+        
+        
+        return cnt;
+        
+        
+//         int cnt = 0;
+//         boolean isPrime[] = new boolean[n];
+        
+//         Arrays.fill(isPrime,true);
+        
+//         for(int i = 2 ; i*i < n ; i++){
+//             if(isPrime[i]){
+//                 for(int j = i*i ; j < n ; j = j+i){
+//                     isPrime[j] = false;
+//                 }
 //             }
 //         }
         
+//         for(int i = 2 ; i < n ; i++){
+//             if(isPrime[i]){
+//                 cnt++;
+//             }
+//         }
         
 //         return cnt;
-        
-        
-        int cnt = 0;
-        boolean isPrime[] = new boolean[n];
-        
-        Arrays.fill(isPrime,true);
-        
-        for(int i = 2 ; i*i < n ; i++){
-            if(isPrime[i]){
-                for(int j = i*i ; j < n ; j = j+i){
-                    isPrime[j] = false;
-                }
-            }
-        }
-        
-        for(int i = 2 ; i < n ; i++){
-            if(isPrime[i]){
-                cnt++;
-            }
-        }
-        
-        return cnt;
     }
 }
