dff569b0c7e24f35f1ff973c4bdff1660bd3a411
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-12-05 21:23:55 +0530

    Time: 28 ms (17.43%), Space: 47.8 MB (60.14%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,48 +1,107 @@
 class Solution {
+    // BFS...
+    
     public boolean canFinish(int numCourses, int[][] preReq) {
         
-        // List<List<Integer>> adj = new ArrayList<List<Integer>>();
-        
-        HashMap<Integer,ArrayList<Integer>> adj = new HashMap<>();
+        int indeg[] = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<List<Integer>>();
         
         for(int i = 0 ; i < numCourses ; i++){
-            adj.put(i,new ArrayList<Integer>());
+            adj.add(new ArrayList<Integer>());
         }
         
         for(int i = 0 ; i < preReq.length ; i++){
             adj.get(preReq[i][0]).add(preReq[i][1]);
         }
         
-        HashSet<Integer> hs = new HashSet<>();
+        Queue<Integer> q = new LinkedList<>();
         
         for(int i = 0 ; i < numCourses ; i++){
-            if(!dfs(i,adj,hs)){
-                return false;
+            for(int nei : adj.get(i)){
+                indeg[nei]++;
             }
         }
         
-        return true;
-        
-    }
-    public static boolean dfs(int node, HashMap<Integer,ArrayList<Integer>> adj ,
-                            HashSet<Integer> hs){
-        
-        if(hs.contains(node)){
-            return false;
-        }
-        
-        hs.add(node);
-        
-        for(int nei : adj.get(node)){
-            if(!dfs(nei,adj,hs)){
-                return false;
+        for(int i = 0 ; i < numCourses ; i++){
+            if(indeg[i]==0){
+                q.add(i);
             }
         }
         
-        adj.put(node,new ArrayList<>());
+        int res[] = new int[numCourses];
+        int k = 0;
         
-        hs.remove(node);
+        while(!q.isEmpty()){
+            int pop = q.poll();
+            res[k++] = pop;
+            
+            for(int nei : adj.get(pop)){
+                indeg[nei]--;
+                if(indeg[nei]==0){
+                    q.add(nei);
+                }
+            }
+        }
         
-        return true;
+        if(k==numCourses){
+            return true;
+        }
+        return false;
     }
+    
+    
+    
+    // DFS...
+    
+//     public boolean canFinish(int numCourses, int[][] preReq) {
+        
+//         // List<List<Integer>> adj = new ArrayList<List<Integer>>();
+        
+//         // Using List of List is giving TLE .. 
+        
+//         HashMap<Integer,ArrayList<Integer>> adj = new HashMap<>();
+        
+//         for(int i = 0 ; i < numCourses ; i++){
+//             adj.put(i,new ArrayList<Integer>());
+//         }
+        
+//         for(int i = 0 ; i < preReq.length ; i++){
+//             adj.get(preReq[i][0]).add(preReq[i][1]);
+//         }
+        
+//         HashSet<Integer> hs = new HashSet<>();
+        
+//         for(int i = 0 ; i < numCourses ; i++){
+//             if(!dfs(i,adj,hs)){
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+        
+//     }
+//     public static boolean dfs(int node, HashMap<Integer,ArrayList<Integer>> adj ,
+//                             HashSet<Integer> hs){
+        
+//         if(hs.contains(node)){
+//             return false;
+//         }
+        
+//         hs.add(node);
+        
+//         for(int nei : adj.get(node)){
+//             if(!dfs(nei,adj,hs)){
+//                 return false;
+//             }
+//         }
+        
+//         adj.put(node,new ArrayList<>());
+        
+//         hs.remove(node);
+        
+//         return true;
+//     }
+    
+    
+    
 }
