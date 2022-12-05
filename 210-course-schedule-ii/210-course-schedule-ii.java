bf7a8aba35179dbab445b7613bf9daebfa2a877f
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-12-05 22:08:25 +0530

    Time: 14 ms (58.70%), Space: 50.3 MB (24.82%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,51 +1,115 @@
 class Solution {
     // BFS...
     
-    public int[] findOrder(int numCourses, int[][] preReq) {
+//     public int[] findOrder(int numCourses, int[][] preReq) {
         
-        int indeg[] = new int[numCourses];
-        List<List<Integer>> adj = new ArrayList<List<Integer>>();
+//         int indeg[] = new int[numCourses];
+//         List<List<Integer>> adj = new ArrayList<List<Integer>>();
+        
+//         for(int i = 0 ; i < numCourses ; i++){
+//             adj.add(new ArrayList<Integer>());
+//         }
+        
+//         for(int i = 0 ; i < preReq.length ; i++){
+//             adj.get(preReq[i][1]).add(preReq[i][0]);
+//         }
+        
+//         Queue<Integer> q = new LinkedList<>();
+        
+//         for(int i = 0 ; i < numCourses ; i++){
+//             for(int nei : adj.get(i)){
+//                 indeg[nei]++;
+//             }
+//         }
+        
+//         for(int i = 0 ; i < numCourses ; i++){
+//             if(indeg[i]==0){
+//                 q.add(i);
+//             }
+//         }
+        
+//         int res[] = new int[numCourses];
+//         int k = 0;
+        
+//         while(!q.isEmpty()){
+//             int pop = q.poll();
+//             res[k++] = pop;
+            
+//             for(int nei : adj.get(pop)){
+//                 indeg[nei]--;
+//                 if(indeg[nei]==0){
+//                     q.add(nei);
+//                 }
+//             }
+//         }
+        
+//         if(k==numCourses){
+//             return res;
+//         }
+//         return new int[]{};
+//     }
+    
+    
+    
+    
+    
+    // DFS...
+    
+    public int[] findOrder(int numCourses, int[][] preReq) { 
+        
+        HashMap<Integer,ArrayList<Integer>> adj = new HashMap<>();
         
         for(int i = 0 ; i < numCourses ; i++){
-            adj.add(new ArrayList<Integer>());
+            adj.put(i,new ArrayList<Integer>());
         }
         
         for(int i = 0 ; i < preReq.length ; i++){
-            adj.get(preReq[i][1]).add(preReq[i][0]);
+            adj.get(preReq[i][0]).add(preReq[i][1]);
         }
         
-        Queue<Integer> q = new LinkedList<>();
+        int vis[] = new int[numCourses];
+        HashSet<Integer> hs = new HashSet<>();
+        ArrayList<Integer> res = new ArrayList<>();
         
         for(int i = 0 ; i < numCourses ; i++){
-            for(int nei : adj.get(i)){
-                indeg[nei]++;
+            if(!dfs(i,adj,vis,res,hs)){
+                return new int[]{};
             }
         }
         
-        for(int i = 0 ; i < numCourses ; i++){
-            if(indeg[i]==0){
-                q.add(i);
-            }
-        }
-        
-        int res[] = new int[numCourses];
+        int ans[] = new int[res.size()];
         int k = 0;
         
-        while(!q.isEmpty()){
-            int pop = q.poll();
-            res[k++] = pop;
-            
-            for(int nei : adj.get(pop)){
-                indeg[nei]--;
-                if(indeg[nei]==0){
-                    q.add(nei);
-                }
+        for(int i : res){
+            ans[k++] = i;
+        }
+        
+        return ans;
+    }
+    public static boolean dfs(int node,HashMap<Integer,ArrayList<Integer>> adj,
+                      int vis[], ArrayList<Integer> res, HashSet<Integer> hs){
+        
+        if(hs.contains(node)){
+            return false;
+        }
+        
+        if(vis[node]==1){
+            return true;
+        }
+        
+        hs.add(node);
+        
+        for(int nei : adj.get(node)){
+            if(!dfs(nei,adj,vis,res,hs)){
+                return false;
             }
         }
         
-        if(k==numCourses){
-            return res;
-        }
-        return new int[]{};
+        vis[node] = 1;
+        res.add(node);
+        hs.remove(node);
+        
+        return true;
     }
+    
 }
