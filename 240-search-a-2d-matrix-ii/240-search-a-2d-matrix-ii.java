dbd685d647ac10e4e1c8aeb16d868cf9e7c1eeff
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-12-13 20:32:04 +0530

    Time: 5 ms (100.00%), Space: 48.4 MB (87.83%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -3,16 +3,20 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         
-        int r = 0, c = n-1;
+        int curRow = 0;
+        int curCol = n-1;
         
-        while(r<m && c>=0){
-            if(matrix[r][c]==target)
+        while(curRow<m && curCol>=0){
+            if(matrix[curRow][curCol]==target){
                 return true;
-            if(target<matrix[r][c]){
-                c--;
-            }else{
-                r++;
             }
+            
+            if(target<matrix[curRow][curCol]){
+                curCol--;
+            }else{
+                curRow++;
+            }
+            
         }
         
         return false;
