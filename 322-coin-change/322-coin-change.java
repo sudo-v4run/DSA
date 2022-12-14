767f4927b7a771e548755fefb881020f3423df1f
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-12-15 03:43:55 +0530

    Time: 14 ms (95.61%), Space: 41.8 MB (93.89%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,11 +1,34 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int dp[][] = new int[n][amount+1];
-        for(int row[] : dp){
-            Arrays.fill(row,-1);
+        
+        
+        // Buttom-Up...
+        
+        int dp[] = new int[amount+1];
+        Arrays.fill(dp,(int)1e9);
+        
+        dp[0] = 0;
+        
+        for(int amnt = 1 ; amnt <= amount ; amnt++){
+            for(int c = 0 ; c < coins.length ; c++){
+                if(amnt-coins[c]>=0){
+                    dp[amnt] = Math.min(dp[amnt],1+dp[amnt-coins[c]]);
+                }
+            }
         }
-        return f(0,amount,coins,dp)==(int)1e9?-1:f(0,amount,coins,dp);
+        
+        return dp[amount]==1e9?-1:dp[amount];
+        
+        
+        
+        // Memoization - Top Down...
+        
+        // int dp[][] = new int[n][amount+1];
+        // for(int row[] : dp){
+        //     Arrays.fill(row,-1);
+        // }
+        // return f(0,amount,coins,dp)==(int)1e9?-1:f(0,amount,coins,dp);
     }
     public static int f(int index,int amount,int coins[],int dp[][]){
         if(amount==0){
