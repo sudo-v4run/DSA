10963c40a44909478d748cdd7cc10e1fc01366b2
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-12-15 04:07:56 +0530

    Time: 42 ms (69.92%), Space: 41.9 MB (78.48%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.java
+++ 279-perfect-squares/279-perfect-squares.java
@@ -11,9 +11,6 @@ class Solution {
         
         for(int i = 1 ; i <= n ; i++){
             for(int s = 1 ; s*s <= n ; s++){
-                if(s*s>i){
-                    break;
-                }
                 if(i-s*s>=0){
                     dp[i] = Math.min(dp[i],1+dp[i-(s*s)]);
                 }
