d6b90f08f414bcc18c44a78e81a0ea0eedbb270d
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-12-17 21:55:15 +0530

    Time: 589 ms (8.10%), Space: 111 MB (10.62%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,212 +1,57 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
+    public int lengthOfLIS(int[] arr) {
         
-        // Using Binary Search....
+        int n = arr.length;
         
-        ArrayList<Integer> lis = new ArrayList<>();
-        lis.add(nums[0]);
-        int len = 1;
+        // Memoization ... Top Down...
         
-        for(int i = 1 ; i < n ; i++){
-            if(nums[i]>lis.get(len-1)){
-                lis.add(nums[i]);
-                len++;
-            }else{
-                if(lis.contains(nums[i])==false){
-                    int index = lower_bound(nums[i],lis);
-                    lis.set(index,nums[i]);
-                }
-            }
+        int dp[][] = new int[n][n+1];
+        
+        for(int row[] : dp){
+            Arrays.fill(row,-1);
         }
         
-        
-        return len;
-        
-        
-        
-        
-//        Tabulation....(Algorithm)...
-        
-//         int dp[] = new int[n]; 
-//         Arrays.fill(dp,1);
-        
-//         int maxi = Integer.MIN_VALUE;
-//         for(int index = 0 ; index < n ; index++){
-//             for(int prev_ind = 0 ; prev_ind < index ; prev_ind++){
-//                 if(nums[index]>nums[prev_ind]){
-//                     dp[index] = Math.max(dp[index],1 + dp[prev_ind]);
-//                 }
-                
-//             }
-//             maxi = Math.max(maxi,dp[index]);
-//         }
-        
-//         return maxi;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // Printing the LIS...
-        
-//         int dp[] = new int[n];
-//         int hash[] = new int[n];
-        
-//         Arrays.fill(dp,1);
-        
-//         int maxi = Integer.MIN_VALUE;
-//         int lastInd = 0;
-        
-//         for(int index = 0 ; index < n ; index++){
-//             hash[index] = index;
-//             for(int prev = 0 ; prev < index ; prev++){
-//                 if(nums[index]>nums[prev] && dp[prev]+1 > dp[index]){
-//                     dp[index] = dp[prev]+1;
-//                     hash[index] = prev;
-//                 }
-//             }
-//             if(maxi < dp[index]){
-//                 maxi = dp[index];
-//                 lastInd = index;
-//             }
-//         }
-        
-//         ArrayList<Integer> arr = new ArrayList<>();
-//         arr.add(nums[lastInd]);
-        
-//         while(hash[lastInd] != lastInd){            
-//             lastInd = hash[lastInd];
-//             arr.add(nums[lastInd]);
-//         }
-        
-//         // LIS is stored in reverse order...
-        
-//         for(int ele : arr){
-//             System.out.print(ele+"  ");
-//         }
-        
-//         System.out.println();
-//         return maxi;
-        
-        
-        //Tabulation....
-        
-//         int cur[] = new int[n+1];
-//         int next[] = new int[n+1];
-        
-//         for(int index = n-1; index >=0 ; index--){
-//             for(int prev_ind = index-1 ; prev_ind >= -1 ; prev_ind--){
-//                 int take = 0;
-//                 if(prev_ind==-1 || nums[index]>nums[prev_ind]){
-//                     take = 1 + next[index+1];
-//                 }
-//                 int notTake = next[prev_ind+1];
-
-//                 cur[prev_ind+1] = Math.max(take,notTake);
-//             }
-//             next = cur.clone();
-//         }
-        
-//         return next[-1+1];
-        
-        
-        
-        //Tabulation....
-        
-//         int dp[][] = new int[n+1][n+1];
-        
-//         for(int index = n-1; index >=0 ; index--){
-//             for(int prev_ind = index-1 ; prev_ind >= -1 ; prev_ind--){
-//                 int take = 0;
-//                 if(prev_ind==-1 || nums[index]>nums[prev_ind]){
-//                     take = 1 + dp[index+1][index+1];
-//                 }
-//                 int notTake = dp[index+1][prev_ind+1];
-
-//                 dp[index][prev_ind+1] = Math.max(take,notTake);
-//             }
-//         }
-        
-//         return dp[0][-1+1];
-        
-        
-        
-        //Memoization....
-        
-//         int dp[][] = new int[n][n+1];
-        
-//         for(int[] row:dp){
-//             Arrays.fill(row,-1);
-//         }
-        
-//         return lis(0,-1,nums,dp);
+        return f(0,-1,arr,dp);
     }
-    
-    static int lower_bound(int key , ArrayList<Integer> arr)
-    {
-        int low = 0, high = arr.size();
-        int mid;
- 
-        while (low < high) {
- 
-            mid = low + (high - low) / 2;
- 
-            if (key <= arr.get(mid)) {
-                high = mid;
-            }
- 
-            else {
- 
-                low = mid + 1;
-            }
-        }
- 
-        if (low < arr.size() && arr.get(low) < key) {
-            low++;
-        }
- 
-        return low;
-    }
-    
-    
-    
-    
-    public static int lis(int index, int prev_ind,int nums[],int dp[][]){
+    public static int f(int index, int prevInd , int arr[],int dp[][]){
         
-        if(index==nums.length)
+        if(index==arr.length){
             return 0;
+        }
         
-        if(dp[index][prev_ind+1] != -1)
-            return dp[index][prev_ind+1];
+        if(dp[index][prevInd+1] != -1){
+            return dp[index][prevInd+1];
+        }
         
         int take = 0;
-        if(prev_ind==-1 || nums[index]>nums[prev_ind]){
-            take = 1 + lis(index+1,index,nums,dp);
+        
+        if(prevInd == -1 || arr[index]>arr[prevInd]){
+            take = 1 + f(index+1,index,arr,dp);
         }
-        int notTake = lis(index+1,prev_ind,nums,dp);
         
-        return dp[index][prev_ind+1] = Math.max(take,notTake);
+        int notTake = f(index+1,prevInd,arr,dp);
         
-        
+        return dp[index][prevInd+1] = Math.max(take,notTake);
         
         
         // Recursion...
         
-//         if(index==nums.length)
+//         if(index>=arr.length){
 //             return 0;
+//         }
+        
+//         int maxLen = 0;
         
 //         int take = 0;
-//         if(prev_ind==-1 || nums[index]>nums[prev_ind]){
-//             take = 1 + lis(index+1,index,nums);
+            
+//         if(arr[index]>last){
+//             take = 1 + f(index+1,arr[index],arr);
 //         }
-//         int notTake = lis(index+1,prev_ind,nums);
         
-//         return Math.max(take,notTake);
+//         int notTake = f(index+1,last,arr);
+        
+//         maxLen = Math.max(take,notTake);
+        
+//         return maxLen;
     }
 }
