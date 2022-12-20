5571dbd25c8c5b199427a081cf5a4611cb1a621b
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-12-20 14:58:10 +0530

    Time: 46 ms (33.17%), Space: 60.9 MB (8.37%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -2,12 +2,17 @@ class Solution {
     public int coinChange(int[] coins, int amnt) {
         int n = coins.length;
         
-        // Tabulation....
+        // Tabulation .... SC- O(N)...
         
-        int dp[][] = new int[n+1][amnt+1];
+        
+        
+        // Tabulation.... Buttom-Up.... SpaceOptimized
+        
+        int cur[] = new int[amnt+1];
+        int next[] = new int[amnt+1];
         
         for(int amount = 1 ; amount <= amnt ; amount++){
-            dp[n][amount] = (int)1e9;
+            next[amount] = (int)1e9;
         }
         
         for(int index = n-1 ; index>=0 ; index--){
@@ -16,16 +21,46 @@ class Solution {
                 int take = (int)1e9;
                 
                 if(amount-coins[index]>=0)
-                    take = 1+dp[index][amount-coins[index]];
+                    take = 1+cur[amount-coins[index]];
                 
-                int notTake = dp[index+1][amount];
+                int notTake = next[amount];
 
-                dp[index][amount] = Math.min(take,notTake);
+                cur[amount] = Math.min(take,notTake);
                 
             }
+            next = cur.clone();
         }
         
-        int res = dp[0][amnt];
+        int res = next[amnt];
+        
+        
+        
+        
+        
+        // Tabulation.... Buttom-Up....
+        
+//         int dp[][] = new int[n+1][amnt+1];
+        
+//         for(int amount = 1 ; amount <= amnt ; amount++){
+//             dp[n][amount] = (int)1e9;
+//         }
+        
+//         for(int index = n-1 ; index>=0 ; index--){
+//             for(int amount = 1 ; amount <= amnt ; amount++){
+                
+//                 int take = (int)1e9;
+                
+//                 if(amount-coins[index]>=0)
+//                     take = 1+dp[index][amount-coins[index]];
+                
+//                 int notTake = dp[index+1][amount];
+
+//                 dp[index][amount] = Math.min(take,notTake);
+                
+//             }
+//         }
+        
+//         int res = dp[0][amnt];
         
         
         
