0d80093eaa3858d973c2fd33c1ddd4c455162d25
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-12-20 14:54:05 +0530

    Time: 46 ms (33.17%), Space: 58.2 MB (10.70%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,19 +1,50 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
+    public int coinChange(int[] coins, int amnt) {
         int n = coins.length;
+        
+        // Tabulation....
+        
+        int dp[][] = new int[n+1][amnt+1];
+        
+        for(int amount = 1 ; amount <= amnt ; amount++){
+            dp[n][amount] = (int)1e9;
+        }
+        
+        for(int index = n-1 ; index>=0 ; index--){
+            for(int amount = 1 ; amount <= amnt ; amount++){
+                
+                int take = (int)1e9;
+                
+                if(amount-coins[index]>=0)
+                    take = 1+dp[index][amount-coins[index]];
+                
+                int notTake = dp[index+1][amount];
+
+                dp[index][amount] = Math.min(take,notTake);
+                
+            }
+        }
+        
+        int res = dp[0][amnt];
+        
+        
         
         
         // Memoization ... Top-Down...
         
-        int dp[][] = new int[n][amount+1];
+//         int dp[][] = new int[n][amnt+1];
         
-        for(int row[] : dp){
-            Arrays.fill(row,-1);
-        }
+//         for(int row[] : dp){
+//             Arrays.fill(row,-1);
+//         }
         
-        int res = f(0,amount,coins,dp);
+//         int res = f(0,amnt,coins,dp);
+        
+        
+        
         
         return res==(int)1e9?-1:res;
+        
     }
     public static int f(int index,int amount,int coins[],int dp[][]){
         
