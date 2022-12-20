3d39325832b51f90897ef7a9870a20ee1147ead7
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-12-20 15:05:36 +0530

    Time: 21 ms (83.14%), Space: 45.6 MB (29.06%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -4,34 +4,50 @@ class Solution {
         
         // Tabulation .... SC- O(N)...
         
+        int dp[] = new int[amnt+1];
+        
+        Arrays.fill(dp,(int)1e9);
+        
+        dp[0] = 0;
+        
+        for(int index = 0 ; index < n ; index++){
+            for(int amount = 1 ; amount <= amnt ; amount++){
+                if(coins[index]>amount)
+                    continue;
+                
+                dp[amount] = Math.min(dp[amount],1+dp[amount-coins[index]]);
+            }
+        }
+        
+        int res = dp[amnt];
         
         
         // Tabulation.... Buttom-Up.... SpaceOptimized
         
-        int cur[] = new int[amnt+1];
-        int next[] = new int[amnt+1];
+//         int cur[] = new int[amnt+1];
+//         int next[] = new int[amnt+1];
         
-        for(int amount = 1 ; amount <= amnt ; amount++){
-            next[amount] = (int)1e9;
-        }
+//         for(int amount = 1 ; amount <= amnt ; amount++){
+//             next[amount] = (int)1e9;
+//         }
         
-        for(int index = n-1 ; index>=0 ; index--){
-            for(int amount = 1 ; amount <= amnt ; amount++){
+//         for(int index = n-1 ; index>=0 ; index--){
+//             for(int amount = 1 ; amount <= amnt ; amount++){
                 
-                int take = (int)1e9;
+//                 int take = (int)1e9;
                 
-                if(amount-coins[index]>=0)
-                    take = 1+cur[amount-coins[index]];
+//                 if(amount-coins[index]>=0)
+//                     take = 1+cur[amount-coins[index]];
                 
-                int notTake = next[amount];
+//                 int notTake = next[amount];
 
-                cur[amount] = Math.min(take,notTake);
+//                 cur[amount] = Math.min(take,notTake);
                 
-            }
-            next = cur.clone();
-        }
+//             }
+//             next = cur.clone();
+//         }
         
-        int res = next[amnt];
+//         int res = next[amnt];
         
         
         
