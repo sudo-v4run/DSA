0562778131f428682ae7002d0183d15114d7f116
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-12-23 20:57:57 +0530

    Time: 0 ms (100.00%), Space: 42 MB (84.75%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.java
+++ 328-odd-even-linked-list/328-odd-even-linked-list.java
@@ -18,18 +18,18 @@ class Solution {
         ListNode temp = head.next;
         ListNode even = head.next;
         
-        while(temp.next!=null && head.next!=null){
+        while(temp!=null && temp.next!=null){
             head.next = temp.next;
             head = head.next;
             
-            if(temp.next==null || head.next==null)
-                break;
+            // if(temp.next==null || head.next==null)
+            //     break;
             
             temp.next = head.next;
             temp = temp.next;
         }
         
-        temp.next = null;
+        // temp.next = null;
         
         head.next = even;
         
