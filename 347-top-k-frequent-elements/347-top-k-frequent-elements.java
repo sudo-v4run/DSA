43fad5826a13c424cfa18c20832b29ffa143483b
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-12-25 20:08:11 +0530

    Time: 8 ms (99.34%), Space: 45.3 MB (74.07%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,27 +1,71 @@
 class Solution {
+    
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
         
+        HashMap<Integer,Integer> hm = new HashMap<>();
+
         for(int i : nums){
             hm.put(i,hm.getOrDefault(i,0)+1);
         }
         
-        List<Map.Entry<Integer, Integer> > list
-            = new LinkedList<Map.Entry<Integer, Integer> >(hm.entrySet());
+        int n = nums.length;
         
-        Collections.sort(list,(i1,i2) -> i2.getValue().compareTo(i1.getValue()));
+        ArrayList<Integer>[] freq = new ArrayList[n+1];
+        
+        for(Map.Entry<Integer,Integer> e : hm.entrySet()){
+            if(freq[e.getValue()]==null){
+                freq[e.getValue()] = new ArrayList<Integer>();
+            }
+            freq[e.getValue()].add(e.getKey());
+        }
         
         int ans[] = new int[k];
-        int j = 0;
+        int j=0;
         
-        for (Map.Entry<Integer, Integer> aa : list) {
-            if(k--==0){
-                break;
+        
+        for(int i = n ; i >=0 ; i--){
+            if(freq[i]!=null){
+                for(int num : freq[i]){
+                    if(k-- == 0){
+                        return ans;
+                    }
+                    
+                    ans[j++] = num;
+                }
             }
-            
-            ans[j++] = aa.getKey();
         }
         
         return ans;
+        
+        
+        
+        
+        
+        
+//         HashMap<Integer,Integer> hm = new HashMap<>();
+        
+//         for(int i : nums){
+//             hm.put(i,hm.getOrDefault(i,0)+1);
+//         }
+        
+//         List<Map.Entry<Integer, Integer> > list
+//             = new LinkedList<Map.Entry<Integer, Integer> >(hm.entrySet());
+        
+//         Collections.sort(list,
+//                (i1,i2) -> i2.getValue().compareTo(i1.getValue()));
+        
+//         int ans[] = new int[k];
+//         int j = 0;
+        
+//         for (Map.Entry<Integer, Integer> aa : list) {
+//             if(k--==0){
+//                 break;
+//             }
+            
+//             ans[j++] = aa.getKey();
+//         }
+        
+//         return ans;
+        
     }
 }
