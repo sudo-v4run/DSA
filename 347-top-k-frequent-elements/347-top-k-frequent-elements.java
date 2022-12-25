04efca9eafb6515b384fdb622ac5dab002681b11
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2022-12-25 18:47:23 +0530

    Time: 11 ms (88.49%), Space: 44.5 MB (97.28%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        
+        for(int i : nums){
+            hm.put(i,hm.getOrDefault(i,0)+1);
+        }
+        
+        List<Map.Entry<Integer, Integer> > list
+            = new LinkedList<Map.Entry<Integer, Integer> >(
+                hm.entrySet());
+ 
+        // Sort the list using lambda expression
+        Collections.sort(
+            list,
+            (i1,
+             i2) -> i2.getValue().compareTo(i1.getValue()));
+        
+        int ans[] = new int[k];
+        int j = 0;
+        
+        for (Map.Entry<Integer, Integer> aa : list) {
+            if(k--==0){
+                break;
+            }
+            
+            ans[j++] = aa.getKey();
+        }
+        
+        return ans;
+    }
+}
