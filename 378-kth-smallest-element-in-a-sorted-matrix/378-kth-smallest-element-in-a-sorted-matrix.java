0a3330dfa780fa01be249adf7bc6a93e7849c6cd
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-01-03 19:17:54 +0530

    Time: 5 ms (71.02%), Space: 56.4 MB (49.18%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,23 +1,21 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        
         int n = matrix.length;
         
         int l = matrix[0][0];
-        int h = matrix[n-1][n-1]+1;
+        int h = matrix[n-1][n-1];
         
         while(l<h){
             int m = l+(h-l)/2;
             
             int cnt = 0;
-            int j = matrix[0].length - 1;
             
-            for(int i = 0; i < matrix.length; i++) {
-                
-                while(j >= 0 && matrix[i][j] > m)
-                    j--;
-                
-                cnt += (j + 1);
+            for(int i = 0; i < n ; i++) {
+                for(int j = 0 ; j < n ; j++){
+                    if(matrix[i][j]<=m){
+                        cnt++;
+                    }
+                }
             }
             
             if(cnt<k){
