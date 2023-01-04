4279cc2d6f96d0fbc7c9713927f8a9304e142acb
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-01-04 19:44:10 +0530

    Time: 76 ms (37.02%), Space: 114.8 MB (15.28%) - LeetHub

---
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.java
@@ -0,0 +1,48 @@
+class RandomizedSet {
+    
+    HashMap<Integer,Integer> hm;
+    ArrayList<Integer> list;
+
+    public RandomizedSet() {
+        hm = new HashMap<>();
+        list = new ArrayList<>();
+    }
+    
+    public boolean insert(int val) {
+        boolean isPresent = hm.containsKey(val);
+        
+        if(!isPresent){
+            hm.put(val,list.size());
+            list.add(val);
+        }
+        
+        return !isPresent;
+    }
+    
+    public boolean remove(int val) {
+        
+        boolean isPresent = hm.containsKey(val);
+        
+        if(isPresent){
+            int index = hm.get(val);
+            int lastVal = list.get(list.size()-1);
+            
+            list.set(index,lastVal);
+            list.remove(list.size()-1);
+            
+            hm.put(lastVal,index);
+            hm.remove(val);
+        }
+        
+        return isPresent;
+    }
+    
+    public int getRandom() {
+        
+        Random rand = new Random();
+        
+        int randomInd = rand.nextInt(list.size());
+        
+        return list.get(randomInd);
+    }
+}
