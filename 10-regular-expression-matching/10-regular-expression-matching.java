c35af9c564218afec1ba1ac699b5f02263e7b34b
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-01-09 22:15:26 +0530

    Time: 2 ms (95.15%), Space: 42.6 MB (59.31%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.java
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -1,60 +1,53 @@
 class Solution {
-    public boolean isMatch(String s, String p) {
+    public boolean isMatch(String s1, String s2) {
         
-        int n1 = s.length();
-        int n2 = p.length();
+        int n1 = s1.length();
+        int n2 = s2.length();
         
         // Tabulation....Buttom Up....
         
-//         int dp[][] = new int[n1+1][n2+1];
+        int dp[][] = new int[n1+1][n2+1];
         
-//         dp[0][0] = 1;
+        dp[0][0] = 1;
         
-//         for(int index2 = n2 ;index2>=0 && s2.charAt(index2)=='*';){
-//             if(s2.charAt(index2)=='*'){
-//                 index2--;
-//             }
-//             index2--;
-//             dp[0][index2] = 1;
-//         }
-        
-//         for(int index1 = 1 ; index1 <= n1 ; index1++){
+        for(int index2 = 2 ; index2 <=n2 ; index2=index2+2){
+            if(s2.charAt(index2-1)!='*'){
+                break;
+            }
             
-//             for(int index2 = 1 ; index2 <= n2 ; index2++){
-                
-//                 if( s1.charAt(index1-1)==s2.charAt(index2-1)){
-//                     if(f(s1,s2,index1-1,index2-1,dp)){
-//                         dp[index1][index2] = 1;
-//                         return true;
-//                     }
-//                 }
-//                 else if(s2.charAt(index2-1)=='.'){
-//                     if(f(s1,s2,index1-1,index2-1,dp)){
-//                         dp[index1][index2] = 1;
-//                         return true;
-//                     }
-//                 }else if(s2.charAt(index2-1)=='*'){
-//                     while(index2 <= n2 && s2.charAt(index2)=='*'){
-//                         index2++;
-//                     }
-//                     if( (s2.charAt(index2-1) == s1.charAt(index1) ||
-//                         s2.charAt(index2-1) == '.') &&
-//                         (f(s1,s2,index1-1,index2,dp) ||
-//                         f(s1,s2,index1,index2-1,dp)) ){
-
-//                         dp[index1][index2] = 1;
-//                         return true;
-//                     }
-//                     if(f(s1,s2,index1,index2-2,dp)){
-//                         dp[index1][index2] = 1;
-//                         return true;
-//                     }
-//                 }
-                
-//             }
-//         }
+            dp[0][index2] = 1;
+        }
         
-//         return dp[n1][n2] == 1 ? true : false;
+        for(int index1 = 1 ; index1 <= n1 ; index1++){
+            
+            for(int index2 = 1 ; index2 <= n2 ; index2++){
+                
+                if( s1.charAt(index1-1)==s2.charAt(index2-1)){
+                    if(dp[index1-1][index2-1] == 1){
+                        dp[index1][index2] = 1;
+                    }
+                }
+                else if(s2.charAt(index2-1)=='.'){
+                    if(dp[index1-1][index2-1] == 1){
+                        dp[index1][index2] = 1;
+                    }
+                }else if(s2.charAt(index2-1)=='*'){
+                    if( (s2.charAt(index2-2) == s1.charAt(index1-1) ||
+                        s2.charAt(index2-2) == '.') &&
+                        (dp[index1-1][index2] == 1 ||
+                        dp[index1][index2-1] == 1 ) ){
+
+                        dp[index1][index2] = 1;
+                    }
+                    if(dp[index1][index2-2] == 1){
+                        dp[index1][index2] = 1;
+                    }
+                }
+                
+            }
+        }
+        
+        return dp[n1][n2] == 1 ? true : false;
         
         
         
@@ -63,13 +56,14 @@ class Solution {
         
         // Memoization...Top Down...
         
-        int dp[][] = new int[n1][n2];
+//         int dp[][] = new int[n1][n2];
         
-        for(int row[] : dp){
-            Arrays.fill(row,-1);
-        }
+//         for(int row[] : dp){
+//             Arrays.fill(row,-1);
+//         }
         
-        return f(s,p,n1-1,n2-1,dp);
+//         return f(s,p,n1-1,n2-1,dp);
+        
     }
     public static boolean f(String s1, String s2,
                             int index1, int index2,int dp[][]){
@@ -125,42 +119,5 @@ class Solution {
         
         return false;
         
-        
-        
-        
-        
-        
-        // Recursion....
-//         if(index1<0){
-//             while(index2>=0 && s2.charAt(index2)=='*'){
-//                 index2--;
-//             }
-//             if(index2<0){
-//                 return true;
-//             }else{
-//                 return false;
-//             }
-//         }
-//         if(index2<0){
-//             return false;
-//         }
-        
-//         if( s1.charAt(index1)==s2.charAt(index2)){
-//             if(f(s1,s2,index1-1,index2-1))
-//                 return true;
-//         }
-//         else if(s2.charAt(index2)=='?'){
-//             if(f(s1,s2,index1-1,index2-1))
-//                 return true;
-//         }else if(s2.charAt(index2)=='*'){
-//             if( f(s1,s2,index1-1,index2) ||
-//                 f(s1,s2,index1,index2-1) ||
-//                 f(s1,s2,index1-1,index2-1) ){
-                
-//                 return true;
-//             }
-//         }
-        
-//         return false;
     }
 }
