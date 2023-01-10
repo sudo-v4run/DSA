b15651ef58df384d7cb24b7bb3f77ef7cf8e6592
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-01-10 13:49:08 +0530

    Time: 2 ms (94.90%), Space: 42 MB (75.80%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.java
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -59,40 +59,40 @@ class Solution {
         
         // Tabulation... n -> 0 ....
         
-        int dp[][] = new int[n1+1][n2+1];
+//         int dp[][] = new int[n1+1][n2+1];
         
-        dp[n1][n2] = 1;
+//         dp[n1][n2] = 1;
         
-        for(int index2 = n2-1 ; index2 >=0 ; index2=index2-2){
-            if(s2.charAt(index2)!='*'){
-                break;
-            }
-            dp[n1][index2-1] = 1;
-            dp[n1][index2] = 1;
-        }
+//         for(int index2 = n2-1 ; index2 >=0 ; index2=index2-2){
+//             if(s2.charAt(index2)!='*'){
+//                 break;
+//             }
+//             dp[n1][index2-1] = 1;
+//             dp[n1][index2] = 1;
+//         }
         
-        for(int index1 = n1-1 ; index1 >=0 ; index1--){
-            for(int index2 = n2-1; index2 >=0 ; index2--){
-                boolean match = index1 < s1.length() && 
-                        (s1.charAt(index1) == s2.charAt(index2) || 
-                        s2.charAt(index2) == '.') ;
+//         for(int index1 = n1-1 ; index1 >=0 ; index1--){
+//             for(int index2 = n2-1; index2 >=0 ; index2--){
+//                 boolean match = index1 < s1.length() && 
+//                         (s1.charAt(index1) == s2.charAt(index2) || 
+//                         s2.charAt(index2) == '.') ;
         
-                if(index2+1 < s2.length() && s2.charAt(index2+1)=='*'){
-                    if( (match && dp[index1+1][index2] == 1) ||  // use *
-                        dp[index1][index2+2] == 1 ){       // don't use *
+//                 if(index2+1 < s2.length() && s2.charAt(index2+1)=='*'){
+//                     if( (match && dp[index1+1][index2] == 1) ||  // use *
+//                         dp[index1][index2+2] == 1 ){       // don't use *
 
-                        if(index1<s1.length()){
-                            dp[index1][index2] = 1;
-                        }
-                    }
-                }
-                if(match && dp[index1+1][index2+1] == 1){
-                    dp[index1][index2] = 1;
-                }
-            }
-        }
+//                         if(index1<s1.length()){
+//                             dp[index1][index2] = 1;
+//                         }
+//                     }
+//                 }
+//                 if(match && dp[index1+1][index2+1] == 1){
+//                     dp[index1][index2] = 1;
+//                 }
+//             }
+//         }
         
-        return dp[0][0] == 1 ? true : false;
+//         return dp[0][0] == 1 ? true : false;
         
         
         
@@ -100,47 +100,43 @@ class Solution {
         
 //         // Tabulation....Buttom Up....
         
-//         int dp[][] = new int[n1+1][n2+1];
+        int dp[][] = new int[n1+1][n2+1];
         
-//         dp[0][0] = 1;
+        dp[0][0] = 1;
         
-//         for(int index2 = 2 ; index2 <=n2 ; index2=index2+2){
-//             if(s2.charAt(index2-1)!='*'){
-//                 break;
-//             }
-//             dp[0][index2] = 1;
-//         }
+        for(int index2 = 2 ; index2 <=n2 ; index2=index2+2){
+            if(s2.charAt(index2-1)!='*'){
+                break;
+            }
+            dp[0][index2] = 1;
+        }
         
-//         for(int index1 = 1 ; index1 <= n1 ; index1++){
+        for(int index1 = 1 ; index1 <= n1 ; index1++){
             
-//             for(int index2 = 1 ; index2 <= n2 ; index2++){
+            for(int index2 = 1 ; index2 <= n2 ; index2++){
                 
-//                 if( s1.charAt(index1-1)==s2.charAt(index2-1)){
-//                     if(dp[index1-1][index2-1] == 1){
-//                         dp[index1][index2] = 1;
-//                     }
-//                 }
-//                 else if(s2.charAt(index2-1)=='.'){
-//                     if(dp[index1-1][index2-1] == 1){
-//                         dp[index1][index2] = 1;
-//                     }
-//                 }else if(s2.charAt(index2-1)=='*'){
-//                     if( (s2.charAt(index2-2) == s1.charAt(index1-1) ||
-//                         s2.charAt(index2-2) == '.') &&
-//                         (dp[index1-1][index2] == 1 ||
-//                         dp[index1][index2-1] == 1 ) ){
+                if( s1.charAt(index1-1)==s2.charAt(index2-1) ||
+                    s2.charAt(index2-1)=='.' ){
+                    if(dp[index1-1][index2-1] == 1){
+                        dp[index1][index2] = 1;
+                    }
+                }else if(s2.charAt(index2-1)=='*'){
+                    if( (s2.charAt(index2-2) == s1.charAt(index1-1) ||
+                        s2.charAt(index2-2) == '.') &&
+                        (dp[index1-1][index2] == 1 ||
+                        dp[index1][index2-1] == 1 ) ){
 
-//                         dp[index1][index2] = 1;
-//                     }
-//                     if(dp[index1][index2-2] == 1){
-//                         dp[index1][index2] = 1;
-//                     }
-//                 }
+                        dp[index1][index2] = 1;
+                    }
+                    if(dp[index1][index2-2] == 1){
+                        dp[index1][index2] = 1;
+                    }
+                }
                 
-//             }
-//         }
+            }
+        }
         
-//         return dp[n1][n2] == 1 ? true : false;
+        return dp[n1][n2] == 1 ? true : false;
         
         
         
