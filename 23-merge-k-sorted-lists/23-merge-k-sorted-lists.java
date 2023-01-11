1a58516fc28df6977bd39d4eba2e6f9c33aac3a2
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-01-11 13:15:57 +0530

    Time: 2 ms (96.19%), Space: 44.1 MB (87.02%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -16,35 +16,69 @@ class Solution {
         }
         
         int k = lists.length;
-        ListNode ans = new ListNode();
-        ListNode ansHead = ans;
-        boolean notFinished = true;
         
-        while(notFinished){
-            int min = Integer.MAX_VALUE;
-            notFinished = false;
-            int minPtr = k+1;
+        while(k > 1){
             
-            for(int i = 0 ; i < k ; i++){
-                if(lists[i] !=null && lists[i].val<min){
-                    min = lists[i].val;
-                    minPtr = i;
-                }
-                if(lists[i] !=null){
-                    notFinished = true;
-                }
-            }
-            if(!notFinished){
-                break;
+            int sz = k%2==0 ? k/2 : (k/2) + 1;
+            
+            ListNode temp[] = new ListNode[sz];
+            int j = 0;
+                
+            for(int i = 0 ; i < k ; i+=2){
+                
+                ListNode l1 = lists[i];
+                ListNode l2 = i+1>=k ? null : lists[i+1];
+                
+                temp[j++] = merge(l1,l2);
+
             }
             
-            lists[minPtr] = lists[minPtr].next;
+            lists = temp.clone();
             
-            ans.next = new ListNode(min);
-            ans = ans.next;
+            k = lists.length;
+            
         }
         
-        return ansHead.next;
+        return lists[0];
+        
+        
+        
+        
+        
+//         if(lists.length==0){
+//             return null;
+//         }
+        
+//         int k = lists.length;
+//         ListNode ans = new ListNode();
+//         ListNode ansHead = ans;
+//         boolean notFinished = true;
+        
+//         while(notFinished){
+//             int min = Integer.MAX_VALUE;
+//             notFinished = false;
+//             int minPtr = k+1;
+            
+//             for(int i = 0 ; i < k ; i++){
+//                 if(lists[i] !=null && lists[i].val<min){
+//                     min = lists[i].val;
+//                     minPtr = i;
+//                 }
+//                 if(lists[i] !=null){
+//                     notFinished = true;
+//                 }
+//             }
+//             if(!notFinished){
+//                 break;
+//             }
+            
+//             lists[minPtr] = lists[minPtr].next;
+            
+//             ans.next = new ListNode(min);
+//             ans = ans.next;
+//         }
+        
+//         return ansHead.next;
         
         
         
@@ -70,5 +104,30 @@ class Solution {
 //         }
         
 //         return res.next;
+    }
+    
+    public static ListNode merge(ListNode l1 , ListNode l2){
+        
+        ListNode dummy = new ListNode();
+        ListNode head = dummy;
+        
+        while(l1 != null && l2 != null){
+            if(l1.val<=l2.val){
+                dummy.next = l1;
+                dummy = dummy.next;
+                l1 = l1.next;
+            }else{
+                dummy.next = l2;
+                dummy = dummy.next;
+                l2 = l2.next;
+            }
+        }
+        if(l1!=null){
+            dummy.next = l1;
+        }else{
+            dummy.next = l2;
+        }
+        
+        return head.next;
     }
 }
