6515a51bfa64f4781f574b021c36ebdb5a959695
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-01-11 10:46:22 +0530

    Time: 415 ms (5.09%), Space: 43.8 MB (95.01%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,26 +10,72 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        if(lists.length==0){
+            return null;
+        }
         
         int k = lists.length;
+        ListNode ans = new ListNode();
+        ListNode ansHead = ans;
+        boolean notFinished = true;
         
-        for(int i = 0 ; i < k ; i++){
-            ListNode listHead = lists[i];
-            while(listHead!=null){
-                pq.add(listHead.val);
-                listHead = listHead.next;
+        while(notFinished){
+            
+            ListNode min = new ListNode(Integer.MAX_VALUE);
+            notFinished = false;
+            for(int i = 0 ; i < k ; i++){
+                if(lists[i] !=null && lists[i].val!=(int)1e7
+                                   && lists[i].val<min.val){
+                    min = lists[i];
+                }
+                if(lists[i] !=null && lists[i].val!=(int)1e7){
+                    notFinished = true;
+                }
             }
+            if(!notFinished){
+                break;
+            }
+            ans.next = new ListNode(min.val);
+            ans = ans.next;
+            
+            if(min.next!=null){
+                remove(min);
+            }else{
+                min.val = (int)1e7;
+            }
+            
         }
         
-        ListNode ansHead = new ListNode();
-        ListNode ans = ansHead;
+        return ansHead.next;
         
-        while(!pq.isEmpty()){
-            ansHead.next = new ListNode(pq.poll());
-            ansHead = ansHead.next;
-        }
         
-        return ans.next;
+        
+        
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+//         int k = lists.length;
+        
+//         for(int i = 0 ; i < k ; i++){
+//             ListNode listHead = lists[i];
+//             while(listHead!=null){
+//                 pq.add(listHead.val);
+//                 listHead = listHead.next;
+//             }
+//         }
+        
+//         ListNode ansHead = new ListNode();
+//         ListNode res = ansHead;
+        
+//         while(!pq.isEmpty()){
+//             ansHead.next = new ListNode(pq.poll());
+//             ansHead = ansHead.next;
+//         }
+        
+//         return res.next;
+    }
+    public static void remove(ListNode node){
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 }
