7eec8d55d08238419f975abddca7f42774dd6de9
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-01-12 09:19:00 +0530

    Time: 1 ms (100.00%), Space: 50.7 MB (75.51%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -9,9 +9,10 @@ class Solution {
         }
         
         for(int i = 0 ; i < n ; i++){
+            
             int val = Math.abs(arr[i]);
             
-            if(val-1 <0 || val-1 >= n){
+            if(val-1 < 0 || val-1 >= n){
                 continue;
             }
             
@@ -20,11 +21,13 @@ class Solution {
             }else if(arr[val-1]==0){
                 arr[val-1] = -(n+1);
             }
+            
         }
         
-        for(int i = 1; i <=n ; i++){
-            if(!(arr[i-1]<0))
+        for(int i = 1 ; i <= n ; i++){
+            if(arr[i-1]>=0){
                 return i;
+            }
         }
         
         return n+1;
