a88154ad5b0db841a0e53a9aa03d0acf1d53ad5a
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-01-12 11:25:46 +0530

    Time: 33 ms (78.63%), Space: 53.6 MB (70.82%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,8 +1,75 @@
 class Solution {
     public int[] sortArray(int[] arr) {
-        quickSort(arr,0,arr.length-1);
+        
+        // MergeSort...
+        
+        int n = arr.length;
+        partition(arr,0,n-1);
         return arr;
+        
+        
+        
+        
+        // QuickSort....
+        
+        // quickSort(arr,0,arr.length-1);
+        // return arr;
     }
+    
+    // MergeSort...
+    
+    public static void partition(int arr[],int l ,int h){
+       
+        if(l<h){
+            int m = (l+h)/2;
+            partition(arr,l,m);
+            partition(arr,m+1,h);
+            merge(arr,l,m,h);
+        }
+        return;
+    }
+    public static void merge(int arr[], int l , int m , int r){
+        int n = r-l+1;
+        int res[] = new int[n];
+        int i = l;
+        int j = m+1;
+        int k = 0;
+        
+        while(i<=m && j<=r){
+            if(arr[i]<arr[j]){
+                res[k++] = arr[i++];
+            }else{
+                res[k++] = arr[j++];
+            }
+        }
+        while(i<=m){
+            res[k++] = arr[i++];
+        }
+        while(j<=r){
+            res[k++] = arr[j++];
+        }
+        
+        i = l;
+        k = 0;
+        for( ; i<=r ; i++){
+            arr[i] = res[k++];
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Quick Sort...
+    
     public static void quickSort(int arr[],int l , int h){
         if(l<h){
             int pivot = partitioningLogic(arr,l,h);
