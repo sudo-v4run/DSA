20f6155f2cf42cb7359867c5c2fb3e1721b130a6
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-01-14 21:29:42 +0530

    Time: 16 ms (74.88%), Space: 42.3 MB (96.07%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,51 +1,42 @@
 class Solution {
     public String minWindow(String s, String t) {
         
-        int n1 = s.length();
-        int n2 = t.length();
-        int start = -1;
-        int end = -1;
-        int ans = Integer.MAX_VALUE;
-        
         Map<Character,Integer> hm = new HashMap();
-        
-        int i = 0, j = 0 ;
         
         for(char ch : t.toCharArray()){
             hm.put(ch,hm.getOrDefault(ch,0)+1);
         }
         
+        int n1 = s.length();
+        int n2 = t.length();
+        int start = -1, end = -1;
+        int len = Integer.MAX_VALUE;
+        int i = 0, j = 0;
         int cnt = hm.size();
         
         while(j<=n1){
             
             if(cnt>0){
                 
-                if(j>=n1)
-                    break;
+                if(j>=n1)   break;
                 
                 char ch = s.charAt(j);
                 
                 if(hm.containsKey(ch)){
                     hm.put(ch,hm.get(ch)-1);
-                    
                     if(hm.get(ch)==0){
                         cnt--;
                     }
                 }
-                
                 j++;
             }
             
             if(cnt==0){
                 
-                if(i>=n1)
-                    break;
+                if(i>=n1)   break;
                 
-                int len = j-i;
-                
-                if(len<ans){
-                    ans = len;
+                if( j-i < len ){
+                    len = j-i;
                     start = i;
                     end = j;
                 }
