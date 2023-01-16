81ad738f5b07f09fd91c8782e36680be47d93028
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-01-16 16:53:49 +0530

    Time: 1 ms (96.57%), Space: 41.8 MB (91.63%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        // TC - O(N*N) --> calculate height from every Tree...
+        // if abs diff of left tree and right tree is greater than 1, 
+        // return false..
+        
+        // TC - O(N) -> by optimizing findHeight function...
+        
+        return findHeight(root) != -1;
+    }
+    
+    public static int findHeight(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        
+        int left = findHeight(root.left);
+        int right = findHeight(root.right);
+        
+        if(left==-1 || right == -1)
+            return -1;
+        
+        if(Math.abs(left-right)>1)
+            return -1;
+        
+        return 1 + Math.max(left,right);
+    }
+}
