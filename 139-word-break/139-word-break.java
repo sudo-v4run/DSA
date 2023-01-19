a85c547e609b4cab2f0d9cacba4594f9c792c476
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-01-19 19:24:13 +0530

    Time: 9 ms (49.73%), Space: 42.7 MB (49.39%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -9,13 +9,27 @@ class Solution {
             hs.add(word);
         }
         
+        int dp[] = new int[n+1];
+        dp[n] = 1;
+        
+        for(int index = n-1; index>=0 ; index--){
+            for(int i = index ; i < s.length() ; i++){
+            
+                if(hs.contains(s.substring(index,i+1)) && dp[i+1]==1){
+                    dp[index] = 1;
+                }
+            }
+        }
+        
+        return dp[0] == 1 ? true : false;
+        
         
         // Memoization.... Top-Down....
         
-        int dp[] = new int[n];
-        Arrays.fill(dp,-1);
+//         int dp[] = new int[n];
+//         Arrays.fill(dp,-1);
         
-        return f(s,0,hs,dp);
+//         return f(s,0,hs,dp);
     }
     public static boolean f(String s, int index, Set<String> hs,int dp[]){
         
