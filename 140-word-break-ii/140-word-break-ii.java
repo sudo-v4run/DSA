216ab2b2389f7ef3cc62393e10421f6d55d8d497
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-01-19 19:51:36 +0530

    Time: 4 ms (63.47%), Space: 40.7 MB (76.58%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.java
+++ 140-word-break-ii/140-word-break-ii.java
@@ -18,21 +18,30 @@ class Solution {
             hs.add(word);
         }
         
-        f(s,0,hs);
+        // Memoization.... Top-Down....
+
+        int dp[] = new int[n];
+        Arrays.fill(dp,-1);
+
+        f(s,0,hs,dp);
         
         return res;
     }
     
-    public static boolean f(String s, int index, Set<String> hs){
+    public static boolean f(String s, int index, Set<String> hs, int dp[]){
         
         if(index >= s.length()){
             return true;
         }
         
+        if(dp[index] != -1){
+            return dp[index] == 1 ? true : false;
+        }
+        
         String temp = ans;
         for(int i = index ; i < s.length() ; i++){
             ans += s.substring(index,i+1)+" ";
-            if(hs.contains(s.substring(index,i+1)) && f(s,i+1,hs)){
+            if(hs.contains(s.substring(index,i+1)) && f(s,i+1,hs,dp)){
                 res.add(ans.substring(0,ans.length()-1));
             }
             ans = temp;
