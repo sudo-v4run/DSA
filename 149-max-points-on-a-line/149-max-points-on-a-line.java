104f9a2722586fb81d8b18cf968d706ddde339a5
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-01-22 00:17:06 +0530

    Time: 43 ms (21.05%), Space: 49 MB (5.69%) - LeetHub

---
+++ 149-max-points-on-a-line/149-max-points-on-a-line.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maxPoints(int[][] points) {
+        int n = points.length;
+        
+        if(n<=2){
+            return n;
+        }
+        
+        Map<Double,Integer> hm = new HashMap();
+        int max = 0;
+        
+        for(int i = 0 ; i < n ; i++){
+            int iMax = 0;
+            for(int j = i+1 ; j < n ; j++){
+                
+                double slope = 1e9;
+                int y2=points[j][1], y1=points[i][1];
+                int x2=points[j][0], x1=points[i][0];
+                
+                int numerator = y2-y1;
+                int denominator = x2-x1;
+                
+                if(denominator != 0){
+                    slope = (double)numerator / (double)denominator;
+                }
+                if(numerator == 0){
+                    slope = (double)0;
+                }
+                
+                hm.put(slope,hm.getOrDefault(slope,1)+1);
+                iMax = Math.max(iMax,hm.get(slope));
+            }
+            max = Math.max(max,iMax);
+            hm.clear();
+        }
+        
+        return max;       // +1 for ith point itself
+    }
+}
