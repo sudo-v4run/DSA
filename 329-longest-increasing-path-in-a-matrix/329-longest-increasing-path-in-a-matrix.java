3fc707521ebdfea3a1a31a1f97949d7ba05c4665
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-02-02 14:28:21 +0530

    Time: 10 ms (63.78%), Space: 42.3 MB (84.28%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -0,0 +1,57 @@
+class Solution {
+    
+    static int lip;
+    
+    Solution(){
+        lip = 1;
+    }
+    
+    public int longestIncreasingPath(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int dp[][] = new int[m][n];
+        
+        for(int row[] : dp){
+            Arrays.fill(row,1);
+        }
+        
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                dfs(matrix,i,j,-1,dp);
+            }
+        }
+        
+        return lip;
+    }
+    public static int dfs(int arr[][], int r, int c, 
+                           int last, int dp[][]){
+        
+        if(r<0 || r>=arr.length || c<0 || c>=arr[0].length || arr[r][c]<=last){
+            
+            return Integer.MIN_VALUE;
+        }
+        
+        if(dp[r][c] != 1){
+            return dp[r][c];
+        }
+        
+        int max = 1;
+        
+        int up = dfs(arr,r-1,c,arr[r][c],dp);
+        max = Math.max(up+1,max);
+        
+        int left = dfs(arr,r,c-1,arr[r][c],dp);
+        max = Math.max(left+1,max);
+        
+        int down = dfs(arr,r+1,c,arr[r][c],dp);
+        max = Math.max(down+1,max);
+        
+        int right = dfs(arr,r,c+1,arr[r][c],dp);
+        max = Math.max(right+1,max);
+        
+        lip = Math.max(lip,max);
+        
+        return dp[r][c] = max;
+    }
+}
