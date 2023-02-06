1b49f957e95b2d5c2e65f7b5fc1e4eedb4931f18
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-02-06 17:13:09 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (81.41%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -49,25 +49,26 @@ class Solution {
         // Morris .....
         
         TreeNode cur = root;
-        while(cur!=null){
+        
+        while(cur != null){
             if(cur.left == null){
                 res.add(cur.val);
                 cur = cur.right;
             }else{
                 TreeNode temp = cur.left;
-                while(temp.right!=null && temp.right != cur){
+                while(temp.right != null && temp.right != cur){
                     temp = temp.right;
                 }
-                if(temp.right==cur){
-                    temp.right = null;
-                    res.add(cur.val);
-                    cur = cur.right;
-                }else{
+                if(temp.right == null){
                     temp.right = cur;
                     cur = cur.left;
+                }else{
+                    res.add(cur.val);
+                    temp.right = null;
+                    cur = cur.right;
                 }
+                
             }
-            
         }
         
         return res;
