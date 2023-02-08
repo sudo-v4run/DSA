d1b19a330c4a03576981bee1c35e4619efd04ed8
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-02-08 10:15:43 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (96.31%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
@@ -2,40 +2,40 @@
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
         
-//         if(root==null){
-//             return new TreeNode(val);
-//         }
-        
-//         if(val < root.val){
-//             root.left = insertIntoBST(root.left,val);
-//         }else{
-//             root.right = insertIntoBST(root.right,val);
-//         }
-        
-//         return root;
-        
-        if(root == null){
+        if(root==null){
             return new TreeNode(val);
         }
         
-        TreeNode cur = root, parent = null;
-        
-        while(cur != null){
-            if(val < cur.val){
-                parent = cur;
-                cur = cur.left;
-            }else{
-                parent = cur;
-                cur = cur.right;
-            }
-        }
-        
-        if(val > parent.val){
-            parent.right = new TreeNode(val);
+        if(val < root.val){
+            root.left = insertIntoBST(root.left,val);
         }else{
-            parent.left = new TreeNode(val);
+            root.right = insertIntoBST(root.right,val);
         }
         
         return root;
+        
+//         if(root == null){
+//             return new TreeNode(val);
+//         }
+        
+//         TreeNode cur = root, parent = null;
+        
+//         while(cur != null){
+//             if(val < cur.val){
+//                 parent = cur;
+//                 cur = cur.left;
+//             }else{
+//                 parent = cur;
+//                 cur = cur.right;
+//             }
+//         }
+        
+//         if(val > parent.val){
+//             parent.right = new TreeNode(val);
+//         }else{
+//             parent.left = new TreeNode(val);
+//         }
+        
+//         return root;
     }
 }
