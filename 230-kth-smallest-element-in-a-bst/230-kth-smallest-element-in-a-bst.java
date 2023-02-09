042eaaae2af20435376fda4ef66125d893adede4
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-02-09 08:46:59 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (51.41%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -17,11 +17,49 @@ class Solution {
     static int ans = -1;
     static int k = -1;
     public int kthSmallest(TreeNode root, int K) {
-        k = K;
         
-        inOrder(root);
+        // TC - O(N) ... SC - O(N) ...
         
-        return ans;
+//         k = K;
+        
+//         inOrder(root);
+        
+//         return ans;
+        
+        
+        // Morris Traversal... TC -> O(N) ... SC -> O(1)...
+        
+        TreeNode cur = root;
+        
+        int cnt = 0;
+        
+        while(cur != null){
+            if(cur.left==null){
+                cnt++;
+                if(cnt==K){
+                    return cur.val;
+                }
+                cur = cur.right;
+            }else{
+                TreeNode temp = cur.left;
+                while(temp.right != null && temp.right != cur){
+                    temp = temp.right;
+                }
+                if(temp.right == null){
+                    temp.right = cur;
+                    cur = cur.left;
+                }else{
+                    temp.right = null;
+                    cnt++;
+                    if(cnt==K){
+                        return cur.val;
+                    }
+                    cur = cur.right;
+                }
+            }
+        }
+        
+        return -1;
         
     }
     public static void inOrder(TreeNode root){
