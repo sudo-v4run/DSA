07f8f5cc67bd68815da5c8a99dcce53f76284b3d
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-02-10 21:22:40 +0530

    Time: 7 ms (57.98%), Space: 44.7 MB (23.54%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,29 +8,70 @@
  * }
  */
 class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(root == null){
+//             return null;
+//         }
+        
+//         if((root.val == p.val) || (root.val == q.val)){
+//             return root;
+//         }
+        
+//         TreeNode left = lowestCommonAncestor(root.left,p,q);
+//         TreeNode right = lowestCommonAncestor(root.right,p,q);
+        
+//         if(left != null && right != null){
+//             return root;
+//         }
+        
+//         if(left != null){
+//             return left;
+//         }
+//         if(right != null){
+//             return right;
+//         }
+        
+//         return null;
+//     }
+    
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null){
-            return null;
+        ArrayList<TreeNode> pArr = new ArrayList<>();
+        ArrayList<TreeNode> qArr = new ArrayList<>();
+        
+        findPath(root,p,pArr);
+        findPath(root,q,qArr);
+        
+        int i = pArr.size()-1;
+        int j = qArr.size()-1;
+        
+        while(i>=0 && j>=0 && pArr.get(i)==qArr.get(j)){
+            i--;
+            j--;
         }
         
-        if((root.val == p.val) || (root.val == q.val)){
-            return root;
-        }
-        
-        TreeNode left = lowestCommonAncestor(root.left,p,q);
-        TreeNode right = lowestCommonAncestor(root.right,p,q);
-        
-        if(left != null && right != null){
-            return root;
-        }
-        
-        if(left != null){
-            return left;
-        }
-        if(right != null){
-            return right;
-        }
-        
-        return null;
+        return pArr.get(i+1);
     }
+    public static boolean findPath(TreeNode root, TreeNode x, 
+                                        ArrayList<TreeNode> arr){
+        
+        if(root == null){
+            return false;
+        }
+        
+        if(root.val == x.val){
+            arr.add(root);
+            return true;
+        }
+        
+        boolean left = findPath(root.left,x,arr);
+        boolean right = findPath(root.right,x,arr);
+        
+        if(left || right){
+            arr.add(root);
+            return true;
+        }
+        
+        return false;
+    }
+    
 }
