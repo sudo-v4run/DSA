91fc86641fdc7502ac813a3c380b5f93f64e3bce
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-02-22 20:46:12 +0530

    Time: 185 ms (8.38%), Space: 62.8 MB (80.52%) - LeetHub

---
+++ 834-sum-of-distances-in-tree/834-sum-of-distances-in-tree.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int[] sumOfDistancesInTree(int n, int[][] edges) {
+        
+        HashMap<Integer,ArrayList<Integer>> adj = new HashMap<>();
+        
+        if(n==1){
+            return new int[]{0};
+        }
+        
+        for(int row[] : edges){
+            if(!adj.containsKey(row[0])){
+                adj.put(row[0],new ArrayList<Integer>());
+            }
+            if(!adj.containsKey(row[1])){
+                adj.put(row[1],new ArrayList<Integer>());
+            }
+            adj.get(row[0]).add(row[1]);
+            adj.get(row[1]).add(row[0]);
+        }
+        
+        System.out.println(adj);
+        
+        int cnt[] = new int[n];
+        int res[] = new int[n];
+        
+        postdfs(0,adj,cnt,res,-1);
+        
+        res[0] -= cnt[0];
+        
+        f(0,adj,cnt,res,-1,n);
+        
+        return res;
+    }
+    public static void postdfs( int root, HashMap<Integer,ArrayList<Integer>> adj,
+                                int cnt[], int res[], int prev ){
+        
+        if(adj.containsKey(root)){
+            for(int child : adj.get(root)){
+                if(child == prev)
+                    continue;
+                
+                postdfs(child,adj,cnt,res,root);
+                cnt[root] += cnt[child];
+            }
+        }
+        
+        cnt[root]++;
+        res[0] += cnt[root];
+    }
+    
+    public static void f( int root, HashMap<Integer,ArrayList<Integer>> adj,
+                                int cnt[], int res[], int prev, int n ){
+        
+        if(adj.containsKey(root)){
+            for(int child : adj.get(root)){
+                if(child == prev)
+                    continue;
+                
+                res[child] = res[root] - cnt[child] + (n - cnt[child]);
+                f(child,adj,cnt,res,root,n);
+            }
+        }
+    }
+}
