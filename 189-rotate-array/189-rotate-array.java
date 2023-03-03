68b4e7b80952456db0cde8f0c38219103706fb86
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-03-03 23:09:36 +0530

    Time: 0 ms (100.00%), Space: 59.3 MB (18.90%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -7,8 +7,10 @@ class Solution {
         rotate(arr,0,n-1);
         rotate(arr,0,k-1);
         rotate(arr,k,n-1);
+        
     }
     public static void rotate(int arr[],int s,int e){
+        
         while(s<=e){
             int temp = arr[s];
             arr[s] = arr[e];
