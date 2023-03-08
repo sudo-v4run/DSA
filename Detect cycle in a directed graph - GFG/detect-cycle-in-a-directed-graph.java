5ea617944603bf420f0bfea12d2acf53cd9eb585
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-03-08 21:32:50 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -33,19 +33,53 @@ class Solution {
         
         // BFS will not work for detecting cycles in directed graphs...
         // 0 -> 1 ; 0 -> 2 ; 1 -> 2 : Test Case eg where BFS fails 
+        // But modified BFS - Kahn's Algo (Topological Sort) can be used...
         
-        HashSet<Integer> hs = new HashSet<>();
-        int vis[] = new int[v];
+        
+        // BFS-Kahn's Algo..
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        int indeg[] = new int[v];
+        findInDeg(adj,indeg);
+        Queue<Integer> q = new LinkedList<>();
         
         for(int i = 0 ; i < v ; i++){
-            if(vis[i] != 1){
-                if(dfs(i,adj,vis,hs)){
-                    return true;
+            if(indeg[i] == 0){
+                q.add(i);
+            }
+        }
+        
+        while(!q.isEmpty()){
+            
+            int pop = q.poll();
+            res.add(pop);
+            
+            for(int nei : adj.get(pop)){
+                indeg[nei]--;
+                if(indeg[nei] == 0){
+                    q.add(nei);
                 }
             }
         }
         
-        return false;
+        return !(res.size() == v);
+        
+        
+        
+        // DFS...
+        
+        // HashSet<Integer> hs = new HashSet<>();
+        // int vis[] = new int[v];
+        
+        // for(int i = 0 ; i < v ; i++){
+        //     if(vis[i] != 1){
+        //         if(dfs(i,adj,vis,hs)){
+        //             return true;
+        //         }
+        //     }
+        // }
+        
+        // return false;
     }
     public static boolean dfs( int v, ArrayList<ArrayList<Integer>> adj, int vis[],
                                HashSet<Integer> hs ){
@@ -65,5 +99,13 @@ class Solution {
         hs.remove(v);
         
         return false;
+    }
+    public static void findInDeg(ArrayList<ArrayList<Integer>> adj, int indeg[]){
+        
+        for(ArrayList<Integer> neis : adj){
+            for(int nei : neis){
+                indeg[nei]++;
+            }
+        }
     }
 }
