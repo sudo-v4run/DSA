4535346d07696e2c03762855164ff737b5ddacc7
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-03-09 21:45:56 +0530

    Time: 4 ms (76.67%), Space: 43.2 MB (24.44%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
         int v = numCourses;
         
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
@@ -12,32 +13,53 @@ class Solution {
             adj.get(pair[1]).add(pair[0]);
         }
         
-        int indeg[] = new int[v];
-        findInDeg(adj,indeg);
         
-        Queue<Integer> q = new LinkedList<>();
+        // BFS...
         
-        for(int i = 0 ; i < numCourses ; i++){
-            if(indeg[i] == 0){
-                q.add(i);
-            }
-        }
+//         int indeg[] = new int[v];
+//         findInDeg(adj,indeg);
         
-        int cnt = 0;
+//         Queue<Integer> q = new LinkedList<>();
         
-        while(!q.isEmpty()){
-            int pop = q.poll();
-            cnt++;
+//         for(int i = 0 ; i < numCourses ; i++){
+//             if(indeg[i] == 0){
+//                 q.add(i);
+//             }
+//         }
+        
+//         int cnt = 0;
+        
+//         while(!q.isEmpty()){
+//             int pop = q.poll();
+//             cnt++;
             
-            for(int nei : adj.get(pop)){
-                indeg[nei]--;
-                if(indeg[nei] == 0){
-                    q.add(nei);
+//             for(int nei : adj.get(pop)){
+//                 indeg[nei]--;
+//                 if(indeg[nei] == 0){
+//                     q.add(nei);
+//                 }
+//             }
+//         }
+        
+//         return cnt == numCourses;
+        
+        
+        
+        // DFS...
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        HashSet<Integer> hs = new HashSet<>();
+        int vis[] = new int[v];
+        
+        for(int i = 0 ; i < v ; i++){
+            if(vis[i] != 1){
+                if(dfs(i,adj,res,vis,hs)){
+                    return false;
                 }
             }
         }
         
-        return cnt == numCourses;
+        return res.size() == numCourses;
     }
     public static void findInDeg(ArrayList<ArrayList<Integer>> adj,int indeg[] ){
         
@@ -46,5 +68,26 @@ class Solution {
                 indeg[nei]++;
             }
         }
+    }
+    public static boolean dfs(int v, ArrayList<ArrayList<Integer>> adj,
+                               ArrayList<Integer> res, int vis[], HashSet<Integer> hs){
+        
+        vis[v] = 1;
+        hs.add(v);
+        
+        for(int nei : adj.get(v)){
+            if(vis[nei] != 1){
+                if(dfs(nei,adj,res,vis,hs)){
+                    return true;
+                }
+            }else if(hs.contains(nei)){
+                return true;
+            }
+        }
+        
+        hs.remove(v);
+        res.add(v);
+        
+        return false;
     }
 }
