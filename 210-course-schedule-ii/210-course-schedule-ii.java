18e31ef3cd6ae1c9f0536d58126a2c780822260d
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-03-09 22:04:00 +0530

    Time: 4 ms (87.17%), Space: 43.8 MB (29.34%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -15,25 +15,55 @@ class Solution {
         
         // BFS...
         
-        int indeg[] = new int[v];
-        findInDeg(adj,indeg);
+//         int indeg[] = new int[v];
+//         findInDeg(adj,indeg);
         
-        Queue<Integer> q = new LinkedList<>();
+//         Queue<Integer> q = new LinkedList<>();
         
-        for(int i = 0 ; i < numCourses ; i++){
-            if(indeg[i] == 0){
-                q.add(i);
-            }
-        }
+//         for(int i = 0 ; i < numCourses ; i++){
+//             if(indeg[i] == 0){
+//                 q.add(i);
+//             }
+//         }
         
-        while(!q.isEmpty()){
-            int pop = q.poll();
-            res.add(pop);
+//         while(!q.isEmpty()){
+//             int pop = q.poll();
+//             res.add(pop);
             
-            for(int nei : adj.get(pop)){
-                indeg[nei]--;
-                if(indeg[nei] == 0){
-                    q.add(nei);
+//             for(int nei : adj.get(pop)){
+//                 indeg[nei]--;
+//                 if(indeg[nei] == 0){
+//                     q.add(nei);
+//                 }
+//             }
+//         }
+        
+//         int ans[] = new int[res.size()];
+//         int k = 0;
+        
+//         for(int ele : res){
+//             ans[k++] = ele;
+//         }
+        
+//         if(ans.length == numCourses){
+//             return ans;
+//         }
+//         return new int[]{};
+        
+        
+        
+        
+        
+        
+        // DFS...
+        
+        HashSet<Integer> hs = new HashSet<>();
+        int vis[] = new int[v];
+        
+        for(int i = 0 ; i < v ; i++){
+            if(vis[i] != 1){
+                if(dfs(i,adj,res,vis,hs)){
+                    return new int[]{};
                 }
             }
         }
@@ -41,13 +71,14 @@ class Solution {
         int ans[] = new int[res.size()];
         int k = 0;
         
-        for(int ele : res){
-            ans[k++] = ele;
+        for(int i = res.size()-1 ; i>=0 ; i--){
+            ans[k++] = res.get(i);
         }
         
         if(ans.length == numCourses){
             return ans;
         }
+        
         return new int[]{};
     }
     public static void findInDeg(ArrayList<ArrayList<Integer>> adj,int indeg[] ){
@@ -57,5 +88,26 @@ class Solution {
                 indeg[nei]++;
             }
         }
+    }
+    public static boolean dfs(int v, ArrayList<ArrayList<Integer>> adj,
+                              ArrayList<Integer> res, int vis[], HashSet<Integer> hs){
+        
+        vis[v] = 1;
+        hs.add(v);
+        
+        for(int nei : adj.get(v)){
+            if(vis[nei] != 1){
+                if(dfs(nei,adj,res,vis,hs)){
+                    return true;
+                }
+            }else if(hs.contains(nei)){
+                return true;
+            }
+        }
+        
+        hs.remove(v);
+        res.add(v);
+        
+        return false;
     }
 }
