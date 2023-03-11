bc3d00fdd639d988c662ab67a4a619751eb8f63a
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-03-11 21:53:59 +0530

    Time: 95 ms (64.20%), Space: 44.6 MB (57.07%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,50 +1,48 @@
 class Solution {
-    
     static class Pair{
         String str;
         int lvl;
         
-        Pair(String str, int lvl){
+        Pair(String str,int lvl){
             this.str = str;
             this.lvl = lvl;
         }
     }
-    
-    public int ladderLength(String beginWord, String endWord, 
-                                            List<String> wordList) {
-        
-        Queue<Pair> q = new LinkedList<>();
-        Set<String> hs = new HashSet();
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        HashSet<String> hs = new HashSet();
         
         for(String s : wordList){
-            hs.add(s);
+            if(!s.equals(beginWord))
+                hs.add(s);
         }
         
+        if(!hs.contains(endWord)){
+            return 0;
+        }
+        
+        Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(beginWord,1));
         
         while(!q.isEmpty()){
             Pair pop = q.poll();
-            String s = pop.str;
+            String curS = pop.str;
             int l = pop.lvl;
             
-            if(s.equals(endWord)){
+            if(curS.equals(endWord)){
                 return l;
             }
             
-            for(int i = 0 ; i < s.length() ; i++){
-                char[] chArray = s.toCharArray();
-                
-                for(char ch = 'a' ; ch <= 'z' ; ch++){
-                    chArray[i] = ch;
-                    String toString = new String(chArray);
-                    
-                    if(hs.contains(toString)){
-                        q.add(new Pair(toString,l+1));
-                        hs.remove(toString);
+            for(int i = 0 ; i < curS.length() ; i++){
+                char[] chArray = curS.toCharArray();
+                for(char alpha = 'a' ; alpha <='z' ; alpha++){
+                    chArray[i] = alpha;
+                    String temp = new String(chArray);
+                    if(!temp.equals(curS) && hs.contains(temp)){
+                        hs.remove(temp);
+                        q.add(new Pair(temp,l+1));
                     }
                 }
             }
-            
         }
         
         return 0;
