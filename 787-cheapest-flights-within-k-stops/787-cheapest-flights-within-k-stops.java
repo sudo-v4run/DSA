8ae89675f460028e7fd25734845cf0469cb8702d
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-03-16 19:23:20 +0530

    Time: 5 ms (85.84%), Space: 43.2 MB (77.45%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -21,6 +21,55 @@ class Solution {
     }
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         
+//         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+        
+//         for(int i = 0 ; i < n ; i++){
+//             adj.add(new ArrayList<>());
+//         }
+        
+//         for(int ed[] : flights){
+//             int u = ed[0];
+//             int v = ed[1];
+//             int wt = ed[2];
+            
+//             adj.get(u).add(new Pair(v,wt));
+//         }
+        
+//         int dist[] = new int[n];
+//         Arrays.fill(dist,(int)1e7);
+//         dist[src] = 0;
+        
+//         PriorityQueue<Tuple> pq = new PriorityQueue<>((a,b)->{return a.wt-b.wt;});
+//         pq.add(new Tuple(src,0,0));
+        
+//         while(!pq.isEmpty()){
+//             Tuple pop = pq.poll();
+//             int node = pop.node;
+//             int wt = pop.wt;
+//             int stops = pop.stops;
+            
+//             if(stops > k){
+//                 continue;
+//             }
+            
+//             for(Pair ed : adj.get(node)){
+//                 int adjWt = wt + ed.wt;
+                
+//                 if(adjWt < dist[ed.node]){
+//                     dist[ed.node] = adjWt;
+//                     pq.add(new Tuple(ed.node,adjWt,stops+1));
+//                 }
+//             }
+//         }
+        
+//         if(dist[dst] == (int)1e7){
+//             return -1;
+//         }
+//         return dist[dst];
+        
+        
+        
+        
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
         
         for(int i = 0 ; i < n ; i++){
