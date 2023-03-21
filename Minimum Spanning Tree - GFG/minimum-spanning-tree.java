daedef34440635d6182370ddc4165d94dc1f455b
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-03-21 21:26:05 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.java
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.java
@@ -1,167 +1,89 @@
 //{ Driver Code Starts
+
+
 import java.util.*;
 import java.io.*;
 import java.lang.*;
 
-class DriverClass
-{
+public class Main{
+	static BufferedReader br;
+	static PrintWriter ot;
     public static void main(String args[]) throws IOException {
-
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String str[] = read.readLine().trim().split(" ");
-            int V = Integer.parseInt(str[0]);
-            int E = Integer.parseInt(str[1]);
-    
-            ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<ArrayList<ArrayList<Integer>>>();
-            for(int i=0;i<V;i++)
-            {
-                adj.add(new ArrayList<ArrayList<Integer>>());
-            }
-            
-            int i=0;
-            while (i++<E) {
-                String S[] = read.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                int w = Integer.parseInt(S[2]);
-                ArrayList<Integer> t1 = new ArrayList<Integer>();
-                ArrayList<Integer> t2 = new ArrayList<Integer>();
-                t1.add(v);
-                t1.add(w);
-                t2.add(u);
-                t2.add(w);
-                adj.get(u).add(t1);
-                adj.get(v).add(t2);
-            }
-            
-            Solution ob = new Solution();
-            
-            System.out.println(ob.spanningTree(V, adj));
-        }
-    }
+		br = new BufferedReader(new InputStreamReader(System.in));
+		ot = new PrintWriter(System.out);
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-- > 0){
+			String s[] = br.readLine().trim().split(" ");
+			int V = Integer.parseInt(s[0]);
+			int E = Integer.parseInt(s[1]);
+			int edges[][] = new int[E][3];
+			for(int i = 0; i < E; i++){
+				s = br.readLine().trim().split(" ");
+				edges[i][0] = Integer.parseInt(s[0]);
+				edges[i][1] = Integer.parseInt(s[1]);
+				edges[i][2] = Integer.parseInt(s[2]);
+			}
+			ot.println(new Solution().spanningTree(V, E, edges));
+		}
+		ot.close();
+	}
 }
 // } Driver Code Ends
 
-class Solution
-{
-    // for Kruskal's Algo ..
-    static class Edge{
-        int u,v,w;
-        Edge(int u,int v, int w){
-            this.u = u;
-            this.v = v;
-            this.w = w;
+class Solution{
+    
+    static class Pair{
+        
+        int node,wt;
+        Pair(int node, int wt){
+            this.node = node;
+            this.wt = wt;
         }
     }
-    
-    // for Prims...
-    // static class Pair{
-    //     int v,w;
-    //     Pair(int v, int w){
-    //         this.v = v;
-    //         this.w = w;
-    //     }
-    // }
-    
-    static int spanningTree(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj) 
-    {
-        // kruskal's Algo ...
-        
-        ArrayList<Edge> ed = new ArrayList<>();
-        int par[] = new int[V];
-        for(int i = 0 ; i < V ; i++){
-            par[i] = i;
-        }
-        
-        for(int i = 0 ; i < V ; i++){
-            ArrayList<ArrayList<Integer>> nei = adj.get(i);
-            for(ArrayList<Integer> a : nei){
-                ed.add(new Edge(i,a.get(0),a.get(1)));
-            }
-        }
-        
-        int ans = 0;
-        
-        Collections.sort(ed,((a,b)->{return a.w-b.w;}));
-        
-        for(Edge i : ed){
-            if(find(par,i.u)!=find(par,i.v)){
-                unionSet(par,i.u,i.v);
-                ans+=i.w;
-            }
-        }
-        
-        return ans;
-        
-        
-        
-        
-        
-        
-        
-        // Prims ...
-        
-        // int key[] = new int[V];
-        // boolean mst[] = new boolean[V];
-        // //int parent[] = new int[V];
-        // int ans = 0;
-        
-        // for(int i = 0 ; i < V ;i++){
-        //     key[i] = 10000000;
-        //     mst[i] = false;
-        //     //parent[i] = -1;
-        // }
-        // key[0] = 0;
-        
-        // PriorityQueue<Pair> q = new PriorityQueue<>((a,b)->{return a.w-b.w;});
-        
-        // q.offer(new Pair(0,key[0]));
-        
-        // while(!q.isEmpty()){
-        //     Pair cur = q.poll();
-            
-        //     mst[cur.v] = true;
-            
-        //     ArrayList<ArrayList<Integer>> nei = adj.get(cur.v);
-            
-        //     for(ArrayList<Integer> a : nei){
-        //         int vr = a.get(0);
-        //         int wt = a.get(1);
-                
-        //         if(mst[vr]==false && key[vr]>wt){
-        //             key[vr]=wt;
-        //           // parent[vr]=cur.v;
-        //             q.offer(new Pair(vr,key[vr]));
-        //         }
-        //     }
-        // }
-        // for(int i=0; i < V ; i++){
-        //     ans+=key[i];
-        // }
-        // return ans;
-    }
-    
-    
-    static int find(int parent[],int x)
-    {
-          if(parent[x]!=x){
-              parent[x] = find(parent,parent[x]);
-          }
-          
-          return parent[x];
-	}
-	static void unionSet(int parent[],int x,int z)
-    {
-         int parX = find(parent,x);
-         int parZ = find(parent,z);
-         
-         if(parX==parZ){
-             return;
-         }
-         
-         parent[parX] = parZ;
+	static int spanningTree(int V, int E, int edges[][]){
+	    
+	    ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+	    
+	    for(int i = 0 ; i < V ; i++){
+	        adj.add(new ArrayList<>());
+	    }
+	    
+	    for(int ed[] : edges){
+	        
+	        int u = ed[0];
+	        int v = ed[1];
+	        int wt = ed[2];
+	        
+	        adj.get(u).add(new Pair(v,wt));
+	        adj.get(v).add(new Pair(u,wt));
+	    }
+	    
+	    PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->{return a.wt-b.wt;});
+	    pq.add(new Pair(0,0));
+	    
+	    int vis[] = new int[V];
+	    int sum = 0;
+	    
+	    while(!pq.isEmpty()){
+	        Pair pop = pq.poll();
+	        int node = pop.node;
+	        int wt = pop.wt;
+	        
+	        if(vis[node] == 1){
+	            continue;
+	        }
+	        
+	        vis[node] = 1;
+	        sum += wt;
+	        
+	        for(Pair nei : adj.get(node)){
+	            if(vis[nei.node] != 1){
+	                pq.add(new Pair(nei.node,nei.wt));
+	            }
+	        }
+	        
+	    }
+	    
+	    return sum;
 	}
 }
