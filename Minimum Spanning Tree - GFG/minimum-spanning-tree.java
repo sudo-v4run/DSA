da6cba5cb53dc66dafc6363ff861046de51a0ec6
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-03-21 21:39:14 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.java
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.java
@@ -32,6 +32,8 @@ public class Main{
 
 class Solution{
     
+    // Only to find Sum... To find MST edges also..check the below code...
+    
     static class Pair{
         
         int node,wt;
@@ -86,4 +88,82 @@ class Solution{
 	    
 	    return sum;
 	}
+	
+	
+	
+	
+	// Finding MST edges also...
+	
+//     static class Tuple{
+//         int node,wt,parent;
+//         Tuple(int node, int wt, int parent){
+//             this.node = node;
+//             this.wt = wt;
+//             this.parent = parent;
+//         }
+//     }
+//     static class Pair{
+        
+//         int first,second;
+//         Pair(int first, int second){
+//             this.first = first;
+//             this.second = second;
+//         }
+//     }
+// 	static int spanningTree(int V, int E, int edges[][]){
+	    
+// 	    ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+	    
+// 	    for(int i = 0 ; i < V ; i++){
+// 	        adj.add(new ArrayList<>());
+// 	    }
+	    
+// 	    for(int ed[] : edges){
+	        
+// 	        int u = ed[0];
+// 	        int v = ed[1];
+// 	        int wt = ed[2];
+	        
+// 	        adj.get(u).add(new Pair(v,wt));
+// 	        adj.get(v).add(new Pair(u,wt));
+// 	    }
+	    
+// 	    PriorityQueue<Tuple> pq = new PriorityQueue<>((a,b)->{return a.wt-b.wt;});
+// 	    pq.add(new Tuple(0,0,-1));
+	    
+// 	    int vis[] = new int[V];
+// 	    int sum = 0;
+	    
+// 	    ArrayList<Pair> mst = new ArrayList<>();
+	    
+// 	    while(!pq.isEmpty()){
+// 	        Tuple pop = pq.poll();
+// 	        int node = pop.node;
+// 	        int wt = pop.wt;
+// 	        int parent = pop.parent;
+	        
+// 	        if(vis[node] == 1){
+// 	            continue;
+// 	        }
+	        
+// 	        if(parent != -1)
+// 	            mst.add(new Pair(node,parent));
+	            
+// 	        vis[node] = 1;   
+// 	        sum += wt;
+	        
+// 	        for(Pair nei : adj.get(node)){
+// 	            if(vis[nei.first] != 1){
+// 	                pq.add(new Tuple(nei.first,nei.second,node));
+// 	            }
+// 	        }
+	        
+// 	    }
+	    
+// 	    for(Pair ed : mst){
+// 	        System.out.println(ed.first+" "+ed.second);
+// 	    }
+	    
+// 	    return sum;
+// 	}
 }
