e6fa7688d24408e591bc98504863fd7c563333f5
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-04-08 21:38:12 +0530

    Time: 8 ms (42.99%), Space: 42.8 MB (55.77%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,23 +1,42 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
+        
+        // without repeating characters == all unique characters...
+        // Similar to Longest Sustrinh with k unique characters...
+        // all unique characters means hm.size() == window size(j-i+1)...
+        // therefore k = window size...
+        
         HashMap<Character,Integer> hm = new HashMap<>();
+        int i = 0, j = 0;
+        int n = s.length();
+        int max = 0;
         
-        int len = 0;
-        int l = 0 ;
-        int r = 0;
-        
-        while(r<n){
-            char ch = s.charAt(r);
-            if(hm.containsKey(ch) && hm.get(ch)+1 > l){
-                l = hm.get(ch)+1;
-            }
-            hm.put(ch,r);
-            len = Math.max(len,r-l+1);
+        while(j < n){
             
-            r++;
+            char ch = s.charAt(j);
+            hm.put(ch,hm.getOrDefault(ch,0)+1);
+            
+            int k = j-i+1;    // k is window_size...condition for all unique chars...
+            
+            if(hm.size() > k){
+                // this condition never occurs..since hm size can't be more than window
+                // size at any given point...
+            }else if(hm.size() == k){
+                max = Math.max(max,j-i+1);
+                j++;
+            }else if(hm.size() < k){
+                while(hm.size() < j-i+1){
+                    char temp = s.charAt(i);
+                    hm.put(temp,hm.get(temp)-1);
+                    if(hm.get(temp) == 0){
+                        hm.remove(temp);
+                    }
+                    i++;
+                }
+                j++;
+            }
         }
         
-        return len;
+        return max;
     }
 }
