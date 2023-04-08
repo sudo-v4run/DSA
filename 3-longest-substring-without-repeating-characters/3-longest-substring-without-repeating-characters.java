3780160e09b28a34f185efd849d0931afb02946b
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-04-08 21:39:43 +0530

    Time: 8 ms (42.99%), Space: 42.6 MB (63.42%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -40,3 +40,28 @@ class Solution {
         return max;
     }
 }
+
+
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int n = s.length();
+//         HashMap<Character,Integer> hm = new HashMap<>();
+        
+//         int len = 0;
+//         int l = 0 ;
+//         int r = 0;
+        
+//         while(r<n){
+//             char ch = s.charAt(r);
+//             if(hm.containsKey(ch) && hm.get(ch)+1 > l){
+//                 l = hm.get(ch)+1;
+//             }
+//             hm.put(ch,r);
+//             len = Math.max(len,r-l+1);
+            
+//             r++;
+//         }
+        
+//         return len;
+//     }
+// }
