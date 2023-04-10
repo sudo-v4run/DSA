52bc3a06faddcabc3bafbbe30b4db894df52c70a
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-04-10 23:04:33 +0530

    Time: 63 ms (20.33%), Space: 52.2 MB (24.61%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        
+        // this Q boils down to the Q - Largest substring with k unique characters
+        // where k = 2 in this case...
+        
+        int k = 2;
+        int n = fruits.length;
+        int i = 0, j = 0;
+        int max = 0;
+        
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        
+        while(j < n){
+            hm.put(fruits[j],hm.getOrDefault(fruits[j],0)+1);
+            
+            if(hm.size() <= k){
+                max = Math.max(max,j-i+1);
+                j++;
+            }else if(hm.size() > k){
+                while(hm.size() > k){
+                    hm.put(fruits[i],hm.get(fruits[i])-1);
+                    if(hm.get(fruits[i]) == 0){
+                        hm.remove(fruits[i]);
+                    }
+                    i++;
+                }
+                j++;
+            }
+        }
+        
+        return max;
+    }
+}
