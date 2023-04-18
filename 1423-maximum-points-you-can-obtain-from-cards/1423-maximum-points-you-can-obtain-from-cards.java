3b05366d26b91a7a63de46a33ce7c22b7e0daaca
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-04-18 21:11:50 +0530

    Time: 2 ms (88.72%), Space: 52 MB (21.38%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -2,38 +2,57 @@ class Solution {
     public int maxScore(int[] arr, int k) {
         
         int n = arr.length;
-        int i = 0, j = n-1;
-        int sum = 0;
-        int left = 0;
-        int right = 0;
+        int i = 0 , j = n-k;
+        int total = 0;
+        for(int ind = j ; ind < n ; ind++){
+            total += arr[ind];
+        }
+        int max = total;
         
-        for(int ind = 0 ; ind < k ; ind++){
-            left += arr[ind];
+        while(j<n){
+            total = total + arr[i] - arr[j];
+            max = Math.max(max,total);
+            i++;
+            j++;
         }
         
-        for(int ind = n-1 ; ind >= n-k ; ind--){
-            right += arr[ind];
-        }
+        return max;
         
-        while(i <= j){
-            if(k == 0){
-                break;
-            }
+        
+        
+//         int n = arr.length;
+//         int i = 0, j = n-1;
+//         int sum = 0;
+//         int left = 0;
+//         int right = 0;
+        
+//         for(int ind = 0 ; ind < k ; ind++){
+//             left += arr[ind];
+//         }
+        
+//         for(int ind = n-1 ; ind >= n-k ; ind--){
+//             right += arr[ind];
+//         }
+        
+//         while(i <= j){
+//             if(k == 0){
+//                 break;
+//             }
             
-            if(left > right){
-                sum += arr[i];
-                left = left - arr[i];
-                right = right - arr[j-k+1];
-                i++;
-            }else{
-                sum += arr[j];
-                right = right - arr[j];
-                left = left - arr[i+k-1];
-                j--;
-            }
-            k--;
-        }
+//             if(left > right){
+//                 sum += arr[i];
+//                 left = left - arr[i];
+//                 right = right - arr[j-k+1];
+//                 i++;
+//             }else{
+//                 sum += arr[j];
+//                 right = right - arr[j];
+//                 left = left - arr[i+k-1];
+//                 j--;
+//             }
+//             k--;
+//         }
         
-        return sum;
+//         return sum;
     }
 }
