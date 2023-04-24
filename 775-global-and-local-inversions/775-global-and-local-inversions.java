d2cb53b133d273b215b25578d008890e3f11821c
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-04-24 22:07:34 +0530

    Time: 73 ms (9.96%), Space: 52.9 MB (9.96%) - LeetHub

---
+++ 775-global-and-local-inversions/775-global-and-local-inversions.java
@@ -0,0 +1,60 @@
+class Solution {
+    
+    static int gCnt;
+    static int lCnt;
+    
+    public boolean isIdealPermutation(int[] nums){
+        
+        gCnt = 0;
+        lCnt = 0;
+        int n = nums.length;
+        
+        for(int i = 0 ; i < n-1 ; i++){
+            if(nums[i] > nums[i+1]){
+                lCnt++;
+            }
+        }
+        
+        merge(nums,0,n-1);
+        
+        System.out.println(gCnt + "     "+lCnt);
+        
+        return gCnt == lCnt;
+    }
+    static void merge(int arr[], int l, int h){
+        if(l < h){
+            int mid = (l+h)/2;
+            
+            merge(arr,l,mid);
+            merge(arr,mid+1,h);
+            mergeSort(arr,l,mid,h);
+        }
+    }
+    static void mergeSort(int arr[], int l, int m, int h){
+        int left = l;
+        int right = m+1;
+        int n = (m-l+1) + (h-m+1+1);
+        int temp[] = new int[n];
+        int k = 0;
+        
+        while(left <= m && right <= h){
+            if(arr[left] > arr[right]){
+                gCnt += m - left + 1;
+                temp[k++] = arr[right++];
+            }else{
+                temp[k++] = arr[left++];
+            }
+        }
+        while(left <= m){
+            temp[k++] = arr[left++];
+        }
+        while(right <= h){
+            temp[k++] = arr[right++];
+        }
+        
+        k = 0;
+        for(int i = l ; i <= h ; i++){
+            arr[i] = temp[k++];
+        }
+    }
+}
