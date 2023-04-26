5bdc774ef4aa965ab9bdda577c32b86fbf973897
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-04-26 19:01:11 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (41.49%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,43 +1,22 @@
 class Solution {
-//     public double myPow(double x, int n) {
+    public double myPow(double x, int n) {
         
-//         if(n==0)
-//             return 1;
-        
-//         double ans = 1;
-            
-//         ans = myPow(x,Math.abs(n/2));
-//         ans = ans*ans;
-        
-//         if(n%2!=0)
-//             ans = ans*x;
-        
-//         if(n<0)
-//             return 1/ans;
-        
-//         return ans;
-        
-//     }
-    
-    
-    public double myPow(double x,int N){
-        double ans = 1;
-        long n = (long)N;
-        if(n<0){
-            n = -n;
-            x = 1/x;
+        if(n == 0){
+            return 1;
         }
         
-        while(n>0){
-            if(n%2==1){
-                ans = ans*x;
-            }
-            
-            x = x*x;
-            n = n/2;
+        double half = myPow(x,Math.abs(n/2));
+        
+        double ans = half * half;
+        
+        if(n % 2 != 0){
+            ans *= x;
         }
         
-        return ans;
+        if(n > 0){
+            return ans;
+        }
+        
+        return 1/ans;
     }
-    
 }
