2a088e6677fa449bc0db98336ef30d51a36b0828
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-04-26 22:39:14 +0530

    Time: 1 ms (99.47%), Space: 46.6 MB (34.20%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,19 +1,22 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        int cnt = 0 ;
-        int ans = nums[0];
-        for(int i = 0 ; i < nums.length ; i++){
-            if(cnt==0){
-                ans = nums[i];
-            }
-            if(nums[i]==ans){
+    public int majorityElement(int[] arr) {
+        
+        int n = arr.length;
+        int ans = arr[0];
+        int cnt = 0;
+        
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] == ans){
                 cnt++;
             }else{
                 cnt--;
+                if(cnt == 0){
+                    ans = arr[i];
+                    cnt++;
+                }
             }
         }
         
         return ans;
-        
     }
 }
