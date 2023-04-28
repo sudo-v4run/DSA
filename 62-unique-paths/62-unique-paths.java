12954c8e65b7e50d77aba310a01b3330864311a9
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-04-28 19:25:16 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (38.58%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -3,27 +3,7 @@ class Solution {
         
         // Tabulation....Space Optimized....
         
-        int prev[] = new int[n];
-        int cur[] = new int[n];
-        
-        for(int j = 0 ; j < n ; j++){
-            prev[j] = 1;
-        }
-        
-        cur[0] = 1;
-        
-        for(int i = 1; i < m ; i++){
-            for(int j = 1; j < n ; j++){
-                
-                int left = cur[j-1];
-                int up = prev[j];
 
-                cur[j] = left+up;
-            }
-            prev = cur.clone();
-        }
-        
-        return prev[n-1];
         
         
         
@@ -57,12 +37,12 @@ class Solution {
         
         // Memoization....
         
-//         int dp[][] = new int[m][n];
-//         for(int[] row:dp){
-//             Arrays.fill(row,-1);
-//         }
+        int dp[][] = new int[m][n];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
         
-//         return f(m-1,n-1,dp);
+        return f(m-1,n-1,dp);
         
         
         
@@ -72,19 +52,27 @@ class Solution {
     }
     public static int f(int m,int n,int dp[][]){
         
-        if(m<0 || n<0)
+        if(m < 0 || n < 0){
             return 0;
+        }
         
-        if(m==0 && n==0)
+        if(m == 0 && n == 0){
             return 1;
+        }
         
-        if(dp[m][n] != -1)
+        if(m == 0 || n == 0){
+            return 1;
+        }
+        
+        if(dp[m][n] != -1){
             return dp[m][n];
+        }
         
         int left = f(m,n-1,dp);
         int up = f(m-1,n,dp);
         
-        return dp[m][n] = left+up;
+        return dp[m][n] = left + up;
+        
         
         
         
