cf2a69ec9c2a3b43ec8955dee9da3457e29a902d
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-05-13 19:27:17 +0530

    Time: 1 ms (99.69%), Space: 45.4 MB (46.00%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -2,29 +2,43 @@
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         
-        int headASize = getSize(headA);
-        int headBSize = getSize(headB);
+        ListNode a = headA;
+        ListNode b = headB;
         
-        while(headASize != headBSize){
-            if(headASize > headBSize){
-                headA = headA.next;
-                headASize--;
-            }else{
-                headB = headB.next;
-                headBSize--;
-            }
+        while(a != b){
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
         }
         
-        while(headA != null && headB != null){
-            if(headA == headB){
-                return headA;
-            }
-            headA = headA.next;
-            headB = headB.next;
-        }
+        return a;
         
-        return null;
+        
+        
+        
+//         int headASize = getSize(headA);
+//         int headBSize = getSize(headB);
+        
+//         while(headASize != headBSize){
+//             if(headASize > headBSize){
+//                 headA = headA.next;
+//                 headASize--;
+//             }else{
+//                 headB = headB.next;
+//                 headBSize--;
+//             }
+//         }
+        
+//         while(headA != null && headB != null){
+//             if(headA == headB){
+//                 return headA;
+//             }
+//             headA = headA.next;
+//             headB = headB.next;
+//         }
+        
+//         return null;
     }
+    
     public static int getSize(ListNode head){
         if(head == null){
             return 0;
