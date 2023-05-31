fbb519e16cd3ca8fe7a37cf7f83cac9bbee29b07
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-05-31 21:46:15 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (58.79%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.java
@@ -1,43 +1,87 @@
 class Solution {
     public boolean checkValidString(String s) {
         
-        Stack<Integer> open = new Stack<>();
-        Stack<Integer> star = new Stack<>();
+        // Greedy...TC -> O(n) and SC -> O(1)...
         
-        for(int i = 0 ; i < s.length() ; i++){
+        int n = s.length();
+        
+        int leftMin = 0;
+        int leftMax = 0;
+        
+        for(int i = 0 ; i < n ; i++){
             char ch = s.charAt(i);
             
             if(ch == '('){
-                open.push(i);
+                leftMin++;
+                leftMax++;
             }else if(ch == '*'){
-                star.push(i);
-            }else{  // balancing ) paranthesis...
-                if(!open.isEmpty()){
-                    open.pop();
-                }else if(!star.isEmpty()){
-                    star.pop();
-                }else{
-                    return false;
-                }
+                leftMin--;  // * == )
+                leftMax++;  // * == (
+            }else{
+                leftMin--;
+                leftMax--;
             }
-        }
-        
-        // balancing ( paranthesis...
-        
-        while(!open.isEmpty()){
             
-            if(star.isEmpty()){
+            if(leftMax < 0){
+                // if leftMax < 0, it means that there were more right parenthesis
+                // '(' can't be more than ')' at any point... 
                 return false;
             }
-            if(star.peek() < open.peek()){    
-                    // * should be present to the right of ( to be valid..
-                    // so if index of * < index of ( , return false...
-                return false;
+            
+            if(leftMin < 0){
+                    
+                // leftMin can't be -ve to be valid .i.e we choose the 
+                // * wrongly to be ( . So don't consider it.i.e make the 
+                // leftMin = 0...
+                // coz leftMin and leftMax represents the valid range of 
+                // possibilities...
+
+                leftMin = 0;
             }
-            star.pop();
-            open.pop();
         }
         
-        return true;
+        return leftMin == 0;
+        
+        
+        // Using Stacks...TC and SC -> O(n)...
+        
+//         Stack<Integer> open = new Stack<>();
+//         Stack<Integer> star = new Stack<>();
+        
+//         for(int i = 0 ; i < s.length() ; i++){
+//             char ch = s.charAt(i);
+            
+//             if(ch == '('){
+//                 open.push(i);
+//             }else if(ch == '*'){
+//                 star.push(i);
+//             }else{  // balancing ) paranthesis...
+//                 if(!open.isEmpty()){
+//                     open.pop();
+//                 }else if(!star.isEmpty()){
+//                     star.pop();
+//                 }else{
+//                     return false;
+//                 }
+//             }
+//         }
+        
+//         // balancing ( paranthesis...
+        
+//         while(!open.isEmpty()){
+            
+//             if(star.isEmpty()){
+//                 return false;
+//             }
+//             if(star.peek() < open.peek()){    
+//                     // * should be present to the right of ( to be valid..
+//                     // so if index of * < index of ( , return false...
+//                 return false;
+//             }
+//             star.pop();
+//             open.pop();
+//         }
+        
+//         return true;
     }
 }
