ae6dc23475f54cee87acd20ebfa4627db48e9dab
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-05-31 19:25:39 +0530

    Time: 2 ms (76.71%), Space: 56.5 MB (20.48%) - LeetHub

--- 860-lemonade-change/860-lemonade-change.java
+++ 860-lemonade-change/860-lemonade-change.java
@@ -1,20 +1,27 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
         
+        
         int n = bills.length;
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        int five = 0;
+        int ten = 0;
         
         for(int i = 0 ; i < n ; i++){
             int cur = bills[i];
-            hm.put(cur,hm.getOrDefault(cur,0)+1);
+            
+            if(cur == 5){
+                five++;
+            }else if(cur == 10){
+                ten++;
+            }
             
             while(cur != 5){
-                if(cur == 20 && hm.containsKey(10) && hm.get(10) > 0){
+                if(cur == 20 && ten > 0){
                     cur -= 10;
-                    hm.put(10,hm.get(10)-1);
-                }else if(hm.containsKey(5) && hm.get(5) > 0){
+                    ten--;
+                }else if(five > 0){
                     cur -= 5;
-                    hm.put(5,hm.get(5)-1);
+                    five--;
                 }else{
                     return false;
                 }
@@ -22,5 +29,30 @@ class Solution {
         }
         
         return true;
+        
+        
+        
+//         int n = bills.length;
+//         HashMap<Integer,Integer> hm = new HashMap<>();
+        
+//         for(int i = 0 ; i < n ; i++){
+//             int cur = bills[i];
+//             hm.put(cur,hm.getOrDefault(cur,0)+1);
+            
+//             while(cur != 5){
+//                 if(cur == 20 && hm.containsKey(10) && hm.get(10) > 0){
+//                     cur -= 10;
+//                     hm.put(10,hm.get(10)-1);
+//                 }else if(hm.containsKey(5) && hm.get(5) > 0){
+//                     cur -= 5;
+//                     hm.put(5,hm.get(5)-1);
+//                 }else{
+//                     return false;
+//                 }
+//             }
+//         }
+        
+//         return true;
+        
     }
 }
