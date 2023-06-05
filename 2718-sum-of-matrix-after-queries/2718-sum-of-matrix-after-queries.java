643a299f2ff5858c8ce105bcd6e6a84a9ddc69af
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-06-05 21:27:10 +0530

    Time: 23 ms (20.00%), Space: 74.3 MB (20.00%) - LeetHub

---
+++ 2718-sum-of-matrix-after-queries/2718-sum-of-matrix-after-queries.java
@@ -0,0 +1,29 @@
+class Solution {
+    public long matrixSumQueries(int n, int[][] queries) {
+        
+        HashSet<Integer> rvis = new HashSet<>();
+        HashSet<Integer> cvis = new HashSet<>();
+        
+        int rows = n;
+        int cols = n;
+        
+        long ans = 0;
+        
+        for(int i = queries.length-1 ; i >= 0 ; i--){
+            
+            int type = queries[i][0];
+            int ind = queries[i][1];
+            int val = queries[i][2];
+
+            if(type == 0 && !rvis.contains(ind)){
+                ans += n*val - val*cvis.size();
+                rvis.add(ind);
+            }else if(type == 1 && !cvis.contains(ind)){
+                ans += n*val - val*rvis.size();
+                cvis.add(ind);
+            }
+        }
+        
+        return ans;
+    }
+}
