f3771e857fcd32d19bb73087397981e81274b8b2
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-06-12 18:41:01 +0530

    Time: 21 ms (83.33%), Space: 48.6 MB (16.67%) - LeetHub

--- 2734-lexicographically-smallest-string-after-substring-operation/2734-lexicographically-smallest-string-after-substring-operation.java
+++ 2734-lexicographically-smallest-string-after-substring-operation/2734-lexicographically-smallest-string-after-substring-operation.java
@@ -11,11 +11,16 @@ class Solution {
         }
         
         if(i >= n){
-            chArr[n-1] = 'z';
+            chArr[n-1] = 'z';       
             return new String(chArr);
+            
+            // we do this coz we have to perform the operation atleast once.
+            // so if there are all a's but since we need the perform the operation
+            // atleast once, we change the last a to z.
+            // coz Q is about returning the lexicographically smallest after performing
+            // the operation. Though aa < az, az is the smallest after performing the
+            // operation.
         }
-        
-        System.out.println(i);
         
         while(i < n && chArr[i] != 'a'){
             chArr[i]--;
