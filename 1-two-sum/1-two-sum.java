1514ac6ea692f7d21eba4fb89ac02ff2e36faa73
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-06-24 20:48:10 +0530

    Time: 1 ms (99.39%), Space: 43.7 MB (48.01%) - LeetHub

---
+++ 1-two-sum/1-two-sum.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int ans[] = new int[2];
+
+        // TC and SC -> O(N) ...
+        
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        
+        for(int i = 0 ; i < nums.length; i++){
+            
+            if(hm.containsKey(target-nums[i])){
+                ans[0] = hm.get(target-nums[i]);
+                ans[1] = i;
+                break;
+            }
+            hm.put(nums[i],i);
+        }
+        
+        return ans;
+        
+        
+        
+        /* O(N.logN).... This is soultion is only for booelan return type.
+                                    Coz indices' position changes when sorted.*/
+        // Using 2 pointers approach....
+        
+//         Arrays.sort(nums);
+//         int s = 0;
+//         int e = nums.length-1;
+        
+//         while(s<e){
+//             if(nums[s]+nums[e]==target){
+//                 return true;
+//             }
+//             if(nums[s]+nums[e]<target){
+//                 s++;
+//             }else{
+//                 e--;
+//             }
+//         }
+        
+//         return false;
+        
+        
+        
+        
+        
+        
+        
+         /* O(N.logN) .... This is soultion is only fore booelan return type.
+                                    Coz indices' position changes when sorted.*/
+        
+        // Using Binary Search...
+        
+//         Arrays.sort(nums);
+        
+        
+//         for(int i = 0 ; i < n ; i++){
+//             if(bs(nums,target-nums[i]))
+//                 return true;
+//         }
+//         return false;
+        
+    }
+}
