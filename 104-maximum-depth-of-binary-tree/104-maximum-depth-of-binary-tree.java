23978e7b7f35acd911d629258c4768b261c941c7
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-07-02 06:53:24 +0530

    Update 104-maximum-depth-of-binary-tree.java

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.java
@@ -7,7 +7,7 @@ class Solution {
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
         
-        return 1+Math.max(left,right);
+        return 1 + Math.max(left,right);
         
        
         
