8d8e74ed79f5ad1406815014febe134badce1ff0
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-07-06 22:55:14 +0530

    Time: 39 ms (56.47%), Space: 57 MB (67.78%) - LeetHub

---
+++ 2762-continuous-subarrays/2762-continuous-subarrays.java
@@ -0,0 +1,40 @@
+class Solution {
+    public long continuousSubarrays(int[] arr) {
+        
+        int n = arr.length;
+        
+        return f(arr);
+    }
+    public static long f(int arr[]){
+        // here don't need k explicitly since we know k = 2.
+        
+        int n = arr.length;
+        TreeMap<Integer,Integer> tm = new TreeMap<>();
+        
+        long cnt = 0;
+        int i = 0;
+        int j = 0;
+        
+        while(j < n){
+            
+            tm.put(arr[j],tm.getOrDefault(arr[j],0)+1);
+            
+            if(tm.lastKey() - tm.firstKey() <= 2){
+                cnt += (j-i+1);
+                j++;
+            }else{
+                while(tm.lastKey() - tm.firstKey() > 2){
+                    tm.put(arr[i],tm.get(arr[i])-1);
+                    if(tm.get(arr[i]) == 0){
+                        tm.remove(arr[i]);
+                    }
+                    i++;
+                }
+                cnt += (j-i+1);
+                j++;
+            }
+        }
+        
+        return cnt;
+    }
+}
