ecfc0ffceeeb6c1cbd45b6870979f78f49d1b7cf
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-07-06 21:45:47 +0530

    Time: 307 ms (49.16%), Space: 56.2 MB (14.19%) - LeetHub

--- 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.java
+++ 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.java
@@ -11,7 +11,7 @@ class Solution {
         
         for(int i = 2 ; i*i <= n ; i++){
             if(isPrime[i] == 1){
-                for(int j = 2*i ; j <= n ; j = j+i){
+                for(int j = i*i ; j <= n ; j = j+i){    // j = 2*i
                     isPrime[j] = 0;
                 }
             }
