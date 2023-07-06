cf1ea5c9ba0c01be023ce68cd0348c39d4f531af
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-07-06 21:45:27 +0530

    Time: 319 ms (46.85%), Space: 56.1 MB (14.64%) - LeetHub

--- 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.java
+++ 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.java
@@ -1,12 +1,10 @@
 class Solution {
     
     static List<List<Integer>> res;
-    static ArrayList<Integer> ans;
     
     public List<List<Integer>> findPrimePairs(int n) {
         
         res = new ArrayList<>();
-        ans = new ArrayList<>();
         
         int isPrime[] = new int[n+1];
         Arrays.fill(isPrime,1);
@@ -27,39 +25,5 @@ class Solution {
         }
         
         return res;
-        
-    
-
-        
-//         for(int i = 2 ; i <= n ; i++){
-//             if(isPrime(i)){
-//                 ans.add(i);
-//                 f(i,n-i);
-//                 ans.remove(ans.size()-1);
-//             }
-//         }
-        
-//         return res;
-    }
-    public static void f(int i, int sum){
-        
-        for(int j = i ; j <= sum ; j++ ){
-            if(isPrime(j) && sum-j == 0){
-                ans.add(j);
-                res.add(new ArrayList<>(ans));
-                ans.remove(ans.size()-1);
-                return;
-            }
-        }
-    }
-    public static boolean isPrime(int n){
-        
-        for(int i = 2 ; i <= n-1 ; i++){
-            if(n % i == 0){
-                return false;
-            }
-        }
-        
-        return true;
     }
 }
