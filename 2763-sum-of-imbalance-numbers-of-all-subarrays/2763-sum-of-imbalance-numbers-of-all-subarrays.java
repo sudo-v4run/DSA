06c52470e9d4c4b0166a9b0d8fe8587a5a21f9eb
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-07-07 21:02:11 +0530

    Time: 149 ms (55.70%), Space: 43.9 MB (47.61%) - LeetHub

--- 2763-sum-of-imbalance-numbers-of-all-subarrays/2763-sum-of-imbalance-numbers-of-all-subarrays.java
+++ 2763-sum-of-imbalance-numbers-of-all-subarrays/2763-sum-of-imbalance-numbers-of-all-subarrays.java
@@ -7,9 +7,9 @@ class Solution {
         for(int i = 0 ; i < n ; i++){
             HashSet<Integer> hs = new HashSet<>();
             int cnt = 0;
-            hs.add(arr[i]);
-            for(int j = i+1 ; j < n ; j++){
-                if(!hs.contains(arr[j])){
+            // hs.add(arr[i]);
+            for(int j = i ; j < n ; j++){
+                if(hs.size() >= 1 && !hs.contains(arr[j])){
                     int d = 1;
                     if(hs.contains(arr[j]-1)) d--;
                     if(hs.contains(arr[j]+1)) d--;
