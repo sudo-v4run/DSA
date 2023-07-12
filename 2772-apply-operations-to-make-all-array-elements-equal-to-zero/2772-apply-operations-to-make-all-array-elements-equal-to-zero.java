b4f314c74a28a96c83715aaf34a8c81b6179c58d
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-07-12 15:47:22 +0530

    Time: 2 ms (100.00%), Space: 58.8 MB (14.81%) - LeetHub

---
+++ 2772-apply-operations-to-make-all-array-elements-equal-to-zero/2772-apply-operations-to-make-all-array-elements-equal-to-zero.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean checkArray(int[] arr, int k) {
+        
+        int n = arr.length;
+        
+        int impact = 0;
+        int i = 0;
+        int j = 0;
+        
+        while(j < n){
+            
+            arr[j] -= impact;
+            if(arr[j] < 0){
+                return false;
+            }
+            impact += arr[j];
+            
+            if(j >= k-1){
+                impact -= arr[i];
+                i++;
+            }
+            j++;
+        }
+        
+        return impact == 0;
+    }
+}
