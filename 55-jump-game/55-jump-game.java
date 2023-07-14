e059ec295794095fcce49dc8e8f0d40383094afd
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-07-14 21:31:23 +0530

    Time: 1 ms (99.98%), Space: 45 MB (7.02%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -4,6 +4,7 @@ class Solution {
         // We can do this Q using DP also...
         
         int n = arr.length;
+        
         int goal = n-1;
         
         for(int i = n-2 ; i >= 0 ; i--){
