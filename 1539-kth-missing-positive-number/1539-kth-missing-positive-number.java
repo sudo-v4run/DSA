007eed79a79a5a1298b46d7e945b1059b8f4c6e9
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-07-22 20:30:31 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (50.75%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -3,13 +3,32 @@ class Solution {
         
         int n = arr.length;
         
-        for(int i = 0 ; i < n ; i++){
-            if(arr[i] <= k){
-                k++;
+        
+        // Another approach -> TC - O(N) 2 pointers ...
+        
+        int i = 1;
+        int j = 0;
+        while(k > 0){
+            if(j < n && i == arr[j]){
+                i++;
+                j++;
             }else{
-                return k;
+                i++;
+                k--;
             }
         }
-        return k;
+        
+        return i-1;
+        
+        // TC -> O(N)...
+        
+        // for(int i = 0 ; i < n ; i++){
+        //     if(arr[i] <= k){
+        //         k++;
+        //     }else{
+        //         return k;
+        //     }
+        // }
+        // return k;
     }
 }
