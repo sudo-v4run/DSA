b8ccdd0f22d97f9c8a0c9a7b801c1cd67f4c9fef
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-08-04 10:00:51 +0530

    Time: 1 ms (100.00%), Space: 42 MB (65.06%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.java
@@ -1,30 +1,73 @@
 class Solution {
     public int longestValidParentheses(String s) {
         
-        // 2 stacks..
-
-        Stack<Character> chr = new Stack<>();
-        Stack<Integer> ind = new Stack<>();
-        ind.push(-1);
+        // 2 variables...
+        
+        int open = 0;
+        int close = 0;
         
         int max = 0;
-        for(int i = 0 ; i < s.length() ; i++){
-            char ch = s.charAt(i);
+        
+        for(char ch : s.toCharArray()){
             if(ch == '('){
-                chr.push('(');
-                ind.push(i);
+                open++;
             }else if(ch == ')'){
-                if(!chr.isEmpty() && chr.peek() == '('){
-                    chr.pop();
-                    ind.pop();
-                    int len = i-ind.peek();
-                    max = Math.max(max,len);
-                }else{
-                    ind.push(i);
-                }
+                close++;
+            }
+            
+            if(open == close){
+                max = Math.max(max,2*open);
+            }else if(close > open){
+                open = close = 0;
             }
         }
-
+        
+        open = close = 0;
+        
+        for(int i = s.length()-1 ; i >= 0 ; i--){
+            char ch = s.charAt(i);
+            
+            if(ch == '('){
+                open++;
+            }else if(ch == ')'){
+                close++;
+            }
+            
+            if(open == close){
+                max = Math.max(max,2*open);
+            }else if(open > close){
+                open = close = 0;
+            }
+        }
+        
         return max;
+        
+        
+        
+        // 2 stacks..
+
+//         Stack<Character> chr = new Stack<>();
+//         Stack<Integer> ind = new Stack<>();
+//         ind.push(-1);
+        
+//         int max = 0;
+//         for(int i = 0 ; i < s.length() ; i++){
+//             char ch = s.charAt(i);
+//             if(ch == '('){
+//                 chr.push('(');
+//                 ind.push(i);
+//             }else if(ch == ')'){
+//                 if(!chr.isEmpty() && chr.peek() == '('){
+//                     chr.pop();
+//                     ind.pop();
+//                     int len = i-ind.peek();
+//                     max = Math.max(max,len);
+//                 }else{
+//                     ind.push(i);
+//                 }
+//             }
+//         }
+
+//         return max;
     }
 }
