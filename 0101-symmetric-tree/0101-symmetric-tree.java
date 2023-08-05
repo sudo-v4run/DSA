9d844da6e2158ae3dbb4f5edc5ecb3a36a43f11d
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-08-05 07:07:12 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (76.97%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        
+        return isSymmtrc(root.left,root.right);
+    }
+    public static boolean isSymmtrc(TreeNode left, TreeNode right){
+        
+        if(left==null && right==null)
+            return true;
+        if(left==null || right==null)
+            return false;
+        
+        if(left.val!=right.val)
+            return false;
+        
+        return isSymmtrc(left.left,right.right) 
+               && isSymmtrc(left.right,right.left);
+    }
+}
