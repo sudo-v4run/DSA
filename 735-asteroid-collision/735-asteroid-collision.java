abaaf9b428cf803d443c45b22170c848cbb219dd
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-08-20 12:48:51 +0530

    Time: 7 ms (70.59%), Space: 44 MB (68.77%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.java
+++ 735-asteroid-collision/735-asteroid-collision.java
@@ -5,43 +5,32 @@ class Solution {
         
         int n = asteroids.length;
         int i = 0;
-        
-        while(i < n && asteroids[i] < 0){
-            s.push(asteroids[i]);
-            i++;
-        }
-        
         while(i < n){
             int cur = asteroids[i];
-            if(cur < 0){
-                if(!s.isEmpty() && Math.abs(cur) < s.peek()){
-                    i++;
-                    continue;
-                }
-                while(!s.isEmpty() && s.peek() > 0 && s.peek() < Math.abs(cur)){
+            if(s.isEmpty()){
+                s.push(cur);
+            }else if(cur > 0){
+                s.push(cur);
+            }else if(cur < 0){
+                while(!s.isEmpty() && s.peek() > 0 && 
+                      s.peek() < Math.abs(cur)){
                     s.pop();
                 }
-                if(s.isEmpty()){
-                    s.push(cur);
-                }else if(s.peek() < 0){
+                if(s.isEmpty() || s.peek() < 0){
                     s.push(cur);
                 }else if(s.peek() == Math.abs(cur)){
                     s.pop();
                 }
-            }else{
-                s.push(cur);
             }
-            
             i++;
         }
         
-        int fn = s.size();
-        int res[] = new int[fn];
-        int k = fn-1;
+        int len = s.size();
+        int res[] = new int[len];
+        int k = len-1;
         
         while(!s.isEmpty()){
-            res[k] = s.pop();
-            k--;
+            res[k--] = s.pop();
         }
         
         return res;
