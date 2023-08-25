a12b44a359f8d9b4c444fc7bb6a3652d4ea4380a
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-08-25 19:08:04 +0530

    Time: 5 ms (98.55%), Space: 43.6 MB (92.20%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,33 +1,33 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
-//         if(root == null){
-//             return null;
-//         }
-        
-//         if(p.val < root.val && q.val < root.val){
-//             return lowestCommonAncestor(root.left,p,q);
-//         }
-        
-//         if(p.val > root.val && q.val > root.val){
-//             return lowestCommonAncestor(root.right,p,q);
-//         }
-        
-//         return root;
-        
-        TreeNode cur = root;
-        
-        while(cur != null){
-            
-            if(p.val < cur.val && q.val < cur.val){
-                cur = cur.left;
-            }else if(p.val > cur.val && q.val > cur.val){
-                cur = cur.right;
-            }else{
-                break;
-            }
+        if(root == null){
+            return null;
         }
         
-        return cur;
+        if(p.val < root.val && q.val < root.val){
+            return lowestCommonAncestor(root.left,p,q);
+        }
+        
+        if(p.val > root.val && q.val > root.val){
+            return lowestCommonAncestor(root.right,p,q);
+        }
+        
+        return root;
+        
+//         TreeNode cur = root;
+        
+//         while(cur != null){
+            
+//             if(p.val < cur.val && q.val < cur.val){
+//                 cur = cur.left;
+//             }else if(p.val > cur.val && q.val > cur.val){
+//                 cur = cur.right;
+//             }else{
+//                 break;
+//             }
+//         }
+        
+//         return cur;
     }
 }
