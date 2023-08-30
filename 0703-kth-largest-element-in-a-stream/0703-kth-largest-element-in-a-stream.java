25fca958256bcef61c18ed83d3de9df36fe7a11b
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-08-30 19:36:42 +0530

    Time: 13 ms (95.04%), Space: 46.6 MB (76.54%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -1,63 +1,84 @@
 class KthLargest {
-    TreeNode root;
+    
+    PriorityQueue<Integer> pq;
+    
     int k;
 
-    public KthLargest(int k, int[] nums) {
-        this.k = k;
-        for (int n : nums)
-        {
-            root = add(root, n);
+    public KthLargest(int K, int[] arr) {
+        k = K;
+        pq = new PriorityQueue<>();
+        
+        int n = arr.length;
+        
+        for(int i = 0 ; i < k && i < n ; i++){
+            pq.add(arr[i]);
+        }
+        
+        for(int i = k ; i < n ; i++){
+            if(arr[i] > pq.peek()){
+                pq.poll();
+                pq.add(arr[i]);
+            }
         }
     }
     
     public int add(int val) {
-        root = add(root, val);
-        return findKthLargest(root);
-    }
-    
-    private int findKthLargest(TreeNode root)
-    {
-        int count = k;
-        TreeNode iter = root;
         
-        while (count > 0)
-        {
-            int rightCount = (iter.right != null ? iter.right.count : 0);
-            int leftCount = (iter.left != null ? iter.left.count : 0);
-            
-            if (count <= rightCount) 
-            {
-                iter = iter.right;
-            }
-            else if (count <= (iter.count - leftCount)) break;
-            else
-            {
-                count -= (iter.count - leftCount);
-                iter = iter.left;
-            }
+        if(pq.size() < k){
+            pq.add(val);
+        }else if(val > pq.peek()){
+            pq.poll();
+            pq.add(val);
         }
         
-        return iter.val;
-    }
-     
-    private TreeNode add(TreeNode root, int val)
-    {
-        if (root == null) return new TreeNode(val);
-        root.count++;
-        
-        if (val < root.val) root.left = add(root.left, val);
-        if (val > root.val) root.right = add(root.right, val);
-        
-        return root;
-    }
-    
-    class TreeNode
-    {
-        int val, count = 1;
-        TreeNode left, right;
-        public TreeNode(int val)
-        {
-            this.val = val;
-        }
+        return pq.peek();
     }
 }
+
+
+// Using BST....
+
+// class KthLargest {
+//     TreeNode root;
+//     int k;
+//     public KthLargest(int k, int[] nums) {
+//         this.k = k;
+//         for (int num: nums) root = add(root, num);
+//     }
+
+//     public int add(int val) {
+//         root = add(root, val);
+//         return findKthLargest();
+//     }
+
+//     private TreeNode add(TreeNode root, int val) {
+//         if (root == null) return new TreeNode(val);
+//         root.count++;
+//         if (val < root.val) root.left = add(root.left, val);
+//         else root.right = add(root.right, val);
+
+//         return root;
+//     }
+
+//     public int findKthLargest() {
+//         int count = k;
+//         TreeNode walker = root;
+
+//         while (count > 0) {
+//             int pos = 1 + (walker.right != null ? walker.right.count : 0);
+//             if (count == pos) break;
+//             if (count > pos) {
+//                 count -= pos;
+//                 walker = walker.left;
+//             } else if (count < pos)
+//                 walker = walker.right;
+//         }
+//         return walker.val;
+//     }
+
+//     class TreeNode {
+//         int val, count = 1;
+//         TreeNode left, right;
+//         TreeNode(int v) { val = v; }
+//     }
+// }
