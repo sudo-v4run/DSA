945ac804777bf779cdddc0220392f33b2b0e3951
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-08-31 19:48:31 +0530

    Time: 27 ms (39.97%), Space: 41.1 MB (99.83%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -1,48 +1,77 @@
-
 class Solution {
-    public Node cloneGraph(Node root) {
+    public Node cloneGraph(Node root){
         
         if(root == null){
             return null;
         }
         
-        HashMap<Integer,Node> hm = new HashMap<>();
+        HashMap<Node,Node> hm = new HashMap<>();
         
-        it(root,hm);
-        
-        HashSet<Node> hs = new HashSet<>();
-        
-        f(root,hm,hs);
-        
-        return hm.get(root.val);
+        return dfs(root,hm);
     }
-    public static void it(Node root, HashMap<Integer,Node> hm){
-        
-        hm.put(root.val,new Node(root.val));
-        
-        for(Node nei : root.neighbors){
-            if(!hm.containsKey(nei.val)){
-                it(nei,hm);
-            }
-        }
-    }
-    public static void f(Node root, HashMap<Integer,Node> hm, HashSet<Node> hs){
-        
-        if(root == null){
-            return;
-        }
-        
+    public static Node dfs(Node root, HashMap<Node,Node> hm){
+
         ArrayList<Node> adj = new ArrayList<>();
         
-        hs.add(root);
+        Node clone = new Node(root.val);
+        hm.put(root,clone);
         
         for(Node nei : root.neighbors){
-            adj.add(hm.get(nei.val));
-            if(!hs.contains(nei)){
-                f(nei,hm,hs);
+            if(!hm.containsKey(nei)){
+                dfs(nei,hm);
             }
+            adj.add(hm.get(nei));
         }
         
-        hm.get(root.val).neighbors = adj;
+        clone.neighbors = adj;
+        
+        return clone;
     }
+    
+    
+//     public Node cloneGraph(Node root) {
+        
+//         if(root == null){
+//             return null;
+//         }
+        
+//         HashMap<Integer,Node> hm = new HashMap<>();
+        
+//         it(root,hm);
+        
+//         HashSet<Node> hs = new HashSet<>();
+        
+//         f(root,hm,hs);
+        
+//         return hm.get(root.val);
+//     }
+//     public static void it(Node root, HashMap<Integer,Node> hm){
+        
+//         hm.put(root.val,new Node(root.val));
+        
+//         for(Node nei : root.neighbors){
+//             if(!hm.containsKey(nei.val)){
+//                 it(nei,hm);
+//             }
+//         }
+//     }
+//     public static void f(Node root, HashMap<Integer,Node> hm, HashSet<Node> hs){
+        
+//         if(root == null){
+//             return;
+//         }
+        
+//         ArrayList<Node> adj = new ArrayList<>();
+        
+//         hs.add(root);
+        
+//         for(Node nei : root.neighbors){
+//             adj.add(hm.get(nei.val));
+//             if(!hs.contains(nei)){
+//                 f(nei,hm,hs);
+//             }
+//         }
+        
+//         hm.get(root.val).neighbors = adj;
+//     }
 }
