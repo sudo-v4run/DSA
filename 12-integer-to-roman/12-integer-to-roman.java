ab53586265349040bb62befa11aa94c2d4c1f819
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-09-09 17:37:30 +0530

    Time: 5 ms (58.31%), Space: 43.6 MB (37.73%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -0,0 +1,69 @@
+class Solution {
+    class Pair{
+        int num;
+        String roman;
+        
+        Pair(int num, String roman){
+            this.num = num;
+            this.roman = roman;
+        }
+    }
+    Pair arr[];
+    String res;
+    public String intToRoman(int num) {
+        
+        arr = new Pair[13];
+        res = "";
+        
+        arr[0] = new Pair(1,"I");
+        arr[1] = new Pair(4,"IV");
+        arr[2] = new Pair(5,"V");
+        arr[3] = new Pair(9,"IX");
+        arr[4] = new Pair(10,"X");
+        arr[5] = new Pair(40,"XL");
+        arr[6] = new Pair(50,"L");
+        arr[7] = new Pair(90,"XC");
+        arr[8] = new Pair(100,"C");
+        arr[9] = new Pair(400,"CD");
+        arr[10] = new Pair(500,"D");
+        arr[11] = new Pair(900,"CM");
+        arr[12] = new Pair(1000,"M");
+        
+        f(num);
+        
+        return res;
+    }
+    public void f(int num){
+        
+        if(num == 0){
+            return;
+        }
+        
+        Pair romanPair = getRoman(num);
+        res += romanPair.roman;
+        num -= romanPair.num;
+        
+        f(num);
+    }
+    public Pair getRoman(int num){
+        
+        int s = 0;
+        int e = arr.length-1;
+        
+        Pair ans = new Pair(-1,"");
+        
+        while(s <= e){
+            
+            int mid = s+(e-s)/2;
+            
+            if(arr[mid].num <= num){
+                ans = new Pair(arr[mid].num,arr[mid].roman);
+                s = mid+1;
+            }else{
+                e = mid-1;
+            }
+        }
+        
+        return ans;
+    }
+}
