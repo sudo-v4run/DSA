4b22f6a18e7e12bff01586aeae4ed7b97a7d32fd
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-09-12 16:50:18 +0530

    Time: 95 ms (61.25%), Space: 40.9 MB (99.52%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -0,0 +1,81 @@
+class Solution {
+    public int maxCoins(int[] nums) {
+        
+//         int n = nums.length;
+//         int arr[] = new int[n+2];
+//         arr[0] = 1;
+//         arr[n+1] = 1;
+//         for(int i = 1 ; i <= n ; i++){
+//             arr[i] = nums[i-1];
+//         }
+        
+//         //Tabulation....
+        
+//         int dp[][] = new int[n+2][n+2];
+        
+//         for(int i = n ; i >= 1 ; i--){
+//             for(int j = 1 ; j <= n ; j++){
+                
+//                 if(i>j)
+//                     continue;               
+                
+//                 int maxi = Integer.MIN_VALUE;
+        
+//                 for(int index = i ; index<=j ; index++){
+//                     int coins = arr[index]*arr[i-1]*arr[j+1]+
+//                                     dp[i][index-1]+dp[index+1][j];
+//                     maxi = Math.max(maxi,coins);
+//                 }
+
+//                 dp[i][j] = maxi;
+//             }
+//         }
+        
+//         return dp[1][n];
+        
+        
+        
+        // Memoization....
+        
+        int n = nums.length;
+        
+        int dp[][] = new int[n][n];
+        
+        for(int row[] : dp){
+            Arrays.fill(row,-1);
+        }
+        
+        return f(0,n-1,nums,dp);
+    }
+    public static int f(int i, int j, int arr[], int dp[][]){
+        
+        if(i > j){
+            return 0;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        int max = 0;
+        
+        for(int k = i ; k <= j ; k++){
+            
+            int left = 1;
+            if(i-1 >= 0){
+                left = arr[i-1];
+            }
+            
+            int right = 1;
+            if(j+1 <= arr.length-1){
+                right = arr[j+1];
+            }
+            
+            int coins = left*arr[k]*right + f(i,k-1,arr,dp) + f(k+1,j,arr,dp);
+            
+            max = Math.max(max,coins);
+        }
+        
+        return dp[i][j] = max;
+    }
+}
