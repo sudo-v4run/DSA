90213a9622ce5be2234af33a1511fb0018641746
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-09-20 17:37:47 +0530

    Time: 4 ms (98.75%), Space: 47 MB (56.60%) - LeetHub

---
+++ div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence18-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsalesforce-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div155-min-stack/div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence18-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence10-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsalesforce-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div155-min-stack.java
@@ -0,0 +1,110 @@
+class MinStack {
+    
+    Stack<Long> st;
+    long mini;
+
+    public MinStack() {
+        st = new Stack<>();
+        mini = Long.MAX_VALUE;
+    }
+    
+    public void push(int value) {
+        
+        Long val = Long.valueOf(value);
+        if(st.isEmpty()){
+            st.push(val);
+            mini = val;
+        }else{
+            if(val<mini){
+                st.push(2*val-mini);
+                mini = val;
+            }else{
+                st.push(val);
+            }
+        }
+    }
+    
+    public void pop() {
+        
+        if(st.isEmpty()){
+            return;
+        }else{
+            Long val = st.pop();
+            if(val<mini){
+                mini = 2*mini-val;
+            }
+        }
+    }
+    
+    public int top() {
+        
+        Long val = st.peek();
+        if(val<mini){
+            return (int)mini;
+        }
+        
+        return val.intValue();
+    }
+    
+    public int getMin() {
+        
+        return (int)mini;
+    }
+    
+    
+    // SC- O(2N) -> use another stack to maintain minEles...
+
+    
+    // Single Stack -> Using Pair class...
+    
+//     class Pair{
+//         int val;
+//         int min;
+//         Pair(int val,int min){
+//             this.val = val;
+//             this.min = min;
+//         }
+//     }
+//     Stack<Pair> st;
+//     public MinStack() {
+//         st = new Stack<>();
+//     }
+    
+//     public void push(int val) {
+//         if(st.isEmpty()){
+//             st.push(new Pair(val,val));
+//         }else{
+//             int min = st.peek().min;
+//             if(val<=min){
+//                 st.push(new Pair(val,val));
+//             }else{
+//                 st.push(new Pair(val,min));
+//             }
+//         }
+//     }
+    
+//     public void pop() {
+//         if(st.isEmpty()){
+//             return;
+//         }else{
+//             st.pop();
+//         }
+//     }
+    
+//     public int top() {
+//         if(st.isEmpty()){
+//             return -1;
+//         }else{
+//             return st.peek().val;
+//         }
+//     }
+    
+//     public int getMin() {
+//         if(st.isEmpty()){
+//             return -1;
+//         }else{
+//             return st.peek().min;
+//         }
+//     }
+    
+}
