5544894f88c791917005dd290e746878d7a270b7
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-09-30 21:31:23 +0530

    Time: 62 ms (22.98%), Space: 45.5 MB (85.71%) - LeetHub

---
+++ div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div907-sum-of-subarray-minimums/div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div907-sum-of-subarray-minimums.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        
+        int n = arr.length;
+        int mod = (int)1e9+7;
+        
+        Stack<Integer> s = new Stack<>();
+        
+        int left[] = new int[n];
+        int right[] = new int[n];
+        
+        for(int i = 0 ; i < n ; i++){
+            while(!s.isEmpty() && arr[s.peek()] >= arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                left[i] = -1;
+            }else{
+                left[i] = s.peek();
+            }
+            s.push(i);
+        }
+        
+        s = new Stack<>();
+        
+        for(int i = n-1 ; i >= 0 ; i--){
+            while(!s.isEmpty() && arr[s.peek()] > arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                right[i] = n;
+            }else{
+                right[i] = s.peek();
+            }
+            s.push(i);
+        }
+        
+        long sum = 0;
+        
+        for(int i = 0 ; i < n ; i++){
+            
+            int leftBoundary = left[i]+1;
+            int rightBoundary = right[i]-1;
+            
+            int leftCnt = i-leftBoundary+1;
+            int rightCnt = rightBoundary-i+1;
+            
+            // if(leftCnt == 0){
+            //     leftCnt = 1;
+            // }
+            // if(rightCnt == 0){
+            //     rightCnt = 1;
+            // }
+            
+            long noOfSubarrays = (leftCnt*rightCnt)%mod;
+            long temp = (noOfSubarrays*arr[i])%mod;
+            
+            sum = (sum+temp)%mod;
+        }
+        
+        return (int)sum;
+    }
+}
