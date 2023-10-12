1c39a941de8fe00799ecb6e26a772d2491ad8e78
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-10-12 19:47:53 +0530

    Time: 31 ms (34.87%), Space: 43.5 MB (82.89%) - LeetHub

---
+++ div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1473-paint-house-iii/div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1473-paint-house-iii.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
+        
+        int dp[][][] = new int[m][n+1][m];
+        for(int a[][] : dp){
+            for(int b[] : a){
+                Arrays.fill(b,-1);
+            }
+        }
+        
+        int res = f(0,0,0,houses,cost,target,dp);
+        
+        return res == (int)1e7 ? -1 : res;
+    }
+    public static int f(int index, int prevColor, int cnt,
+                        int houses[], int cost[][], int target, int dp[][][]){
+        
+        if(cnt > target){
+            return (int)1e7;
+        }
+        
+        if(index == houses.length){
+            if(cnt != target){
+                return (int)1e7;
+            }
+            
+            return 0;
+        }
+        
+        if(dp[index][prevColor][cnt] != -1){
+            return dp[index][prevColor][cnt];
+        }
+        
+        int ans = (int)1e7;
+        
+        if(houses[index] != 0){
+            int newCnt = prevColor == houses[index] ? cnt : cnt+1;
+            
+            ans = f(index+1,houses[index],newCnt,houses,cost,target,dp);
+        }else{
+            
+            for(int j = 0 ; j < cost[0].length ; j++){
+                houses[index] = j+1;
+                int newCnt = prevColor == houses[index] ? cnt : cnt+1;
+                int cur = cost[index][j] + f(index+1,j+1,newCnt,houses,cost,target,dp);
+                houses[index] = 0;
+                ans = Math.min(ans,cur);
+            }
+        }
+        
+        return dp[index][prevColor][cnt] = ans;
+    }
+}
