04e508a70c2e17d92dccbcf707e74e7aeab437ca
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-10-24 13:03:43 +0530

    Time: 13 ms (31.55%), Space: 44.7 MB (86.75%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence21-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div658-find-k-closest-elements/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence21-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div658-find-k-closest-elements.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence21-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div658-find-k-closest-elements/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence21-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divuber-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div658-find-k-closest-elements.java
@@ -1,32 +1,152 @@
 class Solution {
+    class Pair{
+        int diff, ele;
+        Pair(int diff, int ele){
+            this.diff = diff;
+            this.ele = ele;
+        }
+    }
+    
     public List<Integer> findClosestElements(int[] nums, int k, int x) {
         
-        ArrayList<Integer> arr = new ArrayList<>();
+        // Using BS and 2 Prts...
         
-        for(int ele : nums){
-            arr.add(ele);
-        }
-        
-        Collections.sort(arr,(a,b)->{
-            if(Math.abs(a-x) < Math.abs(b-x)){
-                return -1;
-            }else if(Math.abs(a-x) == Math.abs(b-x) && a < b){
-                return -1;
-            }
-            return 1;
-        });
+        int n = nums.length;
         
         List<Integer> res = new ArrayList<>();
         
-        for(int ele : arr){
-            res.add(ele);
-            if(res.size() == k){
-                break;
+        int index = findIndex(nums,x);
+        
+        int i = index;
+        int j = index;
+        
+        while(k != 0){
+            if(i == j){
+                res.add(nums[index]);
+                i--;
+                j++;
+            }else if(i < 0){
+                res.add(nums[j]);
+                j++;
+            }else if(j > n-1){
+                res.add(nums[i]);
+                i--;
+            }else if(Math.abs(x-nums[i]) <= Math.abs(x-nums[j])){
+                res.add(nums[i]);
+                i--;
+            }else{
+                res.add(nums[j]);
+                j++;
             }
+            k--;
         }
         
         Collections.sort(res);
         
         return res;
+        
+        
+        // Using PQ....
+        
+//         PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->{
+//             if(a.diff < b.diff){
+//                 return 1;
+//             }else if(a.diff == b.diff && a.ele < b.ele){
+//                 return 1;
+//             }
+//             return -1;
+//         });
+        
+//         int n = nums.length;
+//         for(int i = 0 ; i < n ; i++){
+//             pq.add(new Pair(Math.abs(nums[i]-x),nums[i]));
+            
+//             if(pq.size() > k){
+//                 pq.poll();
+//             }
+//         }
+        
+//         List<Integer> res = new ArrayList<>();
+        
+//         while(!pq.isEmpty()){
+//             res.add(pq.poll().ele);
+//         }
+        
+//         Collections.sort(res);
+        
+//         return res;
+        
+        // Brute Force...
+        
+        
+//         ArrayList<Integer> arr = new ArrayList<>();
+        
+//         for(int ele : nums){
+//             arr.add(ele);
+//         }
+        
+//         Collections.sort(arr,(a,b)->{
+//             if(Math.abs(a-x) < Math.abs(b-x)){
+//                 return -1;
+//             }else if(Math.abs(a-x) == Math.abs(b-x) && a < b){
+//                 return -1;
+//             }
+//             return 1;
+//         });
+        
+//         List<Integer> res = new ArrayList<>();
+        
+//         for(int ele : arr){
+//             res.add(ele);
+//             if(res.size() == k){
+//                 break;
+//             }
+//         }
+        
+//         Collections.sort(res);
+        
+//         return res;
+    }
+    public int findIndex(int arr[], int x){
+        
+        int n = arr.length;
+        
+        if(x < arr[0]){
+            return 0;
+        }
+        if(x > arr[n-1]){
+            return n-1;
+        }
+        
+        int ans = -1;
+        int s = 0;
+        int e = n-1;
+        while(s <= e){
+            int mid = s+(e-s)/2;
+            
+            if(arr[mid] == x){
+                return mid;
+            }
+            
+            ans = mid;
+            
+            if(arr[mid] < x){
+                s = mid+1;
+            }else{
+                e = mid-1;
+            }
+        }
+        
+        if(ans-1 >= 0 && x < arr[ans]){
+            if(Math.abs(x-arr[ans-1]) <= Math.abs(x-arr[ans])){
+                ans = ans-1;
+            }
+        }else if(ans+1 <= n-1 && x > arr[ans]){
+            if(Math.abs(x-arr[ans+1]) < Math.abs(x-arr[ans])){
+                ans = ans+1;
+            }
+        }
+        
+        return ans;
     }
 }
