1d16119e29ac050740ed6a714072636ed05531bc
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-10-28 22:28:06 +0530

    Time: 42 ms (24.68%), Space: 54.9 MB (12.94%) - LeetHub

--- div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence127-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence49-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divasana-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsumologic-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div973-k-closest-points-to-origin/div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence127-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence49-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divasana-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsumologic-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div973-k-closest-points-to-origin.java
+++ div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence127-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence49-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divasana-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsumologic-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div973-k-closest-points-to-origin/div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence127-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence49-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence6-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divasana-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divlinkedin-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsumologic-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div973-k-closest-points-to-origin.java
@@ -8,35 +8,85 @@ class Solution {
     }
     public int[][] kClosest(int[][] points, int k) {
         
+        // Use QuickSelect for O(N) in Avg case and O(N^2) in worst case....
+        
         int n = points.length;
-        Pair[] arr = new Pair[n];
+        return quickSelect(0,n-1,k-1,points);
         
-        for(int i = 0 ; i < n ; i++){
-            arr[i] = new Pair(points[i][0],points[i][1]);
-        }
+        // PQ...TC-> O(n.logk)...
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->{
-            return (b.x*b.x + b.y*b.y)-(a.x*a.x + a.y*a.y);
-        });
+//         int n = points.length;
+//         Pair[] arr = new Pair[n];
         
-        int res[][] = new int[k][2];
-        int j = 0;
+//         for(int i = 0 ; i < n ; i++){
+//             arr[i] = new Pair(points[i][0],points[i][1]);
+//         }
         
-        for(Pair p : arr){
-            pq.add(p);
+//         PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->{
+//             return (b.x*b.x + b.y*b.y)-(a.x*a.x + a.y*a.y);
+//         });
+        
+//         int res[][] = new int[k][2];
+//         int j = 0;
+        
+//         for(Pair p : arr){
+//             pq.add(p);
             
-            if(pq.size() > k){
-                pq.poll();
+//             if(pq.size() > k){
+//                 pq.poll();
+//             }
+//         }
+        
+//         while(!pq.isEmpty()){
+//             Pair cur = pq.poll();
+//             res[j][0] = cur.x;
+//             res[j][1] = cur.y;
+//             j++;
+//         }
+        
+//         return res;
+    }
+    public int[][] quickSelect(int l, int h, int k, int points[][]){
+        
+        if(l <= h){
+            
+            int pivotIndex = quickSelectSort(l,h,points);
+            
+            if(pivotIndex == k){
+                return Arrays.copyOfRange(points,0,k+1);
+            }
+            if(k < pivotIndex){
+                return quickSelect(l,pivotIndex-1,k,points);
+            }else{
+                return quickSelect(pivotIndex+1,h,k,points);
             }
         }
         
-        while(!pq.isEmpty()){
-            Pair cur = pq.poll();
-            res[j][0] = cur.x;
-            res[j][1] = cur.y;
-            j++;
-        }
+        return points;
+    }
+    public int quickSelectSort(int l, int h, int arr[][]){
+        int i = l;
+        int j = h;
+        int pivot = arr[i][0]*arr[i][0] + arr[i][1]*arr[i][1];
         
-        return res;
+        while(i <= j){
+            while(i <= j && arr[i][0]*arr[i][0] + arr[i][1]*arr[i][1] <= pivot){
+                i++;
+            }
+            while(arr[j][0]*arr[j][0] + arr[j][1]*arr[j][1] > pivot){
+                j--;
+            }
+            
+            if(i < j){
+                int temp[] = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp[] = arr[l];
+        arr[l] = arr[j];
+        arr[j] = temp;
+        
+        return j;
     }
 }
