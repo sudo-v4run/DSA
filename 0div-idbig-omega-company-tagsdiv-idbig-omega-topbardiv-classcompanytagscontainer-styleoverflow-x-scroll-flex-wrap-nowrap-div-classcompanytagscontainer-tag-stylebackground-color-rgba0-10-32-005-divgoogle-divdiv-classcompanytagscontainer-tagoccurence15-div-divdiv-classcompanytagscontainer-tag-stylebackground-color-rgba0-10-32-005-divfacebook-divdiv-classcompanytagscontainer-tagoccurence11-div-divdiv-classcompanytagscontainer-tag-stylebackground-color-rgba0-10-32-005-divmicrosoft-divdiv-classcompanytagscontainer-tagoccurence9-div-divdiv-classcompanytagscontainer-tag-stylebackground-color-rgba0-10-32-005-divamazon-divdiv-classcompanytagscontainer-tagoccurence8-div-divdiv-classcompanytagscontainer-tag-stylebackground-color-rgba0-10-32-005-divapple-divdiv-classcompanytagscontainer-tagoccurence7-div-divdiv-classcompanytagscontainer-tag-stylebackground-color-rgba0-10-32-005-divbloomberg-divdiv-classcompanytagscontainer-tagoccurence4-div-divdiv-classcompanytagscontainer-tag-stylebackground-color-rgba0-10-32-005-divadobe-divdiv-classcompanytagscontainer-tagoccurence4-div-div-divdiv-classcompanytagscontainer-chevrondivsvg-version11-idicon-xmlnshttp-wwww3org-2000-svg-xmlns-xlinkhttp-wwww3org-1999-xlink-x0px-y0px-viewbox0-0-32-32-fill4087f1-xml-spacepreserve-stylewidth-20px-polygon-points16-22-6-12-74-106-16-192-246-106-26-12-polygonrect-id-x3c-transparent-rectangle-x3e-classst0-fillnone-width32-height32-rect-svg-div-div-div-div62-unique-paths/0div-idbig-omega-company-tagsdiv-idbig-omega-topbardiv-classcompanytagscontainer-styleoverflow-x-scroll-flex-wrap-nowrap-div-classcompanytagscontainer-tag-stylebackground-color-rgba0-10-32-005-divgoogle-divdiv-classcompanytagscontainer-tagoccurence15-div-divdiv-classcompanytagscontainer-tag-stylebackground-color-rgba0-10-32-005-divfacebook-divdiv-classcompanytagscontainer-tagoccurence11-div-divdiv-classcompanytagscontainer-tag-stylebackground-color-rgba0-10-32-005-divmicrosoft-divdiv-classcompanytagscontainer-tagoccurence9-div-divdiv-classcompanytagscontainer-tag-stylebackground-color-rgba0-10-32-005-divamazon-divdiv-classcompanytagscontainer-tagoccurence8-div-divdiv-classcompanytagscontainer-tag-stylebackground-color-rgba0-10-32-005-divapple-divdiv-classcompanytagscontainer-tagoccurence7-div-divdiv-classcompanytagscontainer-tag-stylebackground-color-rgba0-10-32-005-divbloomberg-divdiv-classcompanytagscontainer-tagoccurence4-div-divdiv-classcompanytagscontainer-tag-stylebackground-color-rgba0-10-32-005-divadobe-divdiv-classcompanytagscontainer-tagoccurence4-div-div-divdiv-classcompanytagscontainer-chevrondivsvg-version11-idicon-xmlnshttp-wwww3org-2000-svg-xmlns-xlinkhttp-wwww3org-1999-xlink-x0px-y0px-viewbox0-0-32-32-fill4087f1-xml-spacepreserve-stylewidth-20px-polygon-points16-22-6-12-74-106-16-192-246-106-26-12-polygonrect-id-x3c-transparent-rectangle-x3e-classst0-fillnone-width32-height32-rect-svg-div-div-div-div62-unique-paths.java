094b2437d226f4edcfcfdda0e4574f7b80e470b0
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-11-09 20:33:01 +0530

    Time: 1 ms (9.03%), Space: 39.1 MB (50.82%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence15-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence11-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence9-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div62-unique-paths/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence15-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence11-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence9-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence4-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div62-unique-paths.java
@@ -0,0 +1,109 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        
+        // Tabulation....Space Optimized....
+        
+        int prev[] = new int[n];
+        int cur[] = new int[n];
+        
+        Arrays.fill(prev,1);
+        
+        for(int i = 1; i < m ; i++){
+            cur[0] = 1;
+            for(int j = 1; j < n ; j++){
+                
+                int left = cur[j-1];
+                int up = prev[j];
+
+                cur[j] = left+up;
+            }
+            prev = cur.clone();
+        }
+        
+        return prev[n-1];
+        
+        
+        
+        
+        
+        // Tabulation....
+        
+//         int dp[][] = new int[m][n];
+        
+//         for(int i = 0 ; i < m ; i++){
+//             dp[i][0] = 1;
+//         }
+//         for(int j = 0 ; j < n ; j++){
+//             dp[0][j] = 1;
+//         }
+        
+//         for(int i = 1; i < m ; i++){
+//             for(int j = 1; j < n ; j++){
+                
+//                 int left = dp[i][j-1];
+//                 int up = dp[i-1][j];
+
+//                 dp[i][j] = left+up;
+//             }
+//         }
+        
+//         return dp[m-1][n-1];
+        
+        
+        
+        
+        // Memoization....
+        
+//         int dp[][] = new int[m][n];
+//         for(int[] row:dp){
+//             Arrays.fill(row,-1);
+//         }
+        
+//         return f(m-1,n-1,dp);
+        
+        
+        
+        
+        // return f(m-1,n-1);
+        
+    }
+    public static int f(int m,int n,int dp[][]){
+        
+        if(m < 0 || n < 0){
+            return 0;
+        }
+        
+        if(m == 0 && n == 0){
+            return 1;
+        }
+        
+        if(m == 0 || n == 0){
+            return 1;
+        }
+        
+        if(dp[m][n] != -1){
+            return dp[m][n];
+        }
+        
+        int left = f(m,n-1,dp);
+        int up = f(m-1,n,dp);
+        
+        return dp[m][n] = left + up;
+        
+        
+        
+        
+//         Recursion....
+        
+//         if(m<0 || n<0)
+//             return 0;
+        
+//         if(m==0 && n==0)
+//             return 1;
+        
+//         int left = f(m,n-1);
+//         int up = f(m-1,n);
+        
+//         return left+up;
+    }
+}
