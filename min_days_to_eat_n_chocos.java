fb169c55d8fd1aec55f242c9447a3a19e052bc8b
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-11-10 05:39:59 +0530

    Update min_days_to_eat_n_chocos.java

--- min_days_to_eat_n_chocos.java
+++ min_days_to_eat_n_chocos.java
@@ -1,13 +1,14 @@
 /*  min no days to eat n chocolates. You start with 1 on the first day, then on the next days, you can eat 1 chocolate more, or same, or 1 chocolate less. You should be eating 1 chocolate on the last day. Return min of days to eat n chocolates  */
 
+// DP...
 
-
+import java.util.*;
 public class Main
 {
     static int choco;
 	public static void main(String[] args) {
 	    
-	    int n = 11;
+	    int n = 100;
 	    choco = n;
 	    
 	    if(n == 0){
@@ -15,11 +16,17 @@ public class Main
 	        return;
 	    }
 	    
-	    int res = f(n,0);
+	    int dp[][] = new int[n+1][n+1];
+	    
+	    for(int row[] : dp){
+	        Arrays.fill(row,-1);
+	    }
+	    
+	    int res = f(n,0,dp);
 	    
 	    System.out.print(res);
 	}
-	public static int f(int n, int prev){
+	public static int f(int n, int prev, int dp[][]){
 	    
 	    if(n == 0){
 	        if(prev != 1){
@@ -32,21 +39,76 @@ public class Main
 	        return choco+1;
 	    }
 	    
+	    if(dp[n][prev] != -1){
+	        return dp[n][prev];
+	    }
+	    
 	    int a = choco+1;
 	    if(prev+1 > 0){
-	        a = 1 + f(n-(prev+1),prev+1);
+	        a = 1 + f(n-(prev+1),prev+1,dp);
 	    }
 	    
 	    int b = choco+1;
 	    if(prev > 0)
-	     b = 1 + f(n-(prev),prev);
+	     b = 1 + f(n-(prev),prev,dp);
 	    
 	    int c = choco+1;
 	    if(prev-1 > 0){
-	        c = 1 + f(n-(prev-1),prev-1);
+	        c = 1 + f(n-(prev-1),prev-1,dp);
 	    }
 	    
 	    
-	    return Math.min(a,Math.min(b,c));
+	    return dp[n][prev] = Math.min(a,Math.min(b,c));
 	}
 }
+
+// Recursion...
+
+// public class Main
+// {
+//     static int choco;
+// 	public static void main(String[] args) {
+	    
+// 	    int n = 11;
+// 	    choco = n;
+	    
+// 	    if(n == 0){
+// 	        // 0
+// 	        return;
+// 	    }
+	    
+// 	    int res = f(n,0);
+	    
+// 	    System.out.print(res);
+// 	}
+// 	public static int f(int n, int prev){
+	    
+// 	    if(n == 0){
+// 	        if(prev != 1){
+// 	            return choco+1;
+// 	        }
+// 	        return 0;
+// 	    }
+	    
+// 	    if(n < 0){
+// 	        return choco+1;
+// 	    }
+	    
+// 	    int a = choco+1;
+// 	    if(prev+1 > 0){
+// 	        a = 1 + f(n-(prev+1),prev+1);
+// 	    }
+	    
+// 	    int b = choco+1;
+// 	    if(prev > 0)
+// 	     b = 1 + f(n-(prev),prev);
+	    
+// 	    int c = choco+1;
+// 	    if(prev-1 > 0){
+// 	        c = 1 + f(n-(prev-1),prev-1);
+// 	    }
+	    
+	    
+// 	    return Math.min(a,Math.min(b,c));
+// 	}
+// }
