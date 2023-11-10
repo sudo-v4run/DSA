7b48c58d891db176a3aa7e18e981f35f9c5bd191
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-11-10 15:40:04 +0530

    Time: 0 ms (100.00%), Space: 40 MB (94.80%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1328-break-a-palindrome/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1328-break-a-palindrome.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        
+        int n = palindrome.length();
+        
+        if(n == 1){
+            return "";
+        }
+        
+        char arr[] = palindrome.toCharArray();
+        
+        int i = 0;
+        while(i < n && arr[i] == 'a'){
+            i++;
+        }
+        
+        if(i == n){
+            arr[n-1] = 'b';
+            return new String(arr);
+        }
+        
+        if(i != n/2){
+            arr[i] = 'a';
+            return new String(arr);
+        }
+        
+        arr[n-1] = 'b';
+        
+        return new String(arr);
+    }
+}
