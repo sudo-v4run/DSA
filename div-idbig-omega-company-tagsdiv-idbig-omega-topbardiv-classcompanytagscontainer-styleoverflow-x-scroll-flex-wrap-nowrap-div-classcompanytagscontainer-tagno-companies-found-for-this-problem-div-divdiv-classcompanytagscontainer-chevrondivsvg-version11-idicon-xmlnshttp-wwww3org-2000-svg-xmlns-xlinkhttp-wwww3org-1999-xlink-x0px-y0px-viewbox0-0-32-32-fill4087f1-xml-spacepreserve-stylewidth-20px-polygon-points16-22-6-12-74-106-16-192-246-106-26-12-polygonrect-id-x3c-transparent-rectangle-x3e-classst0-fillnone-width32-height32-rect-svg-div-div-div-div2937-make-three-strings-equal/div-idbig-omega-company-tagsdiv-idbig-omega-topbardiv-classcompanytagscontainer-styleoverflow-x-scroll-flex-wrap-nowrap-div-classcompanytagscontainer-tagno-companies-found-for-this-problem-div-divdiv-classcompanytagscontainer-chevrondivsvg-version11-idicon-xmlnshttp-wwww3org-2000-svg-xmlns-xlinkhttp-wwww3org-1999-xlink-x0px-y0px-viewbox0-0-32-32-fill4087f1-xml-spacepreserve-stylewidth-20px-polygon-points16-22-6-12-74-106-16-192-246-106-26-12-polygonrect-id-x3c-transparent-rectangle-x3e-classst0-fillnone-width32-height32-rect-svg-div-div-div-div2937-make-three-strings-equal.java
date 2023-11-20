6ab4fd28caef051dda024259cee3e9d624c3a74d
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-11-20 19:33:30 +0530

    Time: 1 ms (100.00%), Space: 43.5 MB (100.00%) - LeetHub

--- div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2937-make-three-strings-equal/div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2937-make-three-strings-equal.java
+++ div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2937-make-three-strings-equal/div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2937-make-three-strings-equal.java
@@ -6,30 +6,15 @@ class Solution {
         int n3 = s3.length();
         
         int minLen = Math.min(n1,Math.min(n2,n3));
+        int i = 0;
         
-        int cnt = 0;
-        cnt += n1-minLen;
-        cnt += n2-minLen;
-        cnt += n3-minLen;
-        
-        for(int i = minLen-1 ; i >= 0 ; i--){
-            String temp1 = s1.substring(0,i+1);
-            String temp2 = s2.substring(0,i+1);
-            String temp3 = s3.substring(0,i+1);
-            
-            if(!temp1.equals(temp2)){
-                cnt += 3;
-                continue;
-            }
-            if(!temp2.equals(temp3)){
-                cnt += 3;
-                continue;
-            }
-            if(!temp3.equals(temp1)){
-                cnt += 3;
-                continue;
-            }
+        while(i < minLen && s1.charAt(i) == s2.charAt(i) && s2.charAt(i) == s3.charAt(i)){
+            i++;
         }
+        
+        int cnt = n1-i;
+        cnt += n2-i;
+        cnt += n3-i;
         
         if(cnt == n1+n2+n3){
             return -1;
