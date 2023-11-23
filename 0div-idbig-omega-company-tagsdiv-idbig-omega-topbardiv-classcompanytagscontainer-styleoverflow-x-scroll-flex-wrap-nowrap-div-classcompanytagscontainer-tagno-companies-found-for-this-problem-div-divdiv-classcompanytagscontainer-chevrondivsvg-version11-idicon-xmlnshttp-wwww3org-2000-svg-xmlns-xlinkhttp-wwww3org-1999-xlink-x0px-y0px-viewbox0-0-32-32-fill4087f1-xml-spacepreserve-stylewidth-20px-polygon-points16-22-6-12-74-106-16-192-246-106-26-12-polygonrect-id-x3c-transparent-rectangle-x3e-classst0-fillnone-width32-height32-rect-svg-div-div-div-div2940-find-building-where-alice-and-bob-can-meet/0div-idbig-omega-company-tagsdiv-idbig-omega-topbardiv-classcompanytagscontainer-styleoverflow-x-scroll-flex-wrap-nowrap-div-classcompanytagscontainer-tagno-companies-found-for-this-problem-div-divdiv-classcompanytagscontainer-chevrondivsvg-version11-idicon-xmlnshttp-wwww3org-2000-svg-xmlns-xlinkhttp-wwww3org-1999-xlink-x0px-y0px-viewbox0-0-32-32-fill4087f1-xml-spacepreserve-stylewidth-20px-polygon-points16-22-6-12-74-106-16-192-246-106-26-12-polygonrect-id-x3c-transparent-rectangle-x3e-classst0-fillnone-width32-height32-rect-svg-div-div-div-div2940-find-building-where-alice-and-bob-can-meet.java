19005770a69475f0d301d6c6237151e1a4d4bda9
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-11-23 20:46:58 +0530

    Time: 32 ms (71.15%), Space: 71.1 MB (18.97%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2940-find-building-where-alice-and-bob-can-meet/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2940-find-building-where-alice-and-bob-can-meet.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2940-find-building-where-alice-and-bob-can-meet/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2940-find-building-where-alice-and-bob-can-meet.java
@@ -1,61 +1,123 @@
 class Solution {
+    class Pair{
+        int maxVal, ind;
+        Pair(int maxVal, int ind){
+            this.maxVal = maxVal;
+            this.ind = ind;
+        }
+    }
     public int[] leftmostBuildingQueries(int[] heights, int[][] queries) {
-        
+         
         int n = heights.length;
         
-        HashMap<String,Integer> hm = new HashMap<>();
-        
         int ans[] = new int[queries.length];
-        int k = 0;
+        Arrays.fill(ans,-1);
         
-        for(int q[] : queries){
-            int a = q[0];
-            int b = q[1];
+        HashMap<Integer,ArrayList<Pair>> hm = new HashMap<>();
+        
+        for(int i = 0 ; i < queries.length ; i++){
             
-            String temp = a+","+b;
-            if(hm.containsKey(temp)){
-                ans[k++] = hm.get(temp);
-                continue;
-            }
-            
-            int max = Math.max(a,b);
-            int aVal = heights[a];
-            int bVal = heights[b];
+            int a = queries[i][0];
+            int b = queries[i][1];
             
             if(a == b){
-                ans[k] = a;
-                hm.put(temp,a);
-                k++;
-                continue;
-            }else if(aVal > bVal && a > b){
-                ans[k] = a;
-                hm.put(temp,a);
-                k++;
-                continue;
-            }else if(bVal > aVal && b > a){
-                ans[k] = b;
-                hm.put(temp,b);
-                k++;
-                continue;
+                ans[i] = a;
+            }else if(heights[a] > heights[b] && a > b){
+                ans[i] = a;
+            }else if(heights[b] > heights[a] && b > a){
+                ans[i] = b;
+            }else{
+                
+                int maxInd = Math.max(a,b);
+                int maxVal = Math.max(heights[a],heights[b]);
+                
+                if(!hm.containsKey(maxInd)){
+                    hm.put(maxInd, new ArrayList<>());
+                }
+                hm.get(maxInd).add(new Pair(maxVal,i));
+            }
+        }
+        
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->{
+            return a.maxVal-b.maxVal;
+        });
+        
+        for(int i = 0 ; i < heights.length ; i++){
+            
+            while(!pq.isEmpty() && pq.peek().maxVal < heights[i]){
+                int qInd = pq.poll().ind;
+                ans[qInd] = i;
             }
             
-            boolean flag = false;
-            for(int j = max+1 ; j < n ; j++){
-                if(heights[j] > aVal && heights[j] > bVal){
-                    ans[k] = j;
-                    hm.put(temp,j);
-                    k++;
-                    flag = true;
-                    break;
+            if(hm.containsKey(i)){
+                for(Pair p : hm.get(i)){
+                    pq.add(p);
                 }
-            }
-            if(!flag){
-                ans[k] = -1;
-                hm.put(temp,ans[k]);
-                k++;
             }
         }
         
         return ans;
     }
+    
+    
+    
+//     public int[] leftmostBuildingQueries(int[] heights, int[][] queries) {
+        
+//         int n = heights.length;
+        
+//         HashMap<String,Integer> hm = new HashMap<>();
+        
+//         int ans[] = new int[queries.length];
+//         int k = 0;
+        
+//         for(int q[] : queries){
+//             int a = q[0];
+//             int b = q[1];
+            
+//             String temp = a+","+b;
+//             if(hm.containsKey(temp)){
+//                 ans[k++] = hm.get(temp);
+//                 continue;
+//             }
+            
+//             int max = Math.max(a,b);
+//             int aVal = heights[a];
+//             int bVal = heights[b];
+            
+//             if(a == b){
+//                 ans[k] = a;
+//                 hm.put(temp,a);
+//                 k++;
+//                 continue;
+//             }else if(aVal > bVal && a > b){
+//                 ans[k] = a;
+//                 hm.put(temp,a);
+//                 k++;
+//                 continue;
+//             }else if(bVal > aVal && b > a){
+//                 ans[k] = b;
+//                 hm.put(temp,b);
+//                 k++;
+//                 continue;
+//             }
+            
+//             boolean flag = false;
+//             for(int j = max+1 ; j < n ; j++){
+//                 if(heights[j] > aVal && heights[j] > bVal){
+//                     ans[k] = j;
+//                     hm.put(temp,j);
+//                     k++;
+//                     flag = true;
+//                     break;
+//                 }
+//             }
+//             if(!flag){
+//                 ans[k] = -1;
+//                 hm.put(temp,ans[k]);
+//                 k++;
+//             }
+//         }
+        
+//         return ans;
+//     }
 }
