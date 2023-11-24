61c657f6f968d1e5d1b1474fff56ceda553235f3
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-11-24 19:05:03 +0530

    Time: 1 ms (99.01%), Space: 58.8 MB (70.17%) - LeetHub

---
+++ div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence20-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence16-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdatabricks-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgrab-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divwalmart-global-tech-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div41-first-missing-positive/div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence20-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence16-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdatabricks-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgrab-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divwalmart-global-tech-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div41-first-missing-positive.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int firstMissingPositive(int[] arr) {
+        
+        int n = arr.length;
+        
+        // Step 1...
+        
+        // We don't need -ves and we are going to mark the existing elements to negative
+        // later in the Algo...so if we keep the -ves as it is, it will indicate that
+        // the +ve version of it exits even if it does not...so change the initial -ves
+        // to neutral.i.e (neither -ve nor +ve)...
+        
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] < 0){
+                arr[i] = 0;
+            }
+        }
+        
+        // Step 2...
+        
+        // Intuition -> After the first step, we have no -ves in the array.
+        // Now, for eg., if 2 exists, we change the element at index 2 (val-1) to -ve. 
+        // This indicates that 2 exists in the array. We do val-1 because ->
+        // Array is 0 based indexing and the possible elements
+        // range is [1...n], we do (index-1) .i.e. changing element at index 1 
+        // to -ve indicates that 2 is present.
+        
+        for(int i = 0 ; i < n ; i++){
+            
+            int val = Math.abs(arr[i]);
+            
+            if(val-1 < 0 || val-1 >= n){
+                continue;
+            }
+            
+            if(arr[val-1] > 0){
+                
+                arr[val-1] = -arr[val-1];
+            }else if(arr[val-1] == 0){
+                
+                // We can't change 0 to -ve. So, we change it to out of bound value.i.e n+2
+                // which can never be the answer coz the worst case answer is n+1 only.
+                
+                arr[val-1] = -(n+2);
+            }
+            
+        }
+        
+        // Step 3....
+        
+        // We check if any value in [1...n] does not exits in the array. 
+        // .i.e If element at val-1 index is not -ve, then it means, val does not exists
+        // and since we go from 1---n, we return the ans as soon as any val does not exists..
+        // Also, in the step 2, we changed the 0 to -(n+2).. so, if we again get 0, that 
+        // that means, that index+1 val does not exists...
+        
+        for(int i = 1 ; i <= n ; i++){
+            if(arr[i-1] >= 0){
+                return i;
+            }
+        }
+        
+        return n+1;
+    }
+}
