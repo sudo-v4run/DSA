813073cb80f254c903e983eacd1daca29e784c85
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-11-26 09:48:48 +0530

    Time: 1 ms (100.00%), Space: 44.3 MB (33.33%) - LeetHub

--- 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -7,19 +7,9 @@ class Solution {
         k = k%n;
         
         for(int i = 0 ; i < m ; i++){
-            if(i%2 != 0){
-                // odd rows
-                for(int j = 0; j < n; j++){
-                    int p = (j + k) % n;
-                    if(arr[i][j] != arr[i][p]) return false;
-                }
-            }
-            else{
-                // even rows
-                for(int j = 0; j < n; j++){
-                    int p = (j + k) % n;
-                    if(arr[i][j] != arr[i][p]) return false;
-                }
+            for(int j = 0; j < n; j++){
+                int p = (j + k) % n;
+                if(arr[i][j] != arr[i][p]) return false;
             }
         }
         
