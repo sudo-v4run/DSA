a58cf177b7edba0a7559ba864c72be32898b28c3
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-12-05 18:26:19 +0530

    Time: 28 ms (5.22%), Space: 59.5 MB (7.55%) - LeetHub

--- 2952-minimum-number-of-coins-to-be-added/2952-minimum-number-of-coins-to-be-added.java
+++ 2952-minimum-number-of-coins-to-be-added/2952-minimum-number-of-coins-to-be-added.java
@@ -14,6 +14,7 @@ class Solution {
                 i++;
             }else{
                 if(preSum >= t){
+                    // t = preSum;  // for more optimization..
                     continue;
                 }
                 cnt++;
