3f8aceb9bea41bb016a0564bbac03ece8a900c6e
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2023-12-25 13:17:07 +0530

    Update 0330-patching-array.java

--- 0330-patching-array/0330-patching-array.java
+++ 0330-patching-array/0330-patching-array.java
@@ -7,10 +7,12 @@ class Solution {
         
         for(long t = 1 ; t <= n ; t++){
             if(i < arr.length && arr[i] <= t){
+
                 preSum += arr[i];
                 i++;
             }else{
                 if(preSum >= t){
+
                     t = preSum;
                     continue;
                 }
