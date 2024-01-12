53a0f2db557d226f7dbb7e327667f69dab547c5e
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-01-12 10:57:23 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (32.39%) - LeetHub

--- 2513-minimize-the-maximum-of-two-arrays/2513-minimize-the-maximum-of-two-arrays.java
+++ 2513-minimize-the-maximum-of-two-arrays/2513-minimize-the-maximum-of-two-arrays.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minimizeSet(int divisor1, int divisor2, int uniqueCnt1, int uniqueCnt2) {
         
-        int ans = -1;
+        long ans = -1;
         
         long lo = 1;
         long hi = (long)2e9;
@@ -10,32 +10,30 @@ class Solution {
             long mid = lo+(hi-lo)/2;
             
             if(isPossible(mid,divisor1,divisor2,uniqueCnt1,uniqueCnt2)){
-                ans = (int)mid;
+                ans = mid;
                 hi = mid-1;
             }else{
                 lo = mid+1;
             }
         }
         
-        return ans;
+        return (int)ans;
     }
     public boolean isPossible(long x, int d1, int d2, int u1, int u2){
         
         long divble_by_both = x/lcm(d1,d2);
-        long divble_by_d1 = x/d1-divble_by_both;    
-            // valid for arr2 expect divble_by_both set
-        long divble_by_d2 = x/d2-divble_by_both;
-            // valid for arr1 expect divble_by_both set
         
-        long valid_for_both_arr1_arr2 = x-divble_by_d1-divble_by_d2-divble_by_both;
-          // +divble_by_both is coz divble_by_both will be subtracted twice .i.e.
-          // one set of divble_by_both from divble_by_d1 and another from divble_by_d2
+        long divble_by_only_d1 = x/d1-divble_by_both;    // valid for arr2
+        long divble_by_only_d2 = x/d2-divble_by_both;    // valid for arr1
         
-        if(u1 > divble_by_d2){
-            valid_for_both_arr1_arr2 -= (u1-divble_by_d2);
+        long valid_for_both_arr1_arr2 
+              = x-divble_by_only_d1-divble_by_only_d2-divble_by_both;
+        
+        if(u1 > divble_by_only_d2){
+            valid_for_both_arr1_arr2 -= u1-divble_by_only_d2;
         }
-        if(u2 > divble_by_d1){
-            valid_for_both_arr1_arr2 -= (u2-divble_by_d1);
+        if(u2 > divble_by_only_d1){
+            valid_for_both_arr1_arr2 -= u2-divble_by_only_d1;
         }
         
         return valid_for_both_arr1_arr2 >= 0;
