e1bd3dc09f402f4da6367dc8ecc9b836bc55d0fc
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-01-29 19:18:44 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (78.77%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,20 +1,38 @@
 class Solution {
     public int findTheWinner(int n, int k) {
         
-        Queue<Integer> q = new LinkedList<>();
+//         Queue<Integer> q = new LinkedList<>();
         
-        for(int i = 1 ; i <= n ; i++){
-            q.add(i);
+//         for(int i = 1 ; i <= n ; i++){
+//             q.add(i);
+//         }
+        
+//         while(q.size() > 1){
+//             int x = k;
+//             while(x-- > 1){
+//                 q.add(q.poll());
+//             }
+//             q.poll();
+//         }
+        
+//         return q.peek();
+        
+        
+        
+        // Recursive Sol...
+        
+        // Intuition ->
+        // https://cs.stackexchange.com/questions/7048/a-recursive-formula-for-generalized-josephus-problem
+        
+        
+        return f(n,k)+1;       // +1 for coverting into 1 based...
+    }
+    public int f(int n, int k){
+        
+        if(n == 1){
+            return 0;
         }
         
-        while(q.size() > 1){
-            int x = k;
-            while(x-- > 1){
-                q.add(q.poll());
-            }
-            q.poll();
-        }
-        
-        return q.peek();
+        return (f(n-1,k)+k)%n;
     }
 }
