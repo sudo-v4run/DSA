69ffffeadb750324bfa098c5a651360770f18601
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-01-30 21:21:21 +0530

    Time: 43 ms (51.74%), Space: 102.8 MB (57.16%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.java
@@ -1,14 +1,12 @@
 class Solution {
     public int maxEnvelopes(int[][] envelopes) {
         
-        Arrays.sort(envelopes, new Comparator<int[]>(){
-        public int compare(int[] arr1, int[] arr2){
-            if(arr1[0] == arr2[0])
-                return arr2[1] - arr1[1];
-            else
-                return arr1[0] - arr2[0];
-       } 
-    });
+        Arrays.sort(envelopes,(a,b)->{
+            if(a[0] == b[0]){
+                return b[1]-a[1];
+            }
+            return a[0]-b[0];
+        });
         
         int n = envelopes.length;
         
