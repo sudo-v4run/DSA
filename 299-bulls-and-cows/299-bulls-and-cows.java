55ac89d5ac4d7bacd2008c87b9d309bcff6f0515
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-02-02 23:05:53 +0530

    Time: 5 ms (88.91%), Space: 42.2 MB (62.62%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.java
+++ 299-bulls-and-cows/299-bulls-and-cows.java
@@ -4,33 +4,62 @@ class Solution {
         int sLen = secret.length();
         int gLen = guess.length();
         
-        HashMap<Character,Integer> hm = new HashMap<>();
-        
         int bulls = 0;
         int cows = 0;
         
+        // Single  Pass...
+        
+        int cnt[] = new int[10];
+        
         for(int i = 0 ; i < sLen ; i++){
-            char ch = secret.charAt(i);
-            if(ch == guess.charAt(i)){
+            
+            char s = secret.charAt(i);
+            char g = guess.charAt(i);
+            
+            if(s == g){
                 bulls++;
             }else{
-                hm.put(ch,hm.getOrDefault(ch,0)+1);
+                
+                if(cnt[s-'0'] < 0){
+                    cows++;
+                }
+                if(cnt[g-'0'] > 0){
+                    cows++;
+                }
+                
+                cnt[s-'0']++;
+                cnt[g-'0']--;
             }
         }
         
-        for(int i = 0 ; i < gLen ; i++){
-            char ch = guess.charAt(i);
-            if(ch == secret.charAt(i)){
-                continue;
-            }
-            if(hm.containsKey(ch)){
-                cows++;
-                hm.put(ch,hm.get(ch)-1);
-                if(hm.get(ch) == 0){
-                    hm.remove(ch);
-                }
-            }
-        }
+        
+        
+        // 2 O(N) passes....
+        
+//         HashMap<Character,Integer> hm = new HashMap<>();
+        
+//         for(int i = 0 ; i < sLen ; i++){
+//             char ch = secret.charAt(i);
+//             if(ch == guess.charAt(i)){
+//                 bulls++;
+//             }else{
+//                 hm.put(ch,hm.getOrDefault(ch,0)+1);
+//             }
+//         }
+        
+//         for(int i = 0 ; i < gLen ; i++){
+//             char ch = guess.charAt(i);
+//             if(ch == secret.charAt(i)){
+//                 continue;
+//             }
+//             if(hm.containsKey(ch)){
+//                 cows++;
+//                 hm.put(ch,hm.get(ch)-1);
+//                 if(hm.get(ch) == 0){
+//                     hm.remove(ch);
+//                 }
+//             }
+//         }
         
         return bulls+"A"+cows+"B";
     }
