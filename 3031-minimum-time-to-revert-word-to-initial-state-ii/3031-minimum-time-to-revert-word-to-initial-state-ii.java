7de7e0693da31cf1eec937334a2ee008ef30c5b9
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-02-04 17:29:01 +0530

    Time: 40 ms (100.00%), Space: 47.7 MB (50.00%) - LeetHub

---
+++ 3031-minimum-time-to-revert-word-to-initial-state-ii/3031-minimum-time-to-revert-word-to-initial-state-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    static int P = 31;
+    static int mod = (int)1e9+9;
+    public int minimumTimeToInitialState(String word, int k) {
+        
+        int n = word.length();
+        
+        long pow[] = new long[n];
+        pow[0] = 1;
+        for(int i = 1 ; i < n ; i++){
+            pow[i] = (pow[i-1]*P)%mod;
+        }
+        
+        long preHash[] = new long[n];
+        preHash[0] = word.charAt(0)-'a'+1;
+        for(int i = 1 ; i < n ; i++){
+            preHash[i] = (preHash[i-1]+((word.charAt(i)-'a'+1)*pow[i])%mod)%mod;
+        }
+        
+        int ans = (int)Math.ceil((double)n/k);
+        
+        for(int i = n-1 ; i >= 1 ; i--){
+            
+            if (i % k != 0) continue;
+            
+            long prefixHash = preHash[n-i-1];
+            long suffixHash = (preHash[n-1]-preHash[i-1]+mod)%mod;
+            
+            if((prefixHash*pow[i])%mod == suffixHash){
+                ans = Math.min(ans,i/k);
+            }
+        }
+        
+        return ans;
+    }
+}
