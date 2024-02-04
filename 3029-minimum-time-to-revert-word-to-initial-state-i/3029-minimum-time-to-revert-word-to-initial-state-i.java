df23f42d352eb3aaf03caa363b8e73fc62a29402
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-02-04 17:35:00 +0530

    Time: 1 ms (100.00%), Space: 42.5 MB (33.33%) - LeetHub

--- 3029-minimum-time-to-revert-word-to-initial-state-i/3029-minimum-time-to-revert-word-to-initial-state-i.java
+++ 3029-minimum-time-to-revert-word-to-initial-state-i/3029-minimum-time-to-revert-word-to-initial-state-i.java
@@ -1,19 +1,54 @@
 class Solution {
+    static int P = 31;
+    static int mod = (int)1e9+9;
     public int minimumTimeToInitialState(String word, int k) {
         
-        String temp = word;
+        int n = word.length();
         
-        int cnt = 0;
+        long pow[] = new long[n];
+        pow[0] = 1;
+        for(int i = 1 ; i < n ; i++){
+            pow[i] = (pow[i-1]*P)%mod;
+        }
         
-        do{
-            if(temp.length() < k){
-                cnt++;
-                break;
+        long preHash[] = new long[n];
+        preHash[0] = word.charAt(0)-'a'+1;
+        for(int i = 1 ; i < n ; i++){
+            preHash[i] = (preHash[i-1]+((word.charAt(i)-'a'+1)*pow[i])%mod)%mod;
+        }
+        
+        int ans = (int)Math.ceil((double)n/k);
+        
+        for(int i = k ; i < n ; i=i+k){
+            
+            long prefixHash = preHash[n-i-1];
+            long suffixHash = (preHash[n-1]-preHash[i-1]+mod)%mod;
+            
+            if((prefixHash*pow[i])%mod == suffixHash){
+                ans = Math.min(ans,i/k);
             }
-            temp = temp.substring(k);
-            cnt++;
-        }while(temp.length() >= 0 && word.indexOf(temp) != 0);
+        }
         
-        return cnt;
+        return ans;
     }
 }
+
+// class Solution {
+//     public int minimumTimeToInitialState(String word, int k) {
+        
+//         String temp = word;
+        
+//         int cnt = 0;
+        
+//         do{
+//             if(temp.length() < k){
+//                 cnt++;
+//                 break;
+//             }
+//             temp = temp.substring(k);
+//             cnt++;
+//         }while(temp.length() >= 0 && word.indexOf(temp) != 0);
+        
+//         return cnt;
+//     }
+// }
