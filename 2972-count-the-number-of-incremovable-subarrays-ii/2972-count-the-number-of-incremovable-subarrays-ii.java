48a7c828d7d1171b2d0b1508e4ce896c1c1c540c
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-02-08 20:44:00 +0530

    Time: 4 ms (48.25%), Space: 57.3 MB (92.01%) - LeetHub

---
+++ 2972-count-the-number-of-incremovable-subarrays-ii/2972-count-the-number-of-incremovable-subarrays-ii.java
@@ -0,0 +1,58 @@
+class Solution {
+    public long incremovableSubarrayCount(int[] arr) {
+        
+        int n = arr.length;
+        
+        int max_l = 0;
+        while(max_l+1 < n){
+            if(arr[max_l+1] > arr[max_l]){
+                max_l++;
+            }else{
+                break;
+            }
+        }
+        
+        int min_r = n-1;
+        while(min_r-1 >= 0){
+            if(arr[min_r-1] < arr[min_r]){
+                min_r--;
+            }else{
+                break;
+            }
+        }
+        
+        long cnt = 0;
+        
+        for(int l = -1 ; l <= max_l ; l++){
+            int cur = l == -1 ? Integer.MIN_VALUE : arr[l];
+            int index = bs(min_r,n-1,cur,arr);
+            cnt += n-index+1;
+            if(index == l+1){  // when max_l == n-1
+                cnt--;
+                // -1 coz there will be no elements between l & index.
+            }
+        }
+        
+        return cnt;
+    }
+    public int bs(int l, int h, int target, int arr[]){
+        
+        int ans = arr.length;
+        
+        int lo = l;
+        int hi = h;
+        
+        while(lo <= hi){
+            int mid = (lo+hi)/2;
+            
+            if(target < arr[mid]){
+                ans = mid;
+                hi = mid-1;
+            }else{
+                lo = mid+1;
+            }
+        }
+        
+        return ans;
+    }
+}
