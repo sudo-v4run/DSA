f1e1f820029eb69cb13f6cd341bf88eb41fcc903
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-02-14 08:23:28 +0530

    Time: 34 ms (20.20%), Space: 198.5 MB (5.77%) - LeetHub

--- 3036-number-of-subarrays-that-match-a-pattern-ii/3036-number-of-subarrays-that-match-a-pattern-ii.java
+++ 3036-number-of-subarrays-that-match-a-pattern-ii/3036-number-of-subarrays-that-match-a-pattern-ii.java
@@ -6,10 +6,9 @@ class Solution {
         int P = 37;
         int mod = (int)1e9+9;
         
-        StringBuilder s = new StringBuilder();
-        
         // 1 -> a ; 0 -> b ; -1 -> c
         
+        StringBuilder s = new StringBuilder();
         for(int i = 0 ; i < n-1 ; i++){
             if(arr[i] < arr[i+1]){
                 s.append('a');
@@ -37,15 +36,18 @@ class Solution {
         int len = str.length();
         
         long prePrimePow[] = new long[len];
+        
         prePrimePow[0] = 1;
         for(int i = 1 ; i < len ; i++){
             prePrimePow[i] = (prePrimePow[i-1]*P)%mod;
         }
         
         long preHash[] = new long[len];
+        
         preHash[0] = str.charAt(0)-'a'+1;
         for(int i = 1 ; i < len ; i++){
-            preHash[i] = (preHash[i-1] + ((str.charAt(i)-'a'+1)*prePrimePow[i]%mod))%mod;
+            preHash[i] = (preHash[i-1] + 
+                          ((str.charAt(i)-'a'+1)*prePrimePow[i]%mod))%mod;
         }
         
         long hash = 0;
