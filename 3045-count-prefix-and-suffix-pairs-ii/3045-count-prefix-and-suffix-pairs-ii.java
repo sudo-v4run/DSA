f365b56d1d7b5bbb0ccc4d2bb672d4bc68f5d38e
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-02-21 19:47:24 +0530

    Time: 309 ms (14.55%), Space: 55 MB (99.07%) - LeetHub

--- 3045-count-prefix-and-suffix-pairs-ii/3045-count-prefix-and-suffix-pairs-ii.java
+++ 3045-count-prefix-and-suffix-pairs-ii/3045-count-prefix-and-suffix-pairs-ii.java
@@ -52,3 +52,69 @@ class Solution {
         return ans;
     }
 }
+
+
+// If collisions occur for the above code, store the hash value of the string in it's 
+// length() to add bit more complexity to avoid collision...
+
+// class Solution {
+//     public long countPrefixSuffixPairs(String[] words) {
+        
+//         int P = 37;
+//         int mod = (int)1e9+5;
+        
+//         int maxSLen = (int)1e5+5;
+        
+//         long primePower[] = new long[maxSLen];
+//         primePower[0] = 1;
+//         for(int i = 1 ; i < maxSLen ; i++){
+//             primePower[i] = (primePower[i-1]*P)%mod;
+//         }
+        
+//         long ans = 0;
+        
+//         HashMap<Integer,HashMap<Long,Integer>> hm = new HashMap<>();
+        
+//         for(String word : words){
+            
+//             int n = word.length();
+            
+//             long preHash[] = new long[n];
+//             preHash[0] = word.charAt(0)-'a'+1;
+//             for(int i = 1 ; i < n ; i++){
+//                 preHash[i] = (preHash[i-1]+
+//                               ((word.charAt(i)-'a'+1)*primePower[i])%mod)%mod;
+//             }
+            
+//             for(int l = 0 ; l < n ; l++){
+                
+//                 if(!hm.containsKey(l+1)){
+//                     continue;
+//                 }
+                
+//                 long prefixHash = preHash[l];
+                
+//                 int suffixInd = n-(l+1);
+//                 long suffixHash = preHash[n-1];
+//                 if(suffixInd-1 >= 0){
+//                     suffixHash = (preHash[n-1]-preHash[suffixInd-1]+mod)%mod;
+//                 }
+                
+//                 if((prefixHash*primePower[suffixInd])%mod == suffixHash && 
+//                    hm.containsKey(l+1) && 
+//                    hm.get(l+1).containsKey(prefixHash)){
+                    
+//                     ans += hm.get(l+1).get(prefixHash);
+//                 }
+//             }
+            
+//             if(!hm.containsKey(n)){
+//                 hm.put(n, new HashMap<>());
+//             }
+            
+//             hm.get(n).put(preHash[n-1],hm.get(n).getOrDefault(preHash[n-1],0)+1);
+//         }
+        
+//         return ans;
+//     }
+// }
