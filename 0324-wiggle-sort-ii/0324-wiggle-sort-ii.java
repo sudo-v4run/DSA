f76e242647153d653186a5020869273f6cbfc952
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-02-23 10:42:05 +0530

    Time: 13 ms (16.90%), Space: 49.2 MB (8.87%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.java
@@ -3,25 +3,44 @@ class Solution {
         
         int n = arr.length;
         
-        Arrays.sort(arr);
+        if(n <= 1){
+            return;
+        }
+        
+        int median = kthLargest(n/2,arr);
+        
+        // Dutch National Flag...
+        
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+        
+        while(mid <= high){
+            if(arr[mid] < median){
+                swap(arr,low,mid);
+                mid++;
+                low++;
+            }else if(arr[mid] == median){
+                mid++;
+            }else if(arr[mid] > median){
+                swap(arr,mid,high);
+                high--;
+            }
+        }
         
         int temp[] = new int[n];
         
         int j = n-1;
         int i = 1;
-        
         while(i < n){
             temp[i] = arr[j];
-            
             j--;
             i = i+2;
         }
         
         i = 0;
-        
         while(i < n){
             temp[i] = arr[j];
-            
             j--;
             i = i+2;
         }
@@ -30,4 +49,63 @@ class Solution {
             arr[k] = temp[k];
         }
     }
+    public void swap(int arr[], int i, int j){
+        
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public int kthLargest(int k, int arr[]){
+        
+        int n = arr.length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int i = 0 ; i < k ; i++){
+            pq.add(arr[i]);
+        }
+        
+        for(int i = k ; i < n ; i++){
+            if(arr[i] > pq.peek()){
+                pq.poll();
+                pq.add(arr[i]);
+            }
+        }
+        
+        return pq.peek();
+    }
 }
+
+
+// class Solution {
+//     public void wiggleSort(int[] arr) {
+        
+//         int n = arr.length;
+        
+//         Arrays.sort(arr);
+        
+//         int temp[] = new int[n];
+        
+//         int j = n-1;
+//         int i = 1;
+        
+//         while(i < n){
+//             temp[i] = arr[j];
+            
+//             j--;
+//             i = i+2;
+//         }
+        
+//         i = 0;
+        
+//         while(i < n){
+//             temp[i] = arr[j];
+            
+//             j--;
+//             i = i+2;
+//         }
+        
+//         for(int k = 0 ; k < n ; k++){
+//             arr[k] = temp[k];
+//         }
+//     }
+// }
