9d6d2b2d4d6ccb0b4a889b212dfeaf1bc1125892
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-03-01 11:20:31 +0530

    Time: 9 ms (72.69%), Space: 44.7 MB (73.18%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -1,21 +1,31 @@
 class Solution {
     public int findPairs(int[] arr, int k) {
-     
+        
         int n = arr.length;
         
-        HashSet<String> hs = new HashSet<>();
+        HashMap<Integer,Integer> hm = new HashMap<>();
         
         int cnt = 0;
         
         for(int i = 0 ; i < n ; i++){
-            for(int j = i+1 ; j < n ; j++){
-                if(Math.abs(arr[i] - arr[j]) == k && 
-                   !hs.contains(arr[i]+","+arr[j]) && !hs.contains(arr[j]+","+arr[i])){
-                    
+            if(hm.containsKey(arr[i])){
+                if(k == 0 && hm.get(arr[i])==1){
                     cnt++;
-                    hs.add(arr[i]+","+arr[j]);
-                    hs.add(arr[j]+","+arr[i]);
                 }
+                hm.put(arr[i],hm.get(arr[i])+1);
+            }else{
+                if(hm.containsKey(arr[i]-k)){
+                    cnt++;
+                }
+                if(hm.containsKey(arr[i]+k)){   // 'else if' gives wrong ans. Why? ->
+                                                // eg. hm = [3,7] and curr ele is 5 
+                                                // and k = 2. Both the conditions 
+                                                // are true coz (3,5) and (5,7) both
+                                                // are possible pairs for cur ele 5.
+                                                // So both the conditions must be checked.
+                    cnt++;
+                }
+                hm.put(arr[i],1);
             }
         }
         
