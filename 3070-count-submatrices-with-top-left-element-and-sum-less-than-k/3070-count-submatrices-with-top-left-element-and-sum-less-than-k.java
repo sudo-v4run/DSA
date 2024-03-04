413206b8c7e7460368ce575a791fa80fa303d132
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-03-04 20:08:06 +0530

    Time: 8 ms (25.00%), Space: 142 MB (50.00%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.java
@@ -1,22 +1,41 @@
 class Solution {
-    public int countSubmatrices(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
+    public int countSubmatrices(int[][] arr, int k) {
         
-        int count = 0;
+        int m = arr.length;
+        int n = arr[0].length;
         
-        int[][] prefixSum = new int[m + 1][n + 1];
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                prefixSum[i][j] = matrix[i - 1][j - 1] + 
-                    prefixSum[i - 1][j] + prefixSum[i][j - 1] - 
-                    prefixSum[i - 1][j - 1];
-                if(prefixSum[i][j] <= target){
-                    count++;
+        int cnt = 0;
+        
+        int prefix[][] = new int[m][n];
+        
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                
+                int cur = arr[i][j];
+                
+                int up = 0;
+                if(i-1 >= 0){
+                    up = prefix[i-1][j];
+                }
+                
+                int left = 0;
+                if(j-1 >= 0){
+                    left = prefix[i][j-1];
+                }
+                
+                int diagonal = 0;
+                if(i-1 >= 0 && j-1 >= 0){
+                    diagonal = prefix[i-1][j-1];
+                }
+                
+                prefix[i][j] = up+left+cur-diagonal;
+                
+                if(prefix[i][j] <= k){
+                    cnt++;
                 }
             }
         }
-
-        return count;
+        
+        return cnt;
     }
 }
