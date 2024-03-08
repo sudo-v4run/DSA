7409dd9d474b4d76e9c92af194b19d4735b39633
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-03-08 10:44:49 +0530

    Time: 8 ms (61.76%), Space: 43.1 MB (61.21%) - LeetHub

---
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int equalSubstring(String s, String t, int maxCost) {
+        
+        // we are asked to find max substring...so take and notTake does not work...
+        
+        int n = s.length();
+        
+        int i = 0;
+        int j = 0;
+        int cost = 0;
+        int max = Integer.MIN_VALUE;
+        
+        while(j < n){
+            cost += Math.abs((int)s.charAt(j)-(int)t.charAt(j));
+            
+            if(cost <= maxCost){
+                max = Math.max(max,j-i+1);
+                j++;
+            }else if(cost > maxCost){
+                while(cost > maxCost){
+                    cost = cost-Math.abs((int)s.charAt(i)-(int)t.charAt(i));
+                    i++;
+                }
+                max = Math.max(max,j-i+1);
+                j++;
+            }
+        }
+        
+        return max;
+    }
+}
