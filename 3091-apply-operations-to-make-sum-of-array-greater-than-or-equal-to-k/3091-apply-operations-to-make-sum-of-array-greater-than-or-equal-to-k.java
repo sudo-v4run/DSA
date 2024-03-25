51547260269d432032dd4a7c9519378564c18a5d
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-03-25 17:08:56 +0530

    Time: 7 ms (60.00%), Space: 40.8 MB (60.00%) - LeetHub

---
+++ 3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/3091-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minOperations(int k) {
+        
+        int ans = Integer.MAX_VALUE;
+        
+        for(int a = 0 ; a < k ; a++){
+            
+            int curSum = 1 + a;
+            int b = (int)Math.ceil((double)k/curSum);
+            
+            int ops = a+(b-1);
+            
+            ans = Math.min(ans,ops);
+        }
+        
+        return ans;
+    }
+}
