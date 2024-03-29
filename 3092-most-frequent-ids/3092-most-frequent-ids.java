1ffef43c84597c8c76962f214aa4c3b51745271e
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-03-29 18:03:58 +0530

    Time: 254 ms (16.25%), Space: 65.9 MB (76.18%) - LeetHub

--- 3092-most-frequent-ids/3092-most-frequent-ids.java
+++ 3092-most-frequent-ids/3092-most-frequent-ids.java
@@ -1,39 +1,83 @@
 class Solution {
-    public long[] mostFrequentIDs(int[] arr, int[] freq) {
+    public long[] mostFrequentIDs(int[] arr, int[] freq){
         
         int n = arr.length;
         
         long ans[] = new long[n];
         
-        PriorityQueue<Pair> pq = 
-            new PriorityQueue<>((a,b)->{return (int)b.f-(int)a.f;});
+        // TreeMap<Long,HashSet<Integer>> tm = new TreeMap<>();
+        TreeMap<Long,Integer> tm = new TreeMap<>();
         HashMap<Integer,Long> hm = new HashMap<>();
         
         for(int i = 0 ; i < n ; i++){
-            int val = arr[i];
-            hm.put(val,hm.getOrDefault(val,0l)+freq[i]);
-            pq.add(new Pair(hm.get(val),val));
             
-            while(!pq.isEmpty()){
-                Pair pop = pq.peek();
-                if(hm.get(pop.val) != pop.f){
-                    pq.poll();
-                }else{
-                    break;
+            int val = arr[i];
+            long f = hm.getOrDefault(val,0l);
+            
+            if(tm.containsKey(f)){
+                // tm.get(f).remove(val);
+                // if(tm.get(f).size() == 0){
+                //     tm.remove(f);
+                // }
+                
+                tm.put(f,tm.get(f)-1);
+                if(tm.get(f) == 0){
+                    tm.remove(f);
                 }
             }
             
-            ans[i] = pq.peek().f;
+            hm.put(val,hm.getOrDefault(val,0l)+freq[i]);
+            
+            long newFreq = hm.get(val);
+            // if(!tm.containsKey(newFreq)){
+            //     tm.put(newFreq,new HashSet<>());
+            // }
+            // tm.get(newFreq).add(val);
+            tm.put(newFreq,tm.getOrDefault(newFreq,0)+1);
+            
+            ans[i] = tm.lastKey();
         }
         
         return ans;
     }
-    class Pair{
-        long f;
-        int val;
-        Pair(long f, int val){
-            this.f = f;
-            this.val = val;
-        }
-    }
+    
+    
+    
+//     public long[] mostFrequentIDs(int[] arr, int[] freq) {
+        
+//         int n = arr.length;
+        
+//         long ans[] = new long[n];
+        
+//         PriorityQueue<Pair> pq = 
+//             new PriorityQueue<>((a,b)->{return (int)b.f-(int)a.f;});
+//         HashMap<Integer,Long> hm = new HashMap<>();
+        
+//         for(int i = 0 ; i < n ; i++){
+//             int val = arr[i];
+//             hm.put(val,hm.getOrDefault(val,0l)+freq[i]);
+//             pq.add(new Pair(hm.get(val),val));
+            
+//             while(!pq.isEmpty()){
+//                 Pair pop = pq.peek();
+//                 if(hm.get(pop.val) != pop.f){
+//                     pq.poll();
+//                 }else{
+//                     break;
+//                 }
+//             }
+            
+//             ans[i] = pq.peek().f;
+//         }
+        
+//         return ans;
+//     }
+//     class Pair{
+//         long f;
+//         int val;
+//         Pair(long f, int val){
+//             this.f = f;
+//             this.val = val;
+//         }
+//     }
 }
