4d37d8b1ee9445bdaec30d078c331593bbf4d658
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-05-06 16:15:23 +0530

    Time: 43 ms (10.00%), Space: 45.1 MB (90.00%) - LeetHub

--- 3137-minimum-number-of-operations-to-make-word-k-periodic/3137-minimum-number-of-operations-to-make-word-k-periodic.java
+++ 3137-minimum-number-of-operations-to-make-word-k-periodic/3137-minimum-number-of-operations-to-make-word-k-periodic.java
@@ -8,35 +8,20 @@ class Solution {
         int i = 0;
         int j = k-1;
         
+        int maxF = -1;
+        
         while(i < n){
             
             String s = word.substring(i,j+1);
             
             hm.put(s,hm.getOrDefault(s,0)+1);
+            maxF = Math.max(maxF,hm.get(s));
             
             i = i+k;
             j = i+k-1;
             
         }
         
-        ArrayList<Pair> arr = new ArrayList<>();
-        for(String s : hm.keySet()){
-            
-            int freq = hm.get(s);
-            
-            arr.add(new Pair(s,freq));
-        }
-        
-        Collections.sort(arr,(a,b)->{return b.f - a.f;});
-        
-        return (n/k)-arr.get(0).f;
-    }
-    class Pair{
-        String s;
-        int f;
-        Pair(String s, int f){
-            this.s = s;
-            this.f = f;
-        }
+        return (n/k)-maxF;
     }
 }
