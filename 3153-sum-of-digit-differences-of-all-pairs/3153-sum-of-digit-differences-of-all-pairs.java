9a6db19f0aa2fe83d8419b2f96ac873c6d01bab4
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-05-25 12:45:34 +0530

    Time: 40 ms (38.84%), Space: 59.7 MB (52.08%) - LeetHub

---
+++ 3153-sum-of-digit-differences-of-all-pairs/3153-sum-of-digit-differences-of-all-pairs.java
@@ -0,0 +1,26 @@
+class Solution {
+    public long sumDigitDifferences(int[] arr) {
+        
+        int n = arr.length;
+        int prefix[][] = new int[10][10];
+        
+        for(int i = 0 ; i < n ; i++){
+            String cur = String.valueOf(arr[i]);
+            
+            for(int j = 0 ; j < cur.length() ; j++){
+                prefix[j][cur.charAt(j)-'0']++;
+            }
+        }
+        
+        long ans = 0;
+        
+        for(int i = 0 ; i < n ; i++){
+            String cur = String.valueOf(arr[i]);
+            for(int j = 0 ; j < cur.length() ; j++){
+                ans += n-prefix[j][cur.charAt(j)-'0'];
+            }
+        }
+        
+        return ans/2;
+    }
+}
