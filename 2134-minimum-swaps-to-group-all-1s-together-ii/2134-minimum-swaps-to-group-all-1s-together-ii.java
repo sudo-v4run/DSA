1532b0f99590a0d5e684ae89cc86fa8a0e0b904f
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-06-07 15:35:04 +0530

    Time: 6 ms (89.16%), Space: 56.6 MB (98.77%) - LeetHub

---
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int minSwaps(int[] arr) {
+        
+        int n = arr.length;
+        
+        int ones = 0;
+        
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] == 1){
+                ones++;
+            }
+        }
+        
+        int preCnt[] = new int[n];
+        
+        preCnt[0] = arr[0] == 1 ? 1 : 0;
+        for(int i = 1 ; i < n ; i++){
+            
+            if(arr[i] == 1){
+                preCnt[i] = preCnt[i-1] + 1;
+            }else{
+                preCnt[i] = preCnt[i-1];
+            }
+        }
+        
+        if(ones <= 1){
+            return 0;
+        }
+        
+        int ans = n;
+        
+        int l = 0;
+        int r = ones-1;
+        while(l < n){
+            
+            int curOnes = 0;
+            
+            if(l < r%n){
+                curOnes = preCnt[r];
+                if(l > 0){
+                    curOnes = preCnt[r]-preCnt[l-1];
+                }
+            }else{
+                
+                int newR = r%n;
+                
+                curOnes = ones - (preCnt[l-1] - preCnt[newR]);
+            }
+            
+            
+            
+            int reqSwaps = ones-curOnes;
+            
+            ans = Math.min(ans,reqSwaps);
+            
+            l++;
+            r++;
+        }
+        
+        return ans;
+    }
+}
