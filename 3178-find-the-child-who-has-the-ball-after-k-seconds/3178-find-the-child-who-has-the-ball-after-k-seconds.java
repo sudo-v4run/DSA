d15c7c7aa319260964e8aae6750195163c1b4733
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-06-10 21:25:44 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (100.00%) - LeetHub

--- 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.java
+++ 3178-find-the-child-who-has-the-ball-after-k-seconds/3178-find-the-child-who-has-the-ball-after-k-seconds.java
@@ -1,14 +1,16 @@
 class Solution {
     public int numberOfChild(int n, int k) {
         
-        int temp = k/(n-1);
+        n--;
+        
+        int temp = k/n;
         
         if(temp%2 == 0){
-            int rem = k%(n-1);
+            int rem = k%n;
             return rem;
         }else{
-            int rem = k%(n-1);
-            return n-1-rem;
+            int rem = k%n;
+            return n-rem;
         }
     }
 }
