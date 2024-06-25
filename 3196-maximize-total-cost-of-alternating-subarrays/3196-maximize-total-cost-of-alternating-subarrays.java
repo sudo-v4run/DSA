918a8da6b29e1e6d5592ccd6578c261801dcf3bb
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-06-25 10:20:17 +0530

    Time: 46 ms (30.36%), Space: 79.4 MB (42.81%) - LeetHub

--- 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.java
+++ 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.java
@@ -1,15 +1,42 @@
 class Solution {
-    public long maximumTotalCost(int[] nums) {
-        int n = nums.length;
-        long addResult = nums[0];
-        long subResult = nums[0];
-        for (int i = 1; i < n; i++) {
-            long tempAdd = Math.max(addResult, subResult) + nums[i];
-            long tempSub = addResult - nums[i];
-
-            addResult = tempAdd;
-            subResult = tempSub;
+    public long maximumTotalCost(int[] arr) {
+        
+        int n = arr.length;
+        
+        long dp[][] = new long[n][2];
+        
+        for(long row[] : dp){
+            Arrays.fill(row,Long.MIN_VALUE);
         }
-        return Math.max(addResult, subResult);
+        
+        return f(0,1,arr,dp);
+    }
+    public long f(int index, int sign, int arr[], long dp[][]){
+        
+        int n = arr.length;
+        
+        if(index >= n){
+            return 0;
+        }
+        
+        if(dp[index][sign] != Long.MIN_VALUE){
+            return dp[index][sign];
+        }
+        
+        // split here...
+        
+        long split = 0;
+        if(sign == 1){          // 1 -> + and  0 -> -
+            split = arr[index] + f(index+1,1,arr,dp);
+        }else{
+            split = -arr[index] + f(index+1,1,arr,dp);
+        }
+        
+        // do not split here...
+        
+        int newSign = sign == 1 ? 0 : 1;
+        long notSplit = arr[index]*(sign) + f(index+1,newSign,arr,dp);
+        
+        return dp[index][sign] = Math.max(split,notSplit);
     }
 }
