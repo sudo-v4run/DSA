bd4d7ccfa67515634606d93b8f67207ebce1d235
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-07-02 11:34:19 +0530

    Time: 508 ms (18.54%), Space: 44.8 MB (90.59%) - LeetHub

---
+++ 3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.java
@@ -0,0 +1,68 @@
+class Solution {
+    public int maximumLength(int[] arr, int k) {
+        
+        int n = arr.length;
+        
+        HashMap<Integer,ArrayList<Integer>> indices = new HashMap<>();
+        
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = arr[i]%k;
+        }
+        
+        for(int i = 0 ; i < n ; i++){
+            
+            if(!indices.containsKey(arr[i])){
+                indices.put(arr[i], new ArrayList<>());
+            }
+            indices.get(arr[i]).add(i);
+        }
+        
+        int max = 1;
+        
+        for(int i = 0 ; i < k ; i++){
+            for(int j = 0 ; j < k ; j++){
+                
+                ArrayList<Integer> iList = indices.getOrDefault(i, new ArrayList<>());
+                ArrayList<Integer> jList = indices.getOrDefault(j, new ArrayList<>());
+                int iIndex = 0;
+                int jIndex = 0;
+                
+                int cnt = 0;
+                int prevIndex = -1;
+                
+                while(true){
+                    
+                    while(iIndex < iList.size() && iList.get(iIndex) <= prevIndex){
+
+                        iIndex++;
+                    }
+
+                    if(iIndex == iList.size()){
+                        break;
+                    }
+
+                    cnt++;
+                    prevIndex = iList.get(iIndex);
+                    iIndex++;
+
+                    while(jIndex < jList.size() && jList.get(jIndex) <= prevIndex){
+
+                        jIndex++;
+                    }
+
+                    if(jIndex == jList.size()){
+                        break;
+                    }
+
+                    cnt++;
+                    prevIndex = jList.get(jIndex);
+                    jIndex++;
+                }
+                
+                max = Math.max(max,cnt);
+            }
+        }
+        
+        return max;
+    }
+}
