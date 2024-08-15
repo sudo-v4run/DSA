b966b282e0292cf4fe2b4cfca4e3fcf877c0d936
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-08-15 15:15:59 +0530

    Time: 435 ms (7.51%), Space: 62.1 MB (43.42%) - LeetHub

---
+++ 3251-find-the-count-of-monotonic-pairs-ii/3251-find-the-count-of-monotonic-pairs-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    static int mod;
+    public int countOfPairs(int[] arr) {
+        
+        mod = (int)1e9+7;
+        
+        int n = arr.length;
+        
+        int dp[][] = new int[n+1][1001];
+        
+        for(int prev1 = 1000 ; prev1 >= 0 ; prev1--){
+            dp[n][prev1] = 1;
+        }
+        
+        for(int index = n-1 ; index >= 0 ; index--){
+            
+            int prefix[] = new int[1001];
+            prefix[0] = dp[index+1][0];
+            for(int i = 1 ; i <= 1000 ; i++){
+                prefix[i] = (prefix[i-1] + dp[index+1][i])%mod;
+            }
+            
+            
+            for(int prev1 = 1000 ; prev1 >= 0 ; prev1--){
+                
+                int ans = 0;
+                int prev2 = index==0 ? 1002 : (arr[index-1]-prev1);
+                
+                int l = Math.max(arr[index]-prev2,prev1);
+                 // derived from the condition that prev2 should be >= curSecond
+                
+                int r = arr[index];
+                
+                if(l <= r){
+                    int cur = prefix[r];
+                    if(l > 0){
+                        cur = (prefix[r] - prefix[l-1]+mod)%mod;
+                    }
+
+                    ans = (ans + cur)%mod;
+                }
+                dp[index][prev1] = ans%mod;
+            }
+        }
+        
+        return dp[0][0]%mod;
+    }
+}
