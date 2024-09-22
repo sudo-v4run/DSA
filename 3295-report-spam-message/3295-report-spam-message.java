a50d452a696ddd2c6a6446834ea0702931fd34b4
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-09-22 10:55:39 +0530

    Time: 65 ms (100.00%), Space: 73.6 MB (100.00%) - LeetHub

---
+++ 3295-report-spam-message/3295-report-spam-message.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean reportSpam(String[] message, String[] bannedWords) {
+        
+        HashSet<String> hs = new HashSet<>();
+        for(String s : bannedWords){
+            hs.add(s);
+        }
+        
+        int cnt = 0;
+        
+        for(String s : message){
+            if(hs.contains(s)){
+                cnt++;
+            }
+        }
+        
+        return cnt >= 2;
+    }
+}
