170c34445a181853c7d9acd2c283de3135ef42b3
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-10-09 11:22:10 +0530

    Time: 9 ms (42.01%), Space: 42.7 MB (63.84%) - LeetHub

--- 3309-maximum-possible-number-by-binary-concatenation/3309-maximum-possible-number-by-binary-concatenation.java
+++ 3309-maximum-possible-number-by-binary-concatenation/3309-maximum-possible-number-by-binary-concatenation.java
@@ -1,27 +1,43 @@
 class Solution {
-    public int maxGoodNumber(int[] arr) {
+    public int maxGoodNumber(int[] nums) {
         
-        int ans = -1;
+//         int ans = -1;
         
-        for(int i = 0 ; i < 3 ; i++){
-            for(int j = 0 ; j < 3 ; j++){
-                for(int k = 0 ; k < 3 ; k++){
+//         for(int i = 0 ; i < 3 ; i++){
+//             for(int j = 0 ; j < 3 ; j++){
+//                 for(int k = 0 ; k < 3 ; k++){
                     
-                    if(i == j || j == k || k == i){
-                        continue;
-                    }
+//                     if(i == j || j == k || k == i){
+//                         continue;
+//                     }
                     
-                    String b1 = Integer.toBinaryString(arr[i]);
-                    String b2 = Integer.toBinaryString(arr[j]);
-                    String b3 = Integer.toBinaryString(arr[k]);
+//                     String b1 = Integer.toBinaryString(arr[i]);
+//                     String b2 = Integer.toBinaryString(arr[j]);
+//                     String b3 = Integer.toBinaryString(arr[k]);
                     
-                    String res = b1+b2+b3;
+//                     String res = b1+b2+b3;
                     
-                    ans = Math.max(ans,Integer.parseInt(res,2));
-                }
-            }
+//                     ans = Math.max(ans,Integer.parseInt(res,2));
+//                 }
+//             }
+//         }
+        
+        
+        String binary = "";
+        
+        Integer[] arr = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+        
+        Arrays.sort(arr,(a,b)->{
+            String b1 = Integer.toBinaryString(a);
+            String b2 = Integer.toBinaryString(b);
+            
+            return -1*(b1+b2).compareTo(b2+b1);
+        });
+        
+        for(int x : arr){
+            binary += Integer.toBinaryString(x);
         }
         
-        return ans;
+        return Integer.parseInt(binary,2);
     }
 }
