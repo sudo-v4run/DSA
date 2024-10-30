bed9f832661c2e69593fe5beb5168030d1353851
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-10-30 16:07:11 +0530

    Time: 8 ms (76.90%), Space: 42.6 MB (80.63%) - LeetHub

---
+++ 3334-find-the-maximum-factor-score-of-array/3334-find-the-maximum-factor-score-of-array.java
@@ -0,0 +1,40 @@
+class Solution {
+    public long maxScore(int[] arr) {
+        
+        int n = arr.length;
+        
+        if(n == 1){
+            return arr[0]*arr[0];
+        }
+        
+        long ans = 1;
+        
+        for(int i = -1 ; i < n ; i++){
+            
+            long gcd = (i != 0) ? arr[0] : arr[1];
+            long lcm = gcd;
+            
+            for(int j = 0 ; j < n ; j++){
+                
+                if(j == i){
+                    continue;
+                }
+                
+                gcd = findGcd(gcd,arr[j]);
+                lcm = lcm*arr[j] / findGcd(lcm,arr[j]);
+            }
+            
+            ans = Math.max(ans,lcm*1l*gcd);
+        }
+        
+        return ans;
+    }
+    public long findGcd(long a, long b){
+        
+        if(b == 0l){
+            return a;
+        }
+        
+        return findGcd(b,a%b);
+    }
+}
