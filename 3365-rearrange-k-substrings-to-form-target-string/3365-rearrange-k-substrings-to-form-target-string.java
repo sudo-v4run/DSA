3403a9a5a915fb4482b64b5755103dc03dfcce55
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2024-11-28 23:19:43 +0530

    Time: 1871 ms (5.00%), Space: 56 MB (31.47%) - LeetHub

---
+++ 3365-rearrange-k-substrings-to-form-target-string/3365-rearrange-k-substrings-to-form-target-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isPossibleToRearrange(String s, String t, int k) {
+        
+        int n = s.length();
+        int len = n/k;
+        
+        HashMap<String,Integer> hm = new HashMap<>();
+        
+        for(int i = 0 ; i < n ; i = i+len){
+            String temp = "";
+            for(int j = i ; j < i+len ; j++){
+                temp += s.charAt(j);
+            }
+            hm.put(temp,hm.getOrDefault(temp,0)+1);
+        }
+        
+        for(int i = 0 ; i < n ; i = i+len){
+            String temp = "";
+            for(int j = i ; j < i+len ; j++){
+                temp += t.charAt(j);
+            }
+            if(!hm.containsKey(temp)){
+                return false;
+            }else{
+                hm.put(temp,hm.get(temp)-1);
+                if(hm.get(temp) == 0){
+                    hm.remove(temp);
+                }
+            }
+        }
+        
+        return true;
+    }
+}
