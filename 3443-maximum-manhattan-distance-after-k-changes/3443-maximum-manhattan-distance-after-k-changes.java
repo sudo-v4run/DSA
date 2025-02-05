f16bc19eae7ebf0598ed407503642a02ab8f62a4
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2025-02-05 20:15:34 +0530

    Time: 215 ms (13.04%), Space: 45.7 MB (83.03%) - LeetHub

---
+++ 3443-maximum-manhattan-distance-after-k-changes/3443-maximum-manhattan-distance-after-k-changes.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int maxDistance(String s, int k) {
+
+        int ans = -1;
+
+        char vertical[] = {'N' , 'S'};
+        char horizontal[] = {'W' , 'E'};
+
+        for(char ver : vertical){
+            for(char hor : horizontal){
+                int remK = k;
+                int x = 0;
+                int y = 0;
+                for(char ch : s.toCharArray()){
+
+                    if(ch == ver && remK > 0){
+                        if(ch == 'N'){
+                            ch = 'S';
+                        }else{
+                            ch = 'N';
+                        }
+                        remK--;
+                    }else if(ch == hor && remK > 0){
+                        if(ch == 'W'){
+                            ch = 'E';
+                        }else{
+                            ch = 'W';
+                        }
+                        remK--;
+                    }
+
+                    x = x + ((ch == 'N') ? 1 : 0);
+                    x = x - ((ch == 'S') ? 1 : 0);
+                    y = y + ((ch == 'W') ? 1 : 0);
+                    y = y - ((ch == 'E') ? 1 : 0);
+
+                    ans = Math.max(ans,Math.abs(x)+Math.abs(y));
+                }
+            }
+        }
+
+        return ans;
+    }
+}
