5a36deeabafb4368513f18875ea0b0ba97c21dd3
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2025-02-17 22:48:09 +0530

    Time: 310 ms (81.96%), Space: 68.6 MB (41.93%) - LeetHub

---
+++ 3447-assign-elements-to-groups-with-constraints/3447-assign-elements-to-groups-with-constraints.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] assignElements(int[] groups, int[] elements) {
+        
+        int sieve[] = new int[(int)1e6];
+        int maxg = Arrays.stream(groups).max().orElse(-1);
+        Arrays.fill(sieve,-1);
+
+        for(int i = 0 ; i < elements.length ; i++){
+            int cur = elements[i];
+            if (cur > maxg || sieve[cur] != -1){
+                continue;
+            }
+            for(int j = cur ; j <= (int)1e5 ; j = j+cur){
+                if(sieve[j] == -1){
+                    sieve[j] = i;
+                }
+            }
+        }
+
+        int n = groups.length;
+
+        int assigned[] = new int[n];
+
+        for(int i = 0 ; i < n ; i++){
+            assigned[i] = sieve[groups[i]];
+        }
+
+        return assigned;
+    }
+}
