3e10a64896828654fe97c5cff1f75479d3c52943
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2025-03-29 00:44:31 +0530

    Time: 5 ms (100%), Space: 45.3 MB (91.38%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -2,11 +2,19 @@ import java.util.*;
 
 class Solution {
     public List<String> findItinerary(List<List<String>> tickets) {
-        // Adjacency List with Min-Heap (PriorityQueue)
+        
         HashMap<String, PriorityQueue<String>> adj = new HashMap<>();
 
         for (List<String> t : tickets) {
-            adj.computeIfAbsent(t.get(0), k -> new PriorityQueue<>()).add(t.get(1));
+
+            String d = t.get(0);
+            String a = t.get(1);
+
+            if(!adj.containsKey(d)){
+                adj.put(d, new PriorityQueue<>());
+            }
+
+            adj.get(d).add(a);
         }
 
         LinkedList<String> res = new LinkedList<>();
@@ -15,13 +23,12 @@ class Solution {
     }
 
     public void dfs(String v, HashMap<String, PriorityQueue<String>> adj, LinkedList<String> res) {
-        PriorityQueue<String> neighbors = adj.get(v);
 
-        while (neighbors != null && !neighbors.isEmpty()) {
-            String next = neighbors.poll(); // Remove edge directly
+        while (adj.get(v) != null && !adj.get(v).isEmpty()) {
+            String next = adj.get(v).poll();
             dfs(next, adj, res);
         }
         
-        res.addFirst(v); // Add to result in post-order
+        res.addFirst(v);
     }
 }
