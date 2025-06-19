120304be80d9a6853d674b426b4a633ab2624727
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2025-06-19 23:33:46 +0530

    Time: 13 ms (81.06%), Space: 44.8 MB (92.35%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minCost(int[][] arr) {
-        
-        // Dijsktra's
+
+        // 0 1 BFS...
 
         int m = arr.length;
         int n = arr[0].length;
@@ -16,13 +16,11 @@ class Solution {
 
         int vis[][] = new int[m][n];
 
-        PriorityQueue<Triplet> pq = new PriorityQueue<>((a,b)->{
-            return a.cost-b.cost;
-        });
-        pq.add(new Triplet(0,0,0));
+        Deque<Triplet> dq = new ArrayDeque<>();
+        dq.addFirst(new Triplet(0,0,0));
 
-        while(!pq.isEmpty()){
-            Triplet pop = pq.poll();
+        while(!dq.isEmpty()){
+            Triplet pop = dq.pollFirst();
 
             int cost = pop.cost;
             int row = pop.r;
@@ -49,12 +47,73 @@ class Solution {
 
                 if(cost + curCost < dist[newR][newC]){
                     dist[newR][newC] = cost + curCost;
-                    pq.add(new Triplet(dist[newR][newC],newR,newC));
+                    if(curCost == 0){
+                        dq.addFirst(new Triplet(dist[newR][newC],newR,newC));
+                    }else{
+                        dq.addLast(new Triplet(dist[newR][newC],newR,newC));
+                    }
                 }
             }
         }
 
         return dist[m-1][n-1];
+
+
+        
+        // Dijsktra's...
+
+        // int m = arr.length;
+        // int n = arr[0].length;
+
+        // int dir[][] = {{0,1},{0,-1},{1,0},{-1,0}};
+
+        // int dist[][] = new int[m][n];
+        // for(int row[] : dist){
+        //     Arrays.fill(row,(int)1e5);
+        // }
+        // dist[0][0] = 0;
+
+        // int vis[][] = new int[m][n];
+
+        // PriorityQueue<Triplet> pq = new PriorityQueue<>((a,b)->{
+        //     return a.cost-b.cost;
+        // });
+        // pq.add(new Triplet(0,0,0));
+
+        // while(!pq.isEmpty()){
+        //     Triplet pop = pq.poll();
+
+        //     int cost = pop.cost;
+        //     int row = pop.r;
+        //     int col = pop.c;
+
+        //     vis[row][col] = 1;
+
+        //     if(row == m-1 && col == n-1){
+        //         return cost;
+        //     }
+
+        //     for(int d = 0; d < 4; d++){
+        //         int newR = row+dir[d][0];
+        //         int newC = col+dir[d][1];
+
+        //         if(newR < 0 || newR >= m || newC < 0 || newC >= n || vis[newR][newC] == 1){
+        //             continue;
+        //         }
+
+        //         int curCost = 0;
+        //         if(arr[row][col] != d+1){
+        //             curCost = 1;
+        //         }
+
+        //         if(cost + curCost < dist[newR][newC]){
+        //             dist[newR][newC] = cost + curCost;
+        //             pq.add(new Triplet(dist[newR][newC],newR,newC));
+        //         }
+        //     }
+        // }
+
+        // return dist[m-1][n-1];
     }
     class Triplet{
         int cost, r, c;
