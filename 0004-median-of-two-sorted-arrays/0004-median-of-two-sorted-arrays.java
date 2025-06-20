56097978256f8aa27410512371b52854e2a3eb14
Varun Gudamsetti <92177000+cyberciph3r@users.noreply.github.com>
2025-06-21 01:30:20 +0530

    Time: 1 ms (100%), Space: 44.8 MB (99.96%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,52 +1,46 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
         
-        int arr1[] = new int[nums1.length+4];
-        int arr2[] = new int[nums2.length+4];
-
-        int n1 = arr1.length;
-        int n2 = arr2.length;
-
-        if(n2 < n1){
-            return findMedianSortedArrays(nums2,nums1);
+        int n1 = arr1.length, n2 = arr2.length;
+        
+        if(n1>n2){
+            return findMedianSortedArrays(arr2,arr1);
         }
-
-        arr1[0] = arr1[1] = Integer.MIN_VALUE;
-        arr1[n1-1] = arr1[n1-2] = Integer.MAX_VALUE;
-
-        arr2[0] = arr2[1] = Integer.MIN_VALUE;
-        arr2[n2-1] = arr2[n2-2] = Integer.MAX_VALUE;
-
-        for(int i = 0 ; i < nums1.length; i++){
-            arr1[i+2] = nums1[i];
-        }
-        for(int i = 0 ; i < nums2.length; i++){
-            arr2[i+2] = nums2[i];
-        }
-
-        int l1 = 0;
-        int h1 = n1-1;
-        int l2 = 0;
-        int h2 = n2-1;
-
-        while(l1 <= h1){
-            int m1 = (l1+h1)/2;
-            int m2 = (((n1+n2+1)/2)-(m1+1))-1;
-
-            if(arr1[m1] <= arr2[m2+1] && arr2[m2] <= arr1[m1+1]){
-                if((n1+n2)%2 == 0){
-                    return (Math.max(arr1[m1],arr2[m2])+Math.min(arr1[m1+1],arr2[m2+1]))/2.0;
-
+        
+        int l = 0;
+        int h = n1;
+            // h = n1 but not n1-1 coz that's how we take cuts...n1 is also inclusive
+            // i.e. n1 accounts for all elements taken...and 0 means no elements taken...
+            // In that way, cut = 1 means we are taking elements at 0th and before indices
+            // So, cut = n1 means we are taking n-1th element and before indecies elements
+        
+        while(l<=h){
+            
+            int cut1 = (l+h)/2;
+            int cut2 = ((n1+n2)/2) - cut1;
+            
+            int l1 = cut1==0 ? Integer.MIN_VALUE : arr1[cut1-1];
+            int l2 = cut2==0 ? Integer.MIN_VALUE : arr2[cut2-1];
+            
+            int r1 = cut1==n1 ? Integer.MAX_VALUE : arr1[cut1];
+            int r2 = cut2==n2 ? Integer.MAX_VALUE : arr2[cut2];
+            
+            if(l1<=r2 && l2<=r1){
+                if((n1+n2)%2==0){
+                    return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
                 }else{
-                    return Math.max(arr1[m1],arr2[m2]);
+                    return Math.min(r1,r2);
                 }
-            }else if(arr1[m1] > arr2[m2+1]){
-                h1 = m1-1;
-            }else if(arr2[m2] > arr1[m1+1]){
-                l1 = m1+1;
             }
+            
+            if(l1>r2){
+                h = cut1-1;
+            }else if(l2>r1){
+                l = cut1+1;
+            }
+        
         }
-
+        
         return 0.0;
     }
 }
