004f66b373732def78d3b8b7c56e934773fd27e4
Varun Gudamsetti <92177000+sudo-v4run@users.noreply.github.com>
2025-07-21 23:07:02 +0530

    Time: 2 ms (20.08%), Space: 42.3 MB (10.44%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,28 +1,44 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-
-        String res = "";
-
-        int i = 0;
-
-        while(true){
-            
-            for(int j = 0 ; j < strs.length ; j++){
-
-                if(i >= strs[j].length()){
-                    return res;
-                }
-
-                char cur = strs[0].charAt(i);
-
-                if(strs[j].charAt(i) != cur){
-                    return res;
+        int n = strs.length;
+        
+//         String ans = strs[0];
+        
+//         for(int i = 1; i < n ; i++){
+//             String curS = strs[i];
+//             while(curS.indexOf(ans)!=0){
+//                 ans = ans.substring(0,ans.length()-1);
+//             }
+//         }
+        
+        
+//         return ans;
+        
+        
+        
+        String ans = strs[0];
+        
+        for(int i = 0 ; i < ans.length() ; i++){
+            char ch = ans.charAt(i);
+            for(int j = 1 ; j < n ; j++){
+                String curS= strs[j];
+                if(i>=curS.length()||ch!=curS.charAt(i)){
+                    return ans.substring(0,i);
                 }
             }
-
-            res += strs[0].charAt(i);
-
-            i++;
         }
+        
+        return ans;
+        
+        
+//         for(int i = 1; i < n ; i++){
+//             String curS = strs[i];
+//             while(ans.length()>curS.length() || 
+//                             !ans.equals(curS.substring(0,ans.length()) )  ){
+//                 ans = ans.substring(0,ans.length()-1);
+//             }
+//         }
+        
+//         return ans;
     }
 }
