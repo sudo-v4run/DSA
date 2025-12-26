ff49057e1b2ca4cdb9a619699f693af7e136f17b
Varun Gudamsetti <92177000+sudo-v4run@users.noreply.github.com>
2025-12-26 12:22:44 +0530

    Time: 57 ms (39.2%), Space: 79.5 MB (44.89%) - LeetHub

--- 0327-count-of-range-sum/0327-count-of-range-sum.java
+++ 0327-count-of-range-sum/0327-count-of-range-sum.java
@@ -1,21 +1,25 @@
 class Solution {
+    static int res;
     public int countRangeSum(int[] nums, int lower, int upper) {
+        res = 0;
         int n = nums.length;
         long[] sums = new long[n + 1];
         for (int i = 0; i < n; ++i)
             sums[i + 1] = sums[i] + nums[i];
-        return partition(sums, 0, n, lower, upper);
+
+        
+        partition(sums, 0, n, lower, upper);
+
+        return res;
     }
 
-    public static int partition(long arr[], int l, int h, int lower, int upper) {
+    public static void partition(long arr[], int l, int h, int lower, int upper) {
         if (l < h) {
             int m = (l + h) / 2;
-            int count = partition(arr, l, m, lower, upper);
-            count += partition(arr, m + 1, h, lower, upper);
-            count += merge(arr, l, m, h, lower, upper);
-            return count;
+            partition(arr, l, m, lower, upper);
+            partition(arr, m + 1, h, lower, upper);
+            res += merge(arr, l, m, h, lower, upper);
         }
-        return 0;
     }
 
     public static int merge(long arr[], int l, int m, int r, int lower, int upper) {
