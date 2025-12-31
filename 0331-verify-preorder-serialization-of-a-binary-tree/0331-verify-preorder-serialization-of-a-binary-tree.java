b884bd29ff659cbad7b91358fd60b5454b5172f7
Varun Gudamsetti <92177000+sudo-v4run@users.noreply.github.com>
2025-12-31 17:55:33 +0530

    Time: 119 ms (6.84%), Space: 47.9 MB (5.21%) - LeetHub

---
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean isValidSerialization(String preorder) {
+        
+        Stack<Character> st = new Stack<>();
+        String temp[] = preorder.split(",");
+
+        char pre[] = new char[temp.length];
+        int i = 0;
+        for(String s : temp){
+            pre[i++] = s.charAt(0);
+        }
+
+        int index = 1;
+        char prev = 'a';
+        st.push(pre[0]);
+
+        while(!st.isEmpty() && index <= pre.length){
+            if(!st.isEmpty() && st.peek() == prev && prev == '#'){
+                st.pop();
+                st.pop();
+                if(st.isEmpty()){
+                    return false;
+                }
+                st.pop();
+                
+                if(!st.isEmpty()){
+                    prev = st.peek();
+                }else{
+                    prev = 'a';
+                }
+
+                st.push('#');
+            }else{
+                prev = st.peek();
+                if(index == pre.length){
+                    break;
+                }
+                st.push(pre[index++]);
+            }
+
+            System.out.println(st);
+        }
+
+        return st.size() == 1 && st.peek() == '#';
+    }
+}
