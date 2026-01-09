03e8b04b7b8c042dc11872719272f47c0588d6e7
Varun Gudamsetti <92177000+sudo-v4run@users.noreply.github.com>
2026-01-09 16:55:56 +0530

    Time: 4 ms (94.32%), Space: 44.8 MB (80.19%) - LeetHub

--- 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
@@ -1,38 +1,52 @@
 class Solution {
     public boolean isValidSerialization(String preorder) {
-        
-        Stack<Character> st = new Stack<>();
-        String pre[] = preorder.split(",");
 
-        int index = 1;
-        char prev = 'a';
-        st.push(pre[0].charAt(0));
-
-        while(!st.isEmpty()){
-            if(!st.isEmpty() && st.peek() == prev && prev == '#'){
-                st.pop();
-                st.pop();
-                if(st.isEmpty()){
-                    return false;
-                }
-                st.pop();
-
-                if(!st.isEmpty()){
-                    prev = st.peek();
-                }else{
-                    prev = 'a';
-                }
-
-                st.push('#');
-            }else{
-                prev = st.peek();
-                if(index == pre.length){
-                    break;
-                }
-                st.push(pre[index++].charAt(0));
+        String nodes[] = preorder.split(",");
+        int edges = 1;
+        for(String node : nodes){
+            edges--; // consume one for parent
+            if(edges < 0){
+                return false;
+            }
+            if(!node.equals("#")){
+                edges += 2;  // produce 2 childs
             }
         }
 
-        return st.size() == 1 && st.peek() == '#';
+        return edges == 0;
+        
+        // Stack<Character> st = new Stack<>();
+        // String pre[] = preorder.split(",");
+
+        // int index = 1;
+        // char prev = 'a';
+        // st.push(pre[0].charAt(0));
+
+        // while(!st.isEmpty()){
+        //     if(!st.isEmpty() && st.peek() == prev && prev == '#'){
+        //         st.pop();
+        //         st.pop();
+        //         if(st.isEmpty()){
+        //             return false;
+        //         }
+        //         st.pop();
+
+        //         if(!st.isEmpty()){
+        //             prev = st.peek();
+        //         }else{
+        //             prev = 'a';
+        //         }
+
+        //         st.push('#');
+        //     }else{
+        //         prev = st.peek();
+        //         if(index == pre.length){
+        //             break;
+        //         }
+        //         st.push(pre[index++].charAt(0));
+        //     }
+        // }
+
+        // return st.size() == 1 && st.peek() == '#';
     }
 }
