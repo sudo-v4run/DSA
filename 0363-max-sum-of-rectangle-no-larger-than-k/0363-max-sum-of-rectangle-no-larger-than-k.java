35f48042787c688acef36904224a191cbf87e033
Varun Gudamsetti <92177000+sudo-v4run@users.noreply.github.com>
2026-02-03 17:17:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0363-max-sum-of-rectangle-no-larger-than-k/0363-max-sum-of-rectangle-no-larger-than-k.java
@@ -0,0 +1,73 @@
+class Solution {
+    public int maxSumSubmatrix(int[][] matrix, int limit) {
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int pre[][] = new int[m][n];
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+
+                int left = 0;
+                int top = 0;
+                int dia = 0;
+
+                if(j-1 >= 0){
+                    left = pre[i][j-1];
+                }
+                if(i-1 >= 0){
+                    top = pre[i-1][j];
+                }
+                if(i-1 >= 0 && j-1 >= 0){
+                    dia = pre[i-1][j-1];
+                }
+
+                pre[i][j] = matrix[i][j] + left + top - dia;
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+
+                if(pre[i][j] <= limit){
+                    max = Math.max(max, pre[i][j]);
+                }
+
+                for(int k = -1; k <= i; k++){
+                    for(int l = -1; l <= j; l++){
+
+                        if(k == i && l == j){
+                            continue;
+                        }
+                        
+                        int top = 0;
+                        if(k != -1){
+                            top = pre[k][j];
+                        }
+
+                        int left = 0;
+                        if(l != -1){
+                            left = pre[i][l];
+                        }
+
+                        int dia = 0;
+                        if(k != -1 && l != -1){
+                            dia = pre[k][l];
+                        }
+
+                        int cur = pre[i][j]-top-left+dia;
+
+                        if(cur <= limit){
+                            max = Math.max(max, cur);
+                        }
+                    }
+                }
+            }
+        }
+
+        return max;
+    }
+}
