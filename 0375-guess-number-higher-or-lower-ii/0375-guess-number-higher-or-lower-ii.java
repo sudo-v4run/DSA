04198982288ecdf8a2c484fc984dbc7bc4a62958
Varun Gudamsetti <92177000+sudo-v4run@users.noreply.github.com>
2026-04-16 15:32:34 +0530

    Time: 66 ms (5.77%), Space: 43.2 MB (66.77%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.java
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.java
@@ -1,12 +1,23 @@
 class Solution {
+    static int preSum[];
     public int getMoneyAmount(int n) {
+
+        preSum = new int[n+1];
 
         int dp[][] = new int[n+1][n+1];
         for(int row[] : dp){
             Arrays.fill(row, -1);
         }
+
         
-        return f((n/2), n, dp);
+        preSum[0] = 0;
+        for(int i = 1; i <= n; i++){
+            preSum[i] = preSum[i-1]+i;
+        }
+
+        
+        
+        return f(1, n, dp);
     }
     public int f(int l, int h, int dp[][]){
 
@@ -20,7 +31,21 @@ class Solution {
 
         int ans = Integer.MAX_VALUE;
 
-        for(int index = l; index <= h; index++){
+
+        int t = (preSum[h]-preSum[l-1])/2;
+        int ind = 0;
+        int diff = Integer.MAX_VALUE;
+        for(int i = l; i <= h; i++){
+            if(Math.abs(t-(preSum[i]-preSum[l-1])) < diff){
+                ind = i;
+                diff = Math.abs(t-(preSum[i]-preSum[l-1]));
+            }
+        }
+
+        //System.out.println(preSum[h]+","+ind);
+
+
+        for(int index = Math.max(l, ind - 99) ; index <= Math.min(h, ind + 99) ; index++){
             int left = index + f(l, index-1, dp);
             int right = index + f(index+1, h, dp);
 
